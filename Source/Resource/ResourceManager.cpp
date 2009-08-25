a0a9dc3df8a8aa5ce82e97acf44d306c36853e98
jason <jason@amju.com>
2009-08-25 21:48:51 +0000

    Add resource groups

--- Source/Resource/ResourceManager.cpp
+++ Source/Resource/ResourceManager.cpp
@@ -38,6 +38,16 @@ Resource* Md2Loader(const std::string& resName)
   return (Resource*)pModel;
 }
 
+Resource* ResourceManager::ResGroup::GetRes(const std::string& resName)
+{
+  ResMap::iterator it = m_map.find(resName);
+  if (it == m_map.end())
+  {
+    return 0;
+  }
+  return it->second;
+}
+
 ResourceManager::ResourceManager()
 {
   AddLoader("bmp", BmpLoader);
@@ -50,6 +60,46 @@ bool ResourceManager::AddLoader(const std::string& fileExt, Loader loader)
   return true;
 }
 
+bool ResourceManager::LoadResourceGroup(const std::string& rgName)
+{
+  if (m_groups.find(rgName) != m_groups.end())
+  {
+    // Already loaded
+    return true;
+  }
+
+  ResGroup rg;
+  rg.m_name = rgName;
+  File f;
+  if (!f.OpenRead(rgName + ".txt"))
+  {
+    ReportError("Failed to load resource group " + rgName);
+    return false;
+  }
+
+  // Add group to map now so it exists if a resource needs it 
+  m_groups[rgName] = rg; 
+
+  ResGroup* rgRef = &m_groups[rgName];
+
+  std::string s;
+  while (f.GetDataLine(&s))
+  {
+    PResource res = LoadRes(s);
+    rgRef->m_map[s] = res;
+  }
+
+  return true;
+}
+
+void ResourceManager::FreeResourceGroup(const std::string& rg)
+{
+  ResGroupMap::iterator it = m_groups.find(rg);
+  Assert(it != m_groups.end());
+  m_groups.erase(it);
+}
+
+/*
 Resource* ResourceManager::GetRes(const std::string& resName, Loader loader)
 {
   ResMap::iterator it = m_map.find(resName);
@@ -63,15 +113,29 @@ Resource* ResourceManager::GetRes(const std::string& resName, Loader loader)
   m_map[resName] = r;
   return r;
 }
+*/
 
 Resource* ResourceManager::GetRes(const std::string& resName)
 {
-  ResMap::iterator it = m_map.find(resName);
-  if (it != m_map.end())
+  for (ResGroupMap::iterator it = m_groups.begin();
+       it != m_groups.end();
+       ++it)
   {
-    return it->second.GetPtr();
+    Resource* res = it->second.GetRes(resName);
+    if (res)
+    {
+      return res;
+    }
   }
+#ifdef _DEBUG
+  ReportError("Couldn't find resource " + resName + " in any group");
+#endif
+  return 0;
+}
 
+
+Resource* ResourceManager::LoadRes(const std::string& resName)
+{
   // Get file extension, no dot
   std::string ext = GetFileExt(resName);
   LoaderMap::iterator jt = m_loaders.find(ext);
@@ -83,8 +147,8 @@ Resource* ResourceManager::GetRes(const std::string& resName)
   Assert(jt != m_loaders.end());
   Loader myLoader = jt->second;
   Resource* r = myLoader(resName);
+  Assert(r);
 
-  m_map[resName] = r;
   return r;
 }
 }
