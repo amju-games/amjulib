8419966533a85ae77a2a26bf83d4c8baff468eb3
jason@amju.com <jason@amju.com@3cf48660-8901-11de-b841-6521bc8c149b>
2009-08-25 21:48:51 +0000

    Add resource groups

--- trunk/Source/Resource/ResourceManager.h
+++ trunk/Source/Resource/ResourceManager.h
@@ -26,22 +26,41 @@ public:
   // Add Resource Loader function for each supported Resource type
   typedef Resource* (*Loader)(const std::string& resName);
   // Add loader for given file extension (no dot), e.g. "bmp"
-  // Return a value for the 'static initialisation trick'
+  // Return a value for the 'initialisation trick'
   bool AddLoader(const std::string& fileExt, Loader);
+
+
+  // Load a Resource Group
+  // This is a list of Resources which will be loaded into mem.
+  bool LoadResourceGroup(const std::string& resourceGroupFileName);
+
+  // Free up all resources in the named group
+  void FreeResourceGroup(const std::string& resourceGroupFileName);
 
   // Get a resource - the extension is used to decide which Loader 
   //  to use
   Resource* GetRes(const std::string& resName);
 
   // Get a resource, using the specified Loader
-  Resource* GetRes(const std::string& resName, Loader loader);
+//  Resource* GetRes(const std::string& resName, Loader loader);
+
+  Resource* LoadRes(const std::string& resName);
 
 private:
   typedef std::map<std::string, Loader> LoaderMap;
   LoaderMap m_loaders;
 
-  typedef std::map<std::string, PResource> ResMap;
-  ResMap m_map;
+  struct ResGroup
+  {
+    Resource* GetRes(const std::string& resName);
+
+    std::string m_name;
+    typedef std::map<std::string, PResource> ResMap;
+    ResMap m_map;
+  };
+  
+  typedef std::map<std::string, ResGroup> ResGroupMap;
+  ResGroupMap m_groups;
 };
 }
 
