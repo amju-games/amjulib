50f4e628f841aa0e0dd38b67aeb9f69d777b18fe
jason <jason@amju.com>
2009-09-04 15:02:40 +0000

    Move DrawChildren()/UpdateChildren() here

--- Source/Scene/SceneGraph.cpp
+++ Source/Scene/SceneGraph.cpp
@@ -1,22 +1,35 @@
 #include "SceneGraph.h"
-#include "SceneComposite.h" // Bah, dependency bottleneck
 #include "SceneMesh.h"
 
 namespace Amju
 {
-SceneNode* CreateRoot() { return new SceneNode; }
-SceneNode* CreateSceneMesh() { return new SceneMesh; }
+bool SceneGraph::BlendNode::operator<(const BlendNode& bn) const
+{
+  return m_screenZ > bn.m_screenZ;
+}
 
 SceneGraph::SceneGraph()
 {
-  TheSceneNodeFactory::Instance()->Add(SceneNode::NAME, &CreateRoot);
-  TheSceneNodeFactory::Instance()->Add(SceneMesh::NAME, &CreateSceneMesh);
+}
 
+PSceneNode SceneGraph::GetRootNode(GraphType gt)
+{
+  return m_root[gt];
 }
 
 void SceneGraph::SetRootNode(GraphType gt, PSceneNode root)
 {
   m_root[gt] = root;
+}
+
+void SceneGraph::SetCamera(PSceneNode cameraNode)
+{
+  m_camera = cameraNode;
+}
+
+PSceneNode SceneGraph::GetCamera()
+{
+  return m_camera;
 }
 
 void SceneGraph::Clear()
@@ -25,19 +38,79 @@ void SceneGraph::Clear()
   {
     m_root[i] = 0;
   }
-}
-
-PSceneNode SceneGraph::GetRootNode(GraphType gt)
-{
-  return m_root[gt];
+  m_camera = 0;
 }
 
 void SceneGraph::AddBlendNode(PSceneNode p)
 {
   BlendNode bn;
   bn.m_sceneNode = p;
-  bn.m_screenZ = 0; //GetCurrentCamera()->GetPos() - p->GetPos() // ..??
+
+  // Get dist to camera
+  const Matrix& camMat = GetCamera()->m_combined;
+  Vec3f camPos(camMat[12], camMat[13], camMat[14]);
+  const Matrix& mat = p->m_combined;
+  Vec3f pos(mat[12], mat[13], mat[14]);
+
+  bn.m_screenZ = (camPos - pos).SqLen(); // ..??
   m_blendNodes.push_back(bn);
+}
+
+void SceneGraph::DrawNode(SceneNode* p)
+{
+  if (p && p->IsVisible())
+  {
+    PushColour();
+    MultColour(p->m_colour);
+    p->Draw();
+    PopColour();
+  }
+}
+
+void SceneGraph::DrawChildren(SceneNode* node)
+{
+  // If this node is a camera, set the SceneGraph current camera to this..?
+  if (node->IsCamera())
+  {
+    SetCamera(node);
+  }
+
+  node->BeforeDraw(); 
+
+  AmjuGL::PushMatrix();
+  AmjuGL::MultMatrix(node->m_local);
+
+  PushColour();
+  MultColour(node->m_colour);
+
+  unsigned int s = node->m_children.size();
+  for (unsigned int i = 0; i < s; i++)
+  {
+    PSceneNode& child = node->m_children[i];
+
+    // TODO If a node is not visible, are all children automatically
+    //  invisible too ?
+    if (child->IsVisible())
+    {
+      if (child->IsBlended())
+      {
+        // Add to blend list
+        AddBlendNode(child);
+      }
+      else
+      {
+        DrawNode(child);
+      }
+    }
+
+    // Draw children even if this node is invisible
+    DrawChildren(child);
+  }  
+
+  PopColour();
+  AmjuGL::PopMatrix();
+
+  node->AfterDraw();
 }
 
 void SceneGraph::Draw()
@@ -46,54 +119,100 @@ void SceneGraph::Draw()
 
   AmjuGL::PushMatrix();
 
-  // Camera(s) are nodes in the OPAQUE tree.
+  // TODO SORT THIS OUT
+  // Camera(s) are nodes in the OPAQUE tree ??
   // Multiple cameras are ok (allowing mirrors ?) 
   // The current camera is set when DrawChildren() discovers that the
   // current node is a camera.
   // Not: m_camera->Draw();
 
+  if (m_camera)
+  {
+    m_camera->Draw();
+  }
+
   Assert(m_root[AMJU_OPAQUE]);
-  m_root[AMJU_OPAQUE]->Draw();
-  m_root[AMJU_OPAQUE]->DrawChildren();
+
+  m_root[AMJU_OPAQUE]->CombineTransform();
+
+  DrawNode(m_root[AMJU_OPAQUE]);
+  DrawChildren(m_root[AMJU_OPAQUE]);
 
   // Don't want lighting for sky, shadows or HUD, right ?
   AmjuGL::Disable(AmjuGL::AMJU_LIGHTING);
 
   if (m_root[AMJU_SKYBOX])
   {
-    m_root[AMJU_SKYBOX]->Draw();
-    m_root[AMJU_SKYBOX]->DrawChildren();
+    // Centre on camera
+    if (m_camera)
+    {
+      // Translate to camera eye pos ?     
+      //m_camera->Draw(); // ??????
+    }
+
+    DrawNode(m_root[AMJU_SKYBOX]);
+    DrawChildren(m_root[AMJU_SKYBOX]);
   }
 
   AmjuGL::PopMatrix();
+  AmjuGL::PushMatrix();
+
+  // Do camera transformation again for blended nodes
+  if (m_camera)
+  {
+    m_camera->Draw();
+  }
 
   // Draw alpha-blended nodes
   std::sort(m_blendNodes.begin(), m_blendNodes.end());
 
   for (BlendNodes::iterator it = m_blendNodes.begin(); it != m_blendNodes.end(); ++it)
   {
-    AmjuGL::PushMatrix();
     PSceneNode sn = it->m_sceneNode; 
 
-    if (sn->GetParent())
-    {
-      AmjuGL::MultMatrix(sn->GetParent()->GetCombinedTransform());
-    }
-    sn->Draw(); // concats local transform
+    AmjuGL::PushMatrix();
+    // Blended nodes may need to mult by their combined transform.
+    // Shadows don't need to do this because they use absolute coords.
+    DrawNode(sn);
     AmjuGL::PopMatrix();
   }
+  AmjuGL::PopMatrix();
+}
+
+void SceneGraph::UpdateNode(SceneNode* node)
+{
+  if (node)
+  {
+    node->Update();
+  }
+}
+
+void SceneGraph::UpdateChildren(SceneNode* node)
+{
+  if (!node)
+  {
+    return;
+  }
+
+  unsigned int s = node->m_children.size();
+  for (unsigned int i = 0; i < s; i++)
+  {
+    PSceneNode& child = node->m_children[i];
+    UpdateNode(child);
+    UpdateChildren(child);
+  }  
 }
 
 void SceneGraph::Update()
 {
   Assert(m_root[AMJU_OPAQUE]);
-  m_root[AMJU_OPAQUE]->Update();
-  m_root[AMJU_OPAQUE]->UpdateChildren();
+  UpdateNode(m_root[AMJU_OPAQUE]);
+  UpdateChildren(m_root[AMJU_OPAQUE]);
 
   if (m_root[AMJU_SKYBOX])
   {
-    m_root[AMJU_SKYBOX]->Update();
-    m_root[AMJU_SKYBOX]->UpdateChildren();
+    UpdateNode(m_root[AMJU_SKYBOX]);
+    UpdateChildren(m_root[AMJU_SKYBOX]);
   }
 }
 }
