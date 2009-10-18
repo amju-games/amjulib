234db531d85187b257f63855dea0b92dd3042e23
jason <jason@amju.com>
2009-10-18 20:00:16 +0000

    Event poller using Win32 messages

--- Source/EventsWin32/EventPollerImplWin32.h
+++ Source/EventsWin32/EventPollerImplWin32.h
@@ -2,13 +2,29 @@
 #define EVENT_POLLER_IMPL_WIN32
 
 #include "EventPoller.h"
+#include <windows.h>
 
 namespace Amju
 {
 class EventPollerImplWin32 : public EventPollerImpl
 {
 public:
+  EventPollerImplWin32();
+  ~EventPollerImplWin32();
   virtual void Update(Listeners* pListeners);
+
+  static LRESULT WndProc(HWND, UINT, WPARAM, LPARAM);
+
+private:
+  void MouseMove(int x, int y);
+  void LMouseButton(bool down);
+  void OnKey(WPARAM c, bool down);
+
+private:
+  Listeners* m_listeners;
+
+  // Pointer to one and only instance
+  static EventPollerImplWin32* s_instance;
 };
 }
 
