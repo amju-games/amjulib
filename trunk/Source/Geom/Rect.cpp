f7dfb1e4164162f898b33c71ae6a96f2f5e7b717
jason@amju.com <jason@amju.com@3cf48660-8901-11de-b841-6521bc8c149b>
2009-10-18 21:22:15 +0000

    Add Translate, Get funcs

--- trunk/Source/Geom/Rect.cpp
+++ trunk/Source/Geom/Rect.cpp
@@ -1,4 +1,5 @@
 #include "Rect.h"
+#include "AmjuAssert.h"
 
 namespace Amju
 {
@@ -17,6 +18,28 @@ void Rect::Set(float xmin, float xmax, float ymin, float ymax)
   m_xmax = xmax;
   m_ymin = ymin;
   m_ymax = ymax;
+}
+
+void Rect::Translate(float x, float y)
+{
+  m_xmin += x;
+  m_xmax += x;
+  m_ymin += y;
+  m_ymax += y;
+}
+
+float Rect::GetMin(int axis) const
+{
+  Assert(axis < 2);
+  const float* const f = &m_xmin;
+  return f[axis * 2];
+}
+
+float Rect::GetMax(int axis) const
+{
+  Assert(axis < 2);
+  const float* const f = &m_xmin;
+  return f[axis * 2 + 1];
 }
 
 bool Rect::IsPointIn(const Vec2f& point)
