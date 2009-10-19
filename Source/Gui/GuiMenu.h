f53fe57f2182fdc84ac11405e4ba8fc861879fa4
jason <jason@amju.com>
2009-10-19 21:25:48 +0000

    Added menu base class - not finished

---
+++ Source/Gui/GuiMenu.h
@@ -0,0 +1,38 @@
+#ifndef GUI_MENU_H
+#define GUI_MENU_H
+
+#include <vector>
+#include "GuiElement.h"
+#include "GuiText.h"
+
+namespace Amju
+{
+class GuiMenuItem : public GuiText // ?????
+{
+public:
+  GuiMenuItem(const std::string& text);
+  virtual void Draw();
+};
+typedef RCPtr<GuiMenuItem> PGuiMenuItem;
+
+class GuiMenu : public GuiElement
+{
+public:
+  GuiMenu();
+  virtual void Draw();  
+
+  void AddItem(GuiMenuItem* pItem);
+  // TODO Insert, Remove items
+
+  // Adjust selected item when cursor moves
+  virtual void OnCursorEvent(const CursorEvent&);
+
+protected:
+  typedef std::vector<PGuiMenuItem> Items;
+  Items m_items;
+  int m_selected; // index in m_items or -1
+};
+}
+
+#endif
+
