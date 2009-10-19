11701021ef5181d41dcf6c658ae2955df6f88ad3
jason <jason@amju.com>
2009-10-19 21:28:59 +0000

    Add justification, font, etc

--- Source/Gui/GuiText.cpp
+++ Source/Gui/GuiText.cpp
@@ -1,15 +1,78 @@
 #include "GuiText.h"
+#include "Font.h"
+#include "DrawRect.h"
 
 namespace Amju
 {
 const char* GuiText::NAME = "gui-text";
 
+GuiText::GuiText()
+{
+  m_fontName = "font2d/arial-font.font";
+  m_just = AMJU_JUST_CENTRE;
+  m_textWidth = 0;
+}
+
+Font* GuiText::GetFont()
+{
+  static Font* font = 
+    (Font*)TheResourceManager::Instance()->GetRes(m_fontName);
+  Assert(font);
+
+  return font;
+}
+
 void GuiText::Draw()
 {
+#ifdef _DEBUG
+  AmjuGL::PushMatrix();
+  AmjuGL::SetIdentity();
+  Rect r = GetRect(this);
+  DrawRect(r);
+  AmjuGL::PopMatrix();
+#endif // _DEBUG
+
+  switch (m_just)
+  {
+  case AMJU_JUST_LEFT:
+    GetFont()->Print(m_pos.x, m_pos.y, m_text.c_str());
+    break;
+  case AMJU_JUST_RIGHT:
+    GetFont()->Print(m_pos.x + m_size.x - m_textWidth, m_pos.y, m_text.c_str());
+    break;
+  case AMJU_JUST_CENTRE:
+    GetFont()->Print(m_pos.x + 0.5f * (m_size.x - m_textWidth), m_pos.y, m_text.c_str());
+    break;
+  default:
+    Assert(0);
+  }
+}
+
+void GuiText::SetText(const std::string& text)
+{
+  m_text = text;
+  m_textWidth = GetFont()->GetTextWidth(text);
+  //m_size.y = 0.08f; // Size of one line of text - TODO
+  // TODO Multi lines; other GuiElement types
 }
 
 bool GuiText::Load(File* f)
 {
+  if (!GuiElement::Load(f))
+  {
+    return false;
+  }
+  std::string s;
+  if (!f->GetLocalisedString(&s))
+  {
+    f->ReportError("GUI Text: Expected localised string");
+    return false;
+  }
+  SetText(s);
+
+  // TODO Flags for multiline/word wrap
+  // font name, font size, etc
+
   return true;
 }
 }
--- Source/Gui/GuiText.h
+++ Source/Gui/GuiText.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include "GuiElement.h"
+#include "Font.h"
 
 namespace Amju
 {
@@ -11,11 +12,20 @@ class GuiText : public GuiElement
 public:
   static const char* NAME;
 
+  GuiText();
   virtual void Draw();  
   virtual bool Load(File*); 
 
+  void SetText(const std::string& text);
+
+  Font* GetFont();
+
 protected:
   std::string m_text;
+  std::string m_fontName;
+  enum Just { AMJU_JUST_LEFT, AMJU_JUST_RIGHT, AMJU_JUST_CENTRE };
+  Just m_just;
+  float m_textWidth;
 };
 }
 
