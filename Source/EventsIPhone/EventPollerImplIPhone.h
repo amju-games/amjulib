afe912e747e8420a7e5dd138b42746710fa269b9
jason.e.colman <jason.e.colman@gmail.com>
2010-08-06 11:29:44 +0000

    iPhone events - TODO!

---
+++ Source/EventsIPhone/EventPollerImplIPhone.h
@@ -0,0 +1,17 @@
+#ifndef EVENT_POLLER_IMPL_IPHONE_H
+#define EVENT_POLLER_IMPL_IPHONE_H
+
+#include <EventPoller.h>
+
+namespace Amju
+{
+class EventPollerImplIPhone : public EventPollerImpl
+{
+public:
+  virtual void Update(Listeners* pListeners) {}
+};
+}
+
+#endif
+
+
