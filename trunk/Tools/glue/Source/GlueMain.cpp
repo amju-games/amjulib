38a06e31a516f68fabbd0f072977cdf62892e64e
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2010-09-26 15:50:06 +0000

    New features: allow adding multiple files (but MSDOS doesn't expand wildcards). Patch a file - appends new version to glue file. Cat contents of a file.

--- trunk/Tools/glue/Source/GlueMain.cpp
+++ trunk/Tools/glue/Source/GlueMain.cpp
@@ -26,10 +26,12 @@ void ReportError(const std::string& s)
 
 void Usage()
 {
-  cout << "Usage: glue -c <gluefile>         - create new glue file.\n"
-       << "       glue -a <gluefile> <file>  - adds file to gluefile.\n"
-       << "       glue -d <gluefile>         - lists all files in gluefile.\n";
-
+  cout << "Usage:\n"
+       << "glue -c <gluefile>          - create new glue file.\n"
+       << "glue -a <gluefile> <files>  - adds file(s) to gluefile.\n"
+       << "glue -p <gluefile> <files>  - replaces file(s) in gluefile.\n"
+       << "glue -d <gluefile>          - lists all files in gluefile.\n"
+       << "glue -s <gluefile> <file>   - output contents of file.\n";
 }
 
 int main(int argc, char* argv[])
@@ -39,6 +41,8 @@ int main(int argc, char* argv[])
   // - create a new glue file with the given filename.
   // glue -a[dd] <gluefile> <subfile>
   //  - glue subfile onto gluefile.
+  // glue -p[atch] <gluefile> <subfile>
+  //  - replace subfile in gluefile.
   // glue -d[ir] <gluefile>
   //  - list contents of gluefile
 
@@ -92,7 +96,48 @@ int main(int argc, char* argv[])
     return 0;
   }
 
-  if (option == "-a")
+  if (option == "-a" || option == "-p")
+  {
+    if (argc < 4)
+    {
+      Usage();
+      return -1;
+    }
+
+    GlueFile gf;
+    if (!gf.OpenGlueFile(argv[2], false)) // false means write access
+    {
+      cout << "Failed to open glue file " << argv[2] << endl;
+      return -1;
+    }
+
+    for (int i = 3; i < argc; i++)
+    {
+      // Add file to glue file 
+      bool ok = false;
+      if (option == "-a") // add
+      {
+        ok = gf.AddItem(argv[i]); 
+      }
+      else // patch
+      {
+        ok = gf.PatchItem(argv[i]);
+      }
+
+      if (ok)
+      {
+        cout << "Added file " << argv[i] << " ok\n";
+      }
+      else
+      {
+        cout << "Failed to add " << argv[i] << " to glue file " << argv[2] << endl;
+        return -1;
+      }
+    }
+    return 0;
+  }
+
+  if (option == "-s")
   {
     if (argc != 4)
     {
@@ -107,16 +152,24 @@ int main(int argc, char* argv[])
       return -1;
     }
 
-    if (gf.AddItem(argv[3]))
+    std::string subfile = argv[3];
+    cout << "Stat file " << subfile << ":" << endl;
+    unsigned int seekbase = 0;
+    if (!gf.GetSeekBase(subfile, &seekbase))
     {
-      cout << "Added file " << argv[3] << " ok\n";
-      return 0;
-    }
-    else
-    {
-      cout << "Failed to add " << argv[3] << " to glue file " << argv[2] << endl;
+      cout << "Failed to find subfile " << subfile << endl;
       return -1;
     }
+    uint32 size = gf.GetSize(subfile);
+    unsigned char* buf = new unsigned char[size + 1];
+    gf.GetBinary(seekbase, size, buf);
+    for (unsigned int i = 0; i < size; i++)
+    {
+      std::cout << buf[i];
+    }
+    std::cout << "\nEnd of stat\n";
+
+    return 0;
   }
 
   Usage();
