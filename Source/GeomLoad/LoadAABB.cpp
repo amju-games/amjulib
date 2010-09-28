bc5db3b1f5a4d645670a56cdbf19c57358556b30
jason.e.colman <jason.e.colman@gmail.com>
2010-09-28 10:43:53 +0000

    Add LoadAABB

---
+++ Source/GeomLoad/LoadAABB.cpp
@@ -0,0 +1,29 @@
+#include "LoadAABB.h"
+#include <File.h>
+#include <AABB.h>
+#include <StringUtils.h>
+
+namespace Amju
+{
+bool LoadAABB(File* f, AABB* aabb)
+{
+  std::string s;
+  if (!f->GetDataLine(&s))
+  {
+    Assert(0);
+    return false;
+  }
+  Strings strs = Split(s, ',');
+  Assert(strs.size() == 6);
+  aabb->Set(
+    ToFloat(strs[0]), 
+    ToFloat(strs[1]),
+    ToFloat(strs[2]),
+    ToFloat(strs[3]),
+    ToFloat(strs[4]),
+    ToFloat(strs[5]));
+
+  return true;
+
+}
+}
