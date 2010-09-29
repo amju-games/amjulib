bccecebd291ec54650f80c9eab3d34074504fb60
jason.e.colman <jason.e.colman@gmail.com>
2010-09-29 16:23:31 +0000

    GameStates are now Singletons, so not stored in a map and not ref counted

--- Source/Game/GameState.h
+++ Source/Game/GameState.h
@@ -1,12 +1,11 @@
 #ifndef GAME_STATE_H
 #define GAME_STATE_H
 
-#include "RCPtr.h"
 #include "EventListener.h"
 
 namespace Amju
 {
-class GameState : public RefCounted, public EventListener
+class GameState : public EventListener
 {
 public:
   virtual ~GameState() {}
@@ -16,11 +15,9 @@ public:
   virtual void Draw2d() = 0;
   virtual void OnActive();
   virtual void OnDeactive();
-//  virtual bool Load() { return true; } // ?
 
 protected:
 };
-typedef RCPtr<GameState> PGameState;
 }
 
 #endif
