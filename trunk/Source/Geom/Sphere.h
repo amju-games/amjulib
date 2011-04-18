df8685c921fc96f00e065cfa36e04bd4b4621173
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2011-04-18 15:06:38 +0000

    Set functions

--- trunk/Source/Geom/Sphere.h
+++ trunk/Source/Geom/Sphere.h
@@ -8,11 +8,14 @@ namespace Amju
 class Sphere
 {
 public:
-  Sphere();
+  Sphere() { m_radius = 0; }
   Sphere(const Vec3f& centre, float radius) : m_centre(centre), m_radius(radius) {}
  
   const Vec3f& GetCentre() const;
   float GetRadius() const;
+
+  void SetCentre(const Vec3f& c) { m_centre = c; }
+  void SetRadius(float r) { m_radius = r; }
 
 private:
   Vec3f m_centre;
