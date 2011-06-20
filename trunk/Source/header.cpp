c2752abd586fb65400f110a228fdbe227a1d5978
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2011-06-20 16:11:09 +0000

    Updated year

--- trunk/Source/header.cpp
+++ trunk/Source/header.cpp
@@ -1,5 +1,5 @@
 /*
-Amju Games source code (c) Copyright Jason Colman 2000-2010
+Amju Games source code (c) Copyright Jason Colman 2000-2011
 */
 
 #include "AmjuFirst.h"
--- trunk/Source/header.h
+++ trunk/Source/header.h
@@ -1,4 +1,4 @@
 /*
-Amju Games source code (c) Copyright Jason Colman 2000-2008
+Amju Games source code (c) Copyright Jason Colman 2000-2011
 */
 
