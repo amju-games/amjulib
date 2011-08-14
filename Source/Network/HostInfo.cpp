bea07bea9fe800055734df55f99fec9ae8201bb3
jason.e.colman <jason.e.colman@gmail.com>
2011-08-14 17:16:38 +0000

    Added host info for Mac and iPhone

--- Source/Network/HostInfo.cpp
+++ Source/Network/HostInfo.cpp
@@ -2,13 +2,39 @@
 
 #ifdef GEKKO
 #include <network.h>
-
 u32 net_gethostip(); // http://forum.wiibrew.org/read.php?11,7676,7678
-
 #endif
+
+#if defined (MACOSX) || defined (IPHONE)
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#endif // MACOSX/IPHONE
+
+#include <StringUtils.h>
+#include <AmjuAssert.h>
 
 namespace Amju
 {
+std::string ToString(const IfInfo& ifi)
+{
+  std::string s = "Name: ";
+  s += ifi.m_name;
+  s += " IP Addr: ";
+  s += ToString((int)ifi.m_ip[0]);
+  s += ".";
+  s += ToString((int)ifi.m_ip[1]);
+  s += ".";
+  s += ToString((int)ifi.m_ip[2]);
+  s += ".";
+  s += ToString((int)ifi.m_ip[3]);
+  s += " Mac Addr: ";
+  for (int i = 0; i < 6; i++)
+  {
+    s += ToHexString(ifi.m_mac[i]);
+  }
+  return s;
+}
+
 bool GetHostInfo(InfoVec* v)
 {
 #ifdef GEKKO
@@ -26,8 +52,41 @@ bool GetHostInfo(InfoVec* v)
 
   v->push_back(ifi);
   return true;
-#endif
+#endif // GEKKO
 
+#if defined (MACOSX) || defined (IPHONE)
+  // http://blog.zachwaugh.com/post/309927273/programmatically-retrieving-ip-address-of-iphone?d0594d68
+
+  struct ifaddrs *interfaces = 0;
+
+  // retrieve the current interfaces - returns 0 on success
+  int success = getifaddrs(&interfaces);
+
+  if (success == 0)
+  {
+    // Loop through linked list of interfaces
+    struct ifaddrs *temp_addr = interfaces;
+    while (temp_addr)
+    {
+      if (temp_addr->ifa_addr->sa_family == AF_INET)
+      {
+        IfInfo ifi;
+        ifi.m_name = temp_addr->ifa_name;
+        // ("en0" is the wifi connection on the iPhone)
+
+        Assert(sizeof(in_addr) == 4);
+        memcpy(&ifi.m_ip, &((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr, 4);
+
+        v->push_back(ifi);
+      }
+
+      temp_addr = temp_addr->ifa_next;
+    }
+  }
+
+  freeifaddrs(interfaces); // Free memory
+  return (success == 0);
+#endif // MACOSX/IPHONE
 }
 }
 
--- Source/Network/HostInfo.h
+++ Source/Network/HostInfo.h
@@ -1,6 +1,7 @@
 #ifndef AMJU_HOST_INFO_H
 #define AMJU_HOST_INFO_H
 
+#include <string>
 #include <vector>
 
 namespace Amju
@@ -11,7 +12,10 @@ struct IfInfo
   unsigned char m_mac[6];
   unsigned char m_ip[4];
   unsigned char m_ipmask[4];
+  std::string m_name;
 };
+
+std::string ToString(const IfInfo& ifi);
 
 typedef std::vector<IfInfo> InfoVec;
 
