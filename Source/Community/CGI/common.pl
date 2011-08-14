5affd0663d48ed5d8fc84d46b78ae08607c941a7
jason.e.colman <jason.e.colman@gmail.com>
2011-08-14 10:34:17 +0000

    Adding module for community features

---
+++ Source/Community/CGI/common.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl -w
+
+#####################################################################
+# Common subs/DB connect code 
+#####################################################################
+
+use strict;
+use CGI qw(:standard);
+use DBI;
+use CGI::Carp qw(fatalsToBrowser);
+
+require "credentials.pl";
+
+# Global database connection
+# OUR - not MY - so we can access it from files which "require" this one
+our $dbh;
+
+print "Content-type:text/html\n\n";
+
+#
+# Useful DB functions
+#
+sub update_or_insert($)
+{
+  my $sql = shift;
+  my $query = $dbh->prepare($sql) or die "Prepare failed for: '$sql'";
+  $query->execute or die "Failed to execute '$sql'";
+}
+
+sub insert($)
+{
+  my $sql = shift;
+  update_or_insert($sql);
+  print "Insert successful: '$sql'\n";
+}
+
+sub update($)
+{
+  my $sql = shift;
+  update_or_insert($sql);
+  print "Update successful: '$sql'\n";
+}
+
+sub drop_table($)
+{
+  my $table_name = shift;
+
+  my $sql = <<END;
+DROP TABLE IF EXISTS `$table_name`;
+END
+
+  update_or_insert($sql);
+}
+
+sub my_connect()
+{  
+  $dbh = DBI->connect($dsn, $username, $pass);
+
+  if (not $dbh)
+  {
+    die "Failed to connect to DB.\n";
+  }
+  print "Connected to DB!\n";
+}
+
+1;
+
