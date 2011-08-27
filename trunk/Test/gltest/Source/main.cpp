97dccab2478dd7155b4db4b64ebdef8d3c7d3766
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2011-08-27 14:56:51 +0000

    Fiddling with tests, (working on DX11)

--- trunk/Test/gltest/Source/main.cpp
+++ trunk/Test/gltest/Source/main.cpp
@@ -1,8 +1,22 @@
+#ifdef WIN32
+// Windows: use OpenGL or Direct 3D - can choose at runtime, 
+// so don't make this a compile time switch ?
+// OR: allow compile-time switch to completely remove DX or OpenGL, 
+// reducing DLL dependencies
+
+// Define one of these - this chooses the API at compile time ATM
+//#define AMJU_USE_OPENGL
+//#define AMJU_USE_DX9
+#define AMJU_USE_DX11
+
+#endif // WIN32
+
 #include <main.h>
 #include <AmjuGLWindowInfo.h>
 #include <Game.h>
 #include <Font.h>
 #include "GSLighting.h"
+#include "GSTexture.h"
 
 namespace Amju
 {
@@ -11,7 +25,7 @@ Amju::AmjuGLWindowInfo w(640, 480, false);
 
 void StartUp()
 {
-  TheGame::Instance()->SetCurrentState(TheGSLighting::Instance());
+  TheGame::Instance()->SetCurrentState(TheGSTexture::Instance());
   TheResourceManager::Instance()->AddLoader("font", FontLoader);
 }
 }
