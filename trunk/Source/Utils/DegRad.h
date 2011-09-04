c0ec79d7e048f1b4c73d14c5c5e92a8f465f7b03
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2011-09-04 16:23:57 +0000

    Check if defines already defined -- they are in libogc!

--- trunk/Source/Utils/DegRad.h
+++ trunk/Source/Utils/DegRad.h
@@ -7,7 +7,13 @@
 
 #include <math.h>
 
+// (already defined in libogc)
+#ifndef DegToRad 
 #define DegToRad(x) (float)((x)*(M_PI/180.0))
+#endif
+
+#ifndef RadToDeg
 #define RadToDeg(x) (float)((x)*(180.0/M_PI))
+#endif
 
 #endif
