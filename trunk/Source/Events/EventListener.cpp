8f54caef750542c1f8942b8aec10ca894f149552
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2011-09-16 12:25:19 +0000

    Very nasty lifetime-of-singletons bug: an EventListener may outlive the EventPoller, so we can't remove this event listener in the dtor!

--- trunk/Source/Events/EventListener.cpp
+++ trunk/Source/Events/EventListener.cpp
@@ -5,10 +5,13 @@ namespace Amju
 {
 EventListener::~EventListener()
 {
+  // TODO The event poller may be dead now
+/*
   // Make sure this Listener is removed from the Poller's list!
   if (TheEventPoller::Instance()->HasListener(this))
   {
     TheEventPoller::Instance()->RemoveListener(this);
   }
+*/
 }
 }
