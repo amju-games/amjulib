28e269f2d91e638140676178d4126c7f55759338
jason.e.colman <jason.e.colman@gmail.com>
2011-10-06 15:28:29 +0000

    Main function for iphone console build

--- Source/Main/main.h
+++ Source/Main/main.h
@@ -4,10 +4,17 @@ void StartUp();
 }
 
 #ifdef IPHONE
+
+#ifdef AMJU_CONSOLE
+#include <nonglutmain.h>
+#else
+
 #include "iPhone/Classes/EAGLView.mm"
 #include "iPhone/Classes/ES1Renderer.mm"
 #include "iPhone/Classes/iPhoneAppDelegate.mm"
 #include "iPhone/main.mm"
+
+#endif // AMJU_CONSOLE
 #else // IPHONE
 
 #ifdef AMJU_USE_GLUT
