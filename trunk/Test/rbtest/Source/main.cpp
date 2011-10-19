01c74c0d5fc4aee01a4eb075634eb10c11151ea3
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2011-10-19 17:48:01 +0000

    Experimenting with rbtest, nothing major

--- trunk/Test/rbtest/Source/main.cpp
+++ trunk/Test/rbtest/Source/main.cpp
@@ -1,3 +1,13 @@
+#ifdef WIN32
+#if defined(_DEBUG)
+#pragma comment(lib, "../../../../../../Build/Debug/AmjuLibMsvc.lib")
+#else
+#pragma comment(lib, "../../../../../../Build/Release/AmjuLibMsvc.lib")
+#endif 
+#endif // WIN32
+
+#define AMJU_USE_OPENGL
+
 #include <main.h>
 #include <AmjuGLWindowInfo.h>
 #include <Game.h>
