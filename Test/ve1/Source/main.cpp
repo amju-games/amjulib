950b2e0a686c6e252478d170f80afc1fffbce5b0
jason.e.colman <jason.e.colman@gmail.com>
2011-12-05 14:42:49 +0000

    Set resource loaders for Mesh and Font (TODO why not set by default)

--- Test/ve1/Source/main.cpp
+++ Test/ve1/Source/main.cpp
@@ -11,15 +11,27 @@
 #include <main.h>
 #include <AmjuGLWindowInfo.h>
 #include <Game.h>
+#include <ResourceManager.h>
+#include <ObjMesh.h>
+#include <Font.h>
 #include "GSMain.h"
 
 namespace Amju
 {
+void ReportError(const std::string& str)
+{
+  std::cout << "ERROR: " << str << "\n";
+}
+
 // Create global variable window info 
 Amju::AmjuGLWindowInfo w(640, 480, false);
 
 void StartUp()
 {
+  TheResourceManager::Instance()->AddLoader("font", FontLoader);
+  // Use Text obj loader for now
+  TheResourceManager::Instance()->AddLoader("obj", TextObjLoader);
+
   TheGame::Instance()->SetCurrentState(
     TheGSMain::Instance()
   );
