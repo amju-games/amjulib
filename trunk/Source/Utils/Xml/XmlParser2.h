c4c328161f20364aaa805f25bd543896501072b5
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2011-12-05 18:53:28 +0000

    MSVC 2010 fixes

--- trunk/Source/Utils/Xml/XmlParser2.h
+++ trunk/Source/Utils/Xml/XmlParser2.h
@@ -68,7 +68,7 @@
 #endif
 
 // uncomment the next line if you want no support for wchar_t* (no need for the <wchar.h> or <tchar.h> libraries anymore to compile)
-//#define XML_NO_WIDE_CHAR
+#define XML_NO_WIDE_CHAR
 
 #ifdef XML_NO_WIDE_CHAR
 #undef _XMLWINDOWS
