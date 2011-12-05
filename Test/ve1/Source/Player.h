2b50a04c3756ce318aebc352081db4dc81899a5d
jason.e.colman <jason.e.colman@gmail.com>
2011-12-05 14:35:37 +0000

    Add animated character

--- Test/ve1/Source/Player.h
+++ Test/ve1/Source/Player.h
@@ -5,19 +5,26 @@
 
 namespace Amju
 {
+class Animated;
+
 class Player : public GameObject
 {
 public:
   static const char* TYPENAME;
 
+  Player();
   virtual const char* GetTypeName() const { return TYPENAME; }
   virtual void Update();
+  virtual void Draw();
+  virtual bool Load(File*);
+ 
   bool Init();
 
   void MoveTo(const Vec3f& newPos) { m_newPos = newPos; }
 
 protected:
   Vec3f m_newPos;
+  Animated* m_sceneNode;
 };
 
 }
