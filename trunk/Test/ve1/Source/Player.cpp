8a5bd7c960c69d1f6c841527c39dd7ec6b6c2f3a
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2011-12-05 14:35:37 +0000

    Add animated character

--- trunk/Test/ve1/Source/Player.cpp
+++ trunk/Test/ve1/Source/Player.cpp
@@ -3,10 +3,12 @@
 #include <Teapot.h>
 #include "Ve1SceneGraph.h"
 #include <iostream>
+#include "BlinkCharacter.h"
+#include <File.h>
 
 namespace Amju
 {
-class PlayerSceneNode : public SceneNode
+class PlayerSceneNode : public BlinkCharacter
 {
 public:
   PlayerSceneNode(Player* p) : m_player(p)
@@ -15,12 +17,16 @@ public:
 
   virtual void Draw()
   {
+    BlinkCharacter::Draw();
+
+/*
     AmjuGL::PushMatrix();
     Vec3f pos = m_player->GetPos();
     AmjuGL::Translate(pos.x, pos.y, pos.z);
     static Teapot tp;
     tp.Draw();
     AmjuGL::PopMatrix();
+*/
   }
 
 protected:
@@ -29,16 +35,61 @@ protected:
 
 const char* Player::TYPENAME = "player";
 
-bool Player::Init()
+Player::Player() : m_sceneNode(0)
+{
+}
+
+bool Player::Load(File* f)
 {
   SceneNode* root = GetVe1SceneGraph()->GetRootNode(SceneGraph::AMJU_OPAQUE);
-  root->AddChild(new PlayerSceneNode(this));
+  PlayerSceneNode* psn = new PlayerSceneNode(this);
+  m_sceneNode = psn;
+  
+  std::string meshName;
+  if (!f->GetDataLine(&meshName))
+  {
+    f->ReportError("No mesh name for player");
+    return false;
+  }
 
+  // Load mesh and textures from file
+  if (!psn->LoadMd2(meshName))
+  {
+    return false;
+  }
+
+  std::string tex1Name, tex2Name;
+  if (!f->GetDataLine(&tex1Name) || !f->GetDataLine(&tex2Name))
+  {
+    f->ReportError("Failed to get 2 textures for player");
+    return false;
+  }
+
+  if (!psn->LoadTextures(tex1Name, tex2Name))
+  {
+    return false;
+  }
+
+  root->AddChild(psn);
+
+std::cout << "@£$£@ Loaded player ok!\n";
   return true;
+}
+
+void Player::Draw()
+{
 }
 
 void Player::Update()
 {
+  if (m_sceneNode)
+  {
+    Matrix m;
+    m.Translate(m_pos);
+    m_sceneNode->SetLocalTransform(m);
+    m_sceneNode->Update();
+  }
+
   // Move to new pos ?
 //  float sqd = (GetPos() - m_newPos).SqLen();
   Vec3f vel = m_newPos - GetPos();
@@ -46,6 +97,26 @@ void Player::Update()
 //std::cout << "Vel: " << m_vel.x << ", " << m_vel.y << ", " << m_vel.z << "\n";
 
   GameObject::Update();
+
+  float speed = m_vel.SqLen();
+
+static const float MAX_SPEED = 100.0f; // TODO CONFIG
+static const float RUN_SPEED = MAX_SPEED * 0.5f;
+static const float WALK_SPEED = RUN_SPEED * 0.5f;
+  
+  if (speed > RUN_SPEED)
+  {
+    m_sceneNode->SetAnim("run");
+  }
+  else if (speed > WALK_SPEED)
+  {
+    m_sceneNode->SetAnim("walk");
+  }
+  else
+  {
+    m_sceneNode->SetAnim("stand");
+  }
+
 }
 
 }
