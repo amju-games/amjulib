fa5d5afc2af71062dec5a0283508588d5892c869
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2011-12-06 20:52:12 +0000

    Text edit gui element

--- trunk/Source/Gui/GuiFactory.cpp
+++ trunk/Source/Gui/GuiFactory.cpp
@@ -2,6 +2,7 @@
 #include "GuiButton.h"
 #include "GuiImage.h"
 #include "GuiText.h"
+#include "GuiTextEdit.h"
 #include "GuiComposite.h"
 
 namespace Amju
@@ -9,6 +10,7 @@ namespace Amju
 GuiElement* CreateGuiButton() { return new GuiButton; }
 GuiElement* CreateGuiImage() { return new GuiImage;}
 GuiElement* CreateGuiText() { return new GuiText;}
+GuiElement* CreateGuiTextEdit() { return new GuiTextEdit;}
 GuiElement* CreateGuiComposite() { return new GuiComposite;}
 
 GuiFactory::GuiFactory()
@@ -16,6 +18,7 @@ GuiFactory::GuiFactory()
   Add(GuiButton::NAME, &CreateGuiButton);
   Add(GuiImage::NAME, &CreateGuiImage);
   Add(GuiText::NAME, &CreateGuiText);
+  Add(GuiTextEdit::NAME, &CreateGuiTextEdit);
   Add(GuiComposite::NAME, &CreateGuiComposite);
 }
 }
