752af55ca45990807403707e616a40aeffad538d
jason.e.colman <jason.e.colman@gmail.com>
2011-12-06 20:51:33 +0000

    Text edit gui element

---
+++ Source/Gui/GuiTextEdit.h
@@ -0,0 +1,28 @@
+#ifndef GUI_TEXT_EDIT_H
+#define GUI_TEXT_EDIT_H
+
+#include "GuiText.h"
+
+namespace Amju
+{
+class GuiTextEdit : public GuiText
+{
+public:
+  static const char* NAME;
+
+  GuiTextEdit();
+
+  virtual void Draw();  
+  virtual bool Load(File*); 
+  virtual bool OnKeyEvent(const KeyEvent&); 
+
+  std::string GetText() const;
+
+protected:
+
+  int m_caret; // index of caret: 0 means at left
+  std::string m_myText;
+};
+}
+
+#endif
