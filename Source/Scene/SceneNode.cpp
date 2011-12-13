758f436d4da83e5f9e0356b258b0eeb85f2bbf4c
jason.e.colman <jason.e.colman@gmail.com>
2011-12-13 17:59:01 +0000

    Added SceneNode::DelChild

--- Source/Scene/SceneNode.cpp
+++ Source/Scene/SceneNode.cpp
@@ -120,6 +120,15 @@ void SceneNode::UpdateBoundingVol()
   }
 }
 
+void SceneNode::DelChild(PSceneNode node)
+{
+  Assert(node);
+  node->SetParent(0); // probably not necessary
+  Nodes::iterator it = std::find(m_children.begin(), m_children.end(), node);
+  Assert(it != m_children.end());
+  m_children.erase(it);
+}
+
 void SceneNode::AddChild(PSceneNode node)
 {
   Assert(node);
