4c0c6336d5872e9350e0d3d978190f03ad9b45ed
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2011-12-22 17:14:16 +0000

    Add Pause state

---
+++ trunk/Test/ve1/Source/GSPaused.cpp
@@ -0,0 +1,67 @@
+#include "GSPaused.h"
+#include <AmjuGL.h>
+#include <Game.h>
+#include "GSMain.h"
+#include "GSOptions.h"
+
+namespace Amju
+{
+void OnResumeButton()
+{
+  TheGame::Instance()->SetCurrentState(TheGSMain::Instance());
+}
+
+void OnOptionsButton()
+{
+  TheGame::Instance()->SetCurrentState(TheGSOptions::Instance());
+}
+
+void OnQuitButton()
+{
+  // TODO Quit confirm -- use generic yes/no state
+  exit(0); // TODO Check dtors called -> state saved
+}
+
+GSPaused::GSPaused()
+{
+}
+
+void GSPaused::Update()
+{
+  GSGui::Update();
+
+}
+
+void GSPaused::Draw()
+{
+  GSGui::Draw();
+
+}
+
+void GSPaused::Draw2d()
+{
+  GSGui::Draw2d();
+}
+
+void GSPaused::OnActive()
+{
+  GSGui::OnActive();
+
+  m_gui = LoadGui("gui-paused.txt");
+  Assert(m_gui);
+
+  m_gui->GetElementByName("resume-button")->SetCommand(Amju::OnResumeButton);
+  m_gui->GetElementByName("options-button")->SetCommand(Amju::OnOptionsButton);
+  m_gui->GetElementByName("quit-button")->SetCommand(Amju::OnQuitButton);
+}
+
+bool GSPaused::OnCursorEvent(const CursorEvent& ce)
+{
+  return false;
+}
+
+bool GSPaused::OnMouseButtonEvent(const MouseButtonEvent& mbe)
+{
+  return false;
+}
+} // namespace
