47d6b1c56a00c32e3f6cb522b6eaec840ab3ad45
jason.e.colman <jason.e.colman@gmail.com>
2012-01-04 22:50:42 +0000

    Allow underscores in params

--- Test/ve1/Server/cgi-bin/common.pl
+++ Test/ve1/Server/cgi-bin/common.pl
@@ -28,7 +28,7 @@ print "<?xml version=\"1.0\">\n";
 # From: http://advosys.ca/papers/web/61-web-security.html
 #
 #my $ok_chars = 'a-zA-Z0-9 ,-';
-my $ok_chars = 'a-zA-Z0-9\.@';
+my $ok_chars = 'a-zA-Z0-9\.@_';
 foreach my $param_name ( param() ) 
 {
     $_ = HTML::Entities::decode( param($param_name) );
