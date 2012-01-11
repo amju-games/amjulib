3deb8280c7b430463b7607ba7542b8dd51f21990
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-01-11 17:59:51 +0000

    Tidy up

--- trunk/Test/rbtest/Source/RBManager.cpp
+++ trunk/Test/rbtest/Source/RBManager.cpp
@@ -17,9 +17,6 @@ void RBManager::Clear()
 float RBManager::GetDt() const
 {
   return TheTimer::Instance()->GetDt();
-/*
-  return 0.0002f; // TODO TEMP TEST
-*/
 }
 
 bool RBManager::AddRBFunc(RB::TypeName tn1, RB::TypeName tn2, RBFunc f)
