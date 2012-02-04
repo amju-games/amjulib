f8623da8e2791f0d4d24a90423697d4854f1ef42
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-02-04 21:35:02 +0000

    Add MessageQueue -- but needs work

---
+++ trunk/Source/Game/MessageQueue.cpp
@@ -0,0 +1,35 @@
+#include "MessageQueue.h"
+#include <Timer.h>
+
+namespace Amju
+{
+void MessageQueue::Update()
+{
+  // Update current time
+//  float dt = TheTimer::Instance()->GetDt();
+//  m_time += dt;
+
+  // Execute all messages whose timestamps have expired
+  while (!m_q.empty())
+  {
+    PMessage pm =  *(m_q.begin());
+
+    if (pm->m_time < m_time)
+    {
+      pm->Execute();
+      m_q.erase(m_q.begin());
+    }
+    else
+    {
+      break;
+    }
+  }
+}
+
+void MessageQueue::Add(PMessage m)
+{
+  m_q.insert(m);
+}
+
+}
+
