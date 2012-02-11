83b45db29882f4d2a37852cbde3d09d739148ac8
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-02-11 19:35:33 +0000

    Added functino to launch URL in default browser. Also intended to launch processes.

---
+++ trunk/Source/Utils/Launcher.h
@@ -0,0 +1,18 @@
+/*
+Amju Games source code (c) Copyright Jason Colman 2004
+$Log: Launcher.h,v $
+Revision 1.1  2004/09/08 15:43:19  jay
+Added to repository
+  
+*/
+
+#if !defined(AMJU_UTILS_LAUNCHER_H_INCLUDED)
+#define AMJU_UTILS_LAUNCHER_H_INCLUDED
+
+namespace Amju
+{
+bool Launch(const char* url);
+}
+
+#endif
+
