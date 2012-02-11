86df539e2f100c7b1270606841a88cd192c48c85
jason.e.colman <jason.e.colman@gmail.com>
2012-02-11 20:05:48 +0000

    Game modes

--- Test/ve1/Source/GSStartMenu.cpp
+++ Test/ve1/Source/GSStartMenu.cpp
@@ -1,19 +1,41 @@
 #include "GSStartMenu.h"
 #include <AmjuGL.h>
 #include <Game.h>
-#include "GSLogin.h"
+#include "GSChoosePlayer.h"
 #include "GSStartGame.h"
+#include "GameMode.h"
 
 namespace Amju
 {
+void OnNoGameButton()
+{
+  SetGameMode(AMJU_MODE_NO_GAME); 
+  //TheGame::Instance()->SetCurrentState(TheGSLogin::Instance());
+  TheGame::Instance()->SetCurrentState(TheGSChoosePlayer::Instance());
+}
+
+void OnLocalButton()
+{
+  // Local mode -- no server connection
+  SetGameMode(AMJU_MODE_LOCAL); 
+  //TheGame::Instance()->SetCurrentState(TheGSStartGame::Instance());
+  TheGame::Instance()->SetCurrentState(TheGSChoosePlayer::Instance());
+}
+
 void OnSingleButton()
 {
-  TheGame::Instance()->SetCurrentState(TheGSStartGame::Instance());
+  // TODO Still connect to server to log activity and get run time content.
+  // Set single player flag.
+  SetGameMode(AMJU_MODE_SINGLE); 
+  //TheGame::Instance()->SetCurrentState(TheGSLogin::Instance());
+  TheGame::Instance()->SetCurrentState(TheGSChoosePlayer::Instance());
 }
 
 void OnMultiButton()
 {
-  TheGame::Instance()->SetCurrentState(TheGSLogin::Instance());
+  SetGameMode(AMJU_MODE_MULTI); 
+  //TheGame::Instance()->SetCurrentState(TheGSLogin::Instance());
+  TheGame::Instance()->SetCurrentState(TheGSChoosePlayer::Instance());
 }
 
 GSStartMenu::GSStartMenu()
@@ -44,6 +66,8 @@ void GSStartMenu::OnActive()
 
   m_gui->GetElementByName("single-button")->SetCommand(Amju::OnSingleButton);
   m_gui->GetElementByName("multi-button")->SetCommand(Amju::OnMultiButton);
+  m_gui->GetElementByName("nogame-button")->SetCommand(Amju::OnNoGameButton);
+  m_gui->GetElementByName("local-button")->SetCommand(Amju::OnLocalButton);
 }
 
 bool GSStartMenu::OnCursorEvent(const CursorEvent& ce)
