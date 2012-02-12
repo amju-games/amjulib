6eb076980dff7c575e4e57231435ab8c266a91d0
jason.e.colman <jason.e.colman@gmail.com>
2012-02-12 13:44:41 +0000

    First checkin of MFC-based auto updater

---
+++ Test/ve1/Updater/WinMFC/My Game/My GameDlg.h
@@ -0,0 +1,38 @@
+
+// My GameDlg.h : header file
+//
+
+#pragma once
+
+
+// CMyGameDlg dialog
+class CMyGameDlg : public CDialogEx
+{
+// Construction
+public:
+	CMyGameDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_MYGAME_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnClickedButton1();
+//	afx_msg void OnUpdateStatic(CCmdUI *pCmdUI);
+//	afx_msg void OnStatic();
+//	afx_msg void OnIdbPng1();
+//	afx_msg void OnUpdateIdbPng1(CCmdUI *pCmdUI);
+	CString m_text;
+};
