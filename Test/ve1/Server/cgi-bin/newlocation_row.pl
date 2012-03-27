1aa45c39827b607c7a5c109db2421715ff1276f5
jason.e.colman <jason.e.colman@gmail.com>
2012-03-27 16:42:21 +0000

    Updated scripts, for adding new locations from client

---
+++ Test/ve1/Server/cgi-bin/newlocation_row.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+
+require "common.pl";
+
+
+my $loc_id = param('loc_id') or die "Expected location ID";
+my $assetFile = param('asset_file') or die "Expected asset file name for location";
+my $dataFile = param('data_file') or die "Expected data file name for location";
+
+my_connect();
+
+# Add row to object table and objectpos
+$sql = "insert into object (type, assetfile, datafile, owner, createtime) values ('terrain', '$assetFile', '$dataFile', 0, now() ) ";
+insert($sql);
+
+$sql = "insert into objectpos (id, x, y, z, loc, whenchanged) values (LAST_INSERT_ID(), 0, 0, 0, $loc_id, now())";
+insert($sql);
+
+$sql = "update location set obj_id = LAST_INSERT_ID(), whenchanged = now() where id=$loc_id";
+update($sql);
+
+
+if (check_session() == 0)
+{
+  return;
+}
+
+print "OK!\n";
+
