f77510a23f999d2f2d296f0ce00b6acf7937d3a3
jason.e.colman <jason.e.colman@gmail.com>
2012-03-27 16:39:47 +0000

    Add test for GUI classes

---
+++ Test/guitest/Source/GSGuiTest.cpp
@@ -0,0 +1,80 @@
+#include "GSGuiTest.h"
+#include <iostream>
+#include <AmjuGL.h>
+
+namespace Amju
+{
+GSGuiTest::GSGuiTest()
+{
+}
+
+void GSGuiTest::Update()
+{
+  GSGui::Update();
+
+}
+
+void GSGuiTest::Draw()
+{
+  AmjuGL::SetClearColour(Colour(0, 0, 1, 1));
+  GSGui::Draw();
+}
+
+void GSGuiTest::Draw2d()
+{
+  if (m_menu)
+  {
+    m_menu->Draw();
+  }
+
+  GSGui::Draw2d(); // Draw cursor last
+}
+
+void OnOK()
+{
+  std::cout << "OK Button pressed.\n";
+}
+
+void GSGuiTest::OnActive()
+{
+  GSGui::OnActive();
+
+  m_gui = LoadGui("gui-test.txt");
+  Assert(m_gui);
+
+  m_gui->GetElementByName("ok-button")->SetCommand(OnOK);
+}
+
+bool GSGuiTest::OnCursorEvent(const CursorEvent& ce)
+{
+  return false;
+}
+
+void OnChoice()
+{
+  std::cout << "Menu choice selected!\n";
+}
+
+bool GSGuiTest::OnMouseButtonEvent(const MouseButtonEvent& mbe)
+{
+  if (mbe.button == AMJU_BUTTON_MOUSE_RIGHT && mbe.isDown)
+  {
+    m_menu = new GuiMenu;
+    m_menu->SetPos(Vec2f(mbe.x, mbe.y));
+    
+    m_menu->AddItem(new GuiMenuItem("Choice one", OnChoice));
+    m_menu->AddItem(new GuiMenuItem("Choice two", OnChoice));
+
+    GuiMenu* childMenu = new GuiMenu;
+    childMenu->SetName("Child menu");
+
+    // Get types from Game Object Factory..?
+    childMenu->AddItem(new GuiMenuItem("Choice three", OnChoice));
+    // TODO Other types
+
+    m_menu->AddItem(new GuiNestMenuItem("Nested menu >", childMenu));
+  }
+
+  return false;
+}
+} // namespace
---
+++ Test/guitest/Source/main.cpp
@@ -0,0 +1,40 @@
+#ifndef IPHONE
+
+#ifdef WIN32
+#if defined(_DEBUG)
+#pragma comment(lib, "../../../../../../Build/Debug/AmjuLibMsvc.lib")
+#else
+#pragma comment(lib, "../../../../../../Build/Release/AmjuLibMsvc.lib")
+#endif 
+#endif // WIN32
+
+#define AMJU_USE_OPENGL
+
+#include <main.h>
+#include <AmjuGLWindowInfo.h>
+#include <Game.h>
+#include <CursorManager.h>
+#include <Font.h>
+#include "GSGuiTest.h"
+
+namespace Amju
+{
+// Create global variable window info 
+Amju::AmjuGLWindowInfo w(640, 480, false);
+
+void ReportError(const std::string& str)
+{
+  std::cout << str << "\n";
+}
+
+void StartUp()
+{
+  TheCursorManager::Instance()->Load(Vec2f(0.025f, -0.08f));
+  TheResourceManager::Instance()->AddLoader("font", FontLoader);
+
+  TheGame::Instance()->SetCurrentState(TheGSGuiTest::Instance());
+}
+}
+
+#endif // IPHONE
+
