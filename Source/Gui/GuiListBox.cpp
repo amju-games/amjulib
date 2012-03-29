b2e7bbae1e2ce22bc3459ec7a8764608529e0b19
jason.e.colman <jason.e.colman@gmail.com>
2012-03-29 15:16:10 +0000

    Sets viewport - TODO make GuiWindow/GuiScrollable classes

--- Source/Gui/GuiListBox.cpp
+++ Source/Gui/GuiListBox.cpp
@@ -20,9 +20,14 @@ void GuiListBox::Draw()
   int vp[4];
   AmjuGL::GetViewport(&vp[0], &vp[1], &vp[2], &vp[3]);
 
-  SetViewportN(m_pos.x, m_pos.y, m_size.x, m_size.y);
-  GuiComposite::Draw();
+  SetViewportN(m_pos.x, m_pos.y -  m_size.y, m_size.x, m_size.y);
+  // LB should fill whole viewport
+  AmjuGL::PushMatrix();
+  AmjuGL::Translate(m_pos.x * -2.0f / m_size.x - 1.0f, 1.0f - m_pos.y * 2.0f / m_size.y, 0);
+  AmjuGL::Scale(2.0f / m_size.x, 2.0f / m_size.y, 1.0f);
 
+  GuiComposite::Draw();
+  AmjuGL::PopMatrix();
   AmjuGL::Viewport(vp[0], vp[1], vp[2], vp[3]);
 }
 
