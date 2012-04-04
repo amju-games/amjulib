31f9121c9b7749e9cb574d8b92df261d98c6b350
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-04-04 17:12:48 +0000

    Simplify/fix config file access

---
+++ trunk/Test/ve1/Source/SaveConfig.cpp
@@ -0,0 +1,30 @@
+#include "SaveConfig.h"
+#include <ConfigFile.h>
+#include <Directory.h>
+
+namespace Amju
+{
+static const char* CONFIG_FILE_NAME = "game.cfg";
+
+std::string GetConfigFilename()
+{
+  // File game.cfg in same dir as executable
+
+  static std::string configFileName = GetProcessDir() + "/" + CONFIG_FILE_NAME;
+  return configFileName;
+}
+
+void LoadConfig()
+{
+  TheGameConfigFile::Instance()->Load(GetConfigFilename(), false);
+  atexit(SaveConfig);
+}
+
+void SaveConfig()
+{
+  // TODO call on exit
+
+  TheGameConfigFile::Instance()->Save(GetConfigFilename(), false);
+}
+}
+
