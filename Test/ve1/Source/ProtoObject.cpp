5134a2ceb4a5c15104044f2867df0befdb9c06a6
jason.e.colman <jason.e.colman@gmail.com>
2012-04-06 13:40:58 +0000

    Display object info

--- Test/ve1/Source/ProtoObject.cpp
+++ Test/ve1/Source/ProtoObject.cpp
@@ -2,6 +2,8 @@
 #include "ProtoObject.h"
 #include <SceneNode.h>
 #include <DrawAABB.h>
+#include <Unproject.h>
+#include <GuiText.h>
 #include "Ve1SceneGraph.h"
 
 namespace Amju
@@ -16,16 +18,48 @@ public:
     AmjuGL::Disable(AmjuGL::AMJU_TEXTURE_2D);
 
     PushColour();
-    SetColour(Colour(1, 0, 0, 1));
+    MultColour(Colour(1, 0, 0, 1));
+
+    Vec2f screenpos;
+    Vec3f pos = m_obj->GetPos();
+    Project(pos, &screenpos);
 
     Assert(m_obj->GetAABB());
     DrawSolidAABB(*(m_obj->GetAABB()));
-    PopColour();
     DrawAABB(*(m_obj->GetAABB()));
 
     AmjuGL::Enable(AmjuGL::AMJU_TEXTURE_2D);
 
     // TODO Draw info
+    AmjuGL::Disable(AmjuGL::AMJU_LIGHTING);
+    AmjuGL::Enable(AmjuGL::AMJU_BLEND);
+    AmjuGL::Disable(AmjuGL::AMJU_DEPTH_READ);
+
+    AmjuGL::PushMatrix();
+    AmjuGL::SetIdentity();
+
+    AmjuGL::SetMatrixMode(AmjuGL::AMJU_PROJECTION_MATRIX);
+    AmjuGL::PushMatrix();
+    AmjuGL::SetIdentity();
+ 
+    MultColour(Colour(0, 0, 1, 1));
+    GuiText text;
+    text.SetTextSize(0.5f);
+    std::string s = "Object " + ToString(m_obj->GetId()) + " " + m_obj->GetTypeName();
+    text.SetText(s);
+    text.SizeToText(); 
+    text.SetJust(GuiText::AMJU_JUST_LEFT);
+    text.SetPos(screenpos);
+    text.Draw();
+
+    PopColour();
+
+    AmjuGL::PopMatrix();
+    AmjuGL::SetMatrixMode(AmjuGL::AMJU_MODELVIEW_MATRIX);
+    AmjuGL::PopMatrix();
+
+    AmjuGL::Enable(AmjuGL::AMJU_DEPTH_READ);
+    AmjuGL::Disable(AmjuGL::AMJU_BLEND);
   }
 
 private:
