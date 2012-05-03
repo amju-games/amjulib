6fa716498f187bcb2b2101c4c16b980fce065c88
jason.e.colman <jason.e.colman@gmail.com>
2012-05-03 18:28:17 +0000

    Improved login script, and added log out script, so we can tell when a player drops out of the game.

--- Test/ve1/Server/cgi-bin/login.pl
+++ Test/ve1/Server/cgi-bin/login.pl
@@ -31,12 +31,12 @@ print "Query: $sql\n";
 
   if (my ($player_id) = $query->fetchrow_array)
   {
-    # Delete old session
-    $sql = "delete from session where player_id=$player_id";
-print "Query: $sql\n";
-    $query = $dbh->prepare($sql) or die
-      "Query prepare failed for this query: $sql\n";
-    $query->execute;
+    # DON'T Delete old session
+    #$sql = "delete from session where player_id=$player_id";
+    #print "Query: $sql\n";
+    #$query = $dbh->prepare($sql) or die
+    #  "Query prepare failed for this query: $sql\n";
+    #$query->execute;
 
     # Create new session
     # Session lasts 24 hours..?
@@ -44,7 +44,7 @@ print "Query: $sql\n";
     insert($sql);
 
     # Get session ID for this player and start location
-    $sql = "select now(), a.id, b.playername, b.obj_id, c.loc from session as a, player as b, objectpos as c where a.player_id=$player_id and a.player_id=b.id and b.obj_id=c.id";
+    $sql = "select now(), a.id, b.playername, b.obj_id, c.loc from session as a, player as b, objectpos as c where a.player_id=$player_id and a.player_id=b.id and b.obj_id=c.id and a.id=LAST_INSERT_ID()";
 
 print "Query: $sql\n";
 
@@ -57,6 +57,10 @@ print "Query: $sql\n";
 print "Your new session ID: $session_id\n";
 
       print "<now>$now</now> <session>$session_id</session><playername>$playername</playername><objid>$objid</objid><loc>$loc</loc>\n";
+   
+      # Set logged in flag as object value 
+      $sql = "insert into objectstate (`id`, `key`, `val`) select player.obj_id, 'loggedin', 'y' from player, session where player.id = session.player_id and session.id=$session_id on duplicate key update val='y'";
+      insert($sql);
     }
     else
     {
