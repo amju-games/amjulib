b3f11760c9946a6bc5d5d5898b5cf78db691e2e2
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-05-09 18:20:17 +0000

    Fixes due to changes in Gui module

--- trunk/Test/guitest/Source/GSGuiTest.cpp
+++ trunk/Test/guitest/Source/GSGuiTest.cpp
@@ -72,19 +72,19 @@ bool GSGuiTest::OnMouseButtonEvent(const MouseButtonEvent& mbe)
   if (mbe.button == AMJU_BUTTON_MOUSE_RIGHT && mbe.isDown)
   {
     m_menu = new GuiMenu;
-    m_menu->SetPos(Vec2f(mbe.x, mbe.y));
+    m_menu->SetLocalPos(Vec2f(mbe.x, mbe.y));
     
-    m_menu->AddItem(new GuiMenuItem("Choice one", OnChoice));
-    m_menu->AddItem(new GuiMenuItem("Choice two", OnChoice));
+    m_menu->AddChild(new GuiMenuItem("Choice one", OnChoice));
+    m_menu->AddChild(new GuiMenuItem("Choice two", OnChoice));
 
     GuiMenu* childMenu = new GuiMenu;
     childMenu->SetName("Child menu");
 
     // Get types from Game Object Factory..?
-    childMenu->AddItem(new GuiMenuItem("Choice three", OnChoice));
+    childMenu->AddChild(new GuiMenuItem("Choice three", OnChoice));
     // TODO Other types
 
-    m_menu->AddItem(new GuiNestMenuItem("Nested menu >", childMenu));
+    m_menu->AddChild(new GuiNestMenuItem("Nested menu >", childMenu));
   }
 
   return false;
