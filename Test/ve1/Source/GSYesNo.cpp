bc53e05d17a76122f41cc683f6520f2970bf360c
jason.e.colman <jason.e.colman@gmail.com>
2012-05-09 18:07:09 +0000

    Add confirm GUI states

---
+++ Test/ve1/Source/GSYesNo.cpp
@@ -0,0 +1,64 @@
+#include <Game.h>
+#include <GuiButton.h>
+#include <GuiText.h>
+#include "GSYesNo.h"
+
+namespace Amju
+{
+static void OnYes()
+{
+  GSYesNo* yn = dynamic_cast<GSYesNo*>(TheGame::Instance()->GetState());
+  Assert(yn);
+  yn->OnYes();
+}
+
+static void OnNo()
+{
+  GSYesNo* yn = dynamic_cast<GSYesNo*>(TheGame::Instance()->GetState());
+  Assert(yn);
+  yn->OnNo();
+}
+
+GSYesNo::GSYesNo()
+{
+}
+
+void GSYesNo::Update()
+{
+  GSGui::Update();
+
+}
+
+void GSYesNo::Draw()
+{
+  GSGui::Draw();
+
+}
+
+void GSYesNo::Draw2d()
+{
+  GSGui::Draw2d();
+}
+
+void GSYesNo::OnActive()
+{
+  GSGui::OnActive();
+
+  m_gui = LoadGui("gui-yesno.txt");
+  Assert(m_gui);
+
+  GuiButton* yes = (GuiButton*)GetElementByName(m_gui, "yes-button");
+  yes->SetCommand(Amju::OnYes);
+  yes->SetHasFocus(true);
+  yes->SetText(m_yesText);
+
+  GuiButton* no = (GuiButton*)GetElementByName(m_gui, "no-button");
+  no->SetCommand(Amju::OnNo);
+  no->SetIsCancelButton(true); 
+  no->SetText(m_noText);
+  
+  GuiText* q = (GuiText*)GetElementByName(m_gui, "question");
+  q->SetText(m_question);
+}
+
+} // namespace
---
+++ Test/ve1/Source/GSYesNoQuitProcess.cpp
@@ -0,0 +1,24 @@
+#include "GSYesNoQuitProcess.h"
+
+namespace Amju
+{
+GSYesNoQuitProcess::GSYesNoQuitProcess()
+{
+  m_question = "Do you really want to quit ?";
+  m_yesText = "quit";
+  m_noText = "cancel";
+}
+
+void GSYesNoQuitProcess::OnYes()
+{
+  exit(0);
+}
+
+void GSYesNoQuitProcess::OnNo()
+{
+  GoBack();
+}
+
+}
+
+
