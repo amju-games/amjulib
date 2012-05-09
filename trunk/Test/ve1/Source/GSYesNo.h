34c72a5d859dd9d4d734cd4c12b2ef1dfd301982
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-05-09 18:07:09 +0000

    Add confirm GUI states

---
+++ trunk/Test/ve1/Source/GSYesNo.h
@@ -0,0 +1,26 @@
+#ifndef GS_YESNO_H_INCLUDED
+#define GS_YESNO_H_INCLUDED
+
+#include "GSGui.h"
+
+namespace Amju 
+{
+class GSYesNo : public GSGui
+{
+public:
+  GSYesNo();
+  virtual void Update();
+  virtual void Draw();
+  virtual void Draw2d();
+  virtual void OnActive();
+
+  virtual void OnYes() = 0;
+  virtual void OnNo() = 0;
+
+protected:
+  std::string m_question;
+  std::string m_yesText;
+  std::string m_noText;
+};
+} // namespace
+#endif
