f90daf55beb247046277fef46afa2ec6059e278f
jason.e.colman <jason.e.colman@gmail.com>
2012-05-14 17:59:22 +0000

    Changes so we can have NPCs

---
+++ Test/ve1/Source/Baddie.h
@@ -0,0 +1,25 @@
+#ifndef BADDIE_H_INCLUDED
+#define BADDIE_H_INCLUDED
+
+#include "Ve1ObjectChar.h"
+
+namespace Amju
+{
+// NPCs which are obstacles in the game. Players can battle NPCs. If defeated, the Baddie
+//  can give a rewards to the player.
+
+class Baddie : public Ve1ObjectChar
+{
+public:
+  static const char* TYPENAME;
+
+  virtual const char* GetTypeName() const;
+  virtual void Update();
+  virtual bool Load(File*);
+  virtual void OnLocationEntry();
+  virtual void OnLocationExit();
+};
+}
+
+#endif
+
