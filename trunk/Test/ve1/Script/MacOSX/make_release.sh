a03d1f4498509385940b8a3fddbda2db77d3d17e
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-06-06 12:28:45 +0000

    Release script to make universal binary

---
+++ trunk/Test/ve1/Script/MacOSX/make_release.sh
@@ -0,0 +1,49 @@
+export VERSION=0.2
+
+export AMJU_BUILD_ARCH=Mac-i386
+export AMJU_MACHDEP='-arch i386 -DMACOSX -DXP_MACOSX -D__i386__'
+pushd ../../../../Script/MacOSX
+make
+popd
+make
+
+export AMJU_BUILD_ARCH=Mac-ppc
+export AMJU_MACHDEP='-arch ppc -DMACOSX -DXP_MACOSX'
+pushd ../../../../Script/MacOSX
+make
+popd
+make
+
+# TODO Make fat binary
+export BUILD=../../../../Build/ve1/
+
+lipo -create $(BUILD)/Mac-i386/ve1 $(BUILD)/Mac-ppc/ve1 -output ve1.univ
+strip ve1.univ
+
+# Make app bundle, put universal binary in 
+
+rm -rf $(BUILD)/"My Game.app"
+cp -R "My Game.app" $(BUILD)
+mkdir -p $(BUILD)/"My Game.app"/Contents/Resources/Data
+mkdir -p $(BUILD)/"My Game.app"/Contents/Resources/Data/font2d
+mkdir -p $(BUILD)/"My Game.app"/Contents/Resources/Data/font3d
+cp -R $(ASSETSDIR)/* $(BUILD)/"My Game.app"/Contents/Resources/Data/
+cp -R $(FONT2D)/* $(BUILD)/"My Game.app"/Contents/Resources/Data/font2d
+cp -R $(FONT3D)/* $(BUILD)/"My Game.app"/Contents/Resources/Data/font3d
+cp ve1.univ $(BUILD)/"My Game.app"/Contents/MacOS/"My Game"
+cd $(BUILD)
+
+# Zip bundle
+rm "My Game.zip"
+zip -9 "My Game.zip" "My Game.app"
+zip -9 "My Game.zip" "My Game.app/Contents"
+zip -9 "My Game.zip" "My Game.app/Contents/Info.plist"
+zip -9 "My Game.zip" "My Game.app/Contents/MacOS/My Game"
+zip -9 "My Game.zip" "My Game.app/Contents/Resources"
+zip -9 -r "My Game.zip" "My Game.app/Contents/Resources/Data"
+zip -9 "My Game.zip" "My Game.app/Contents/Resources/MyGame.icns"
+zip -9 "My Game.zip" "My Game.app/Contents/Resources/English.lproj"
+zip -9 "My Game.zip" "My Game.app/Contents/Resources/English.lproj/InfoPlist.strings"
+mv "My Game.zip" "MyGame-v."$(VERSION)".zip"
+
+
