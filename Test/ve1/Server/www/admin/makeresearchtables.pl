ac64f6dfca89d56dfa7a1983763444b6d228ac70
jason.e.colman <jason.e.colman@gmail.com>
2012-06-23 10:43:22 +0000

    Add more tables to hold data for research project

--- Test/ve1/Server/www/admin/makeresearchtables.pl
+++ Test/ve1/Server/www/admin/makeresearchtables.pl
@@ -13,6 +13,8 @@ print "That's it!\n";
 
 sub create_table_position()
 {
+  drop_table("research_objectpos");
+
   my $sql = <<END;
 
 CREATE TABLE `research_objectpos`
@@ -28,16 +30,102 @@ END
 
   update_or_insert($sql);
 
-  print "Created object table! $sql\n<br>\n";
+  print "Created research_objectpos table! $sql\n<br>\n";
 }
+  
+sub create_table_cogtest()
+{
+  drop_table("research_cogtest");
+  drop_table("research_testresult");
+
+  my $sql = <<END;
+
+CREATE TABLE `research_cogtest`
+(`id` INT NOT NULL,
+`playerid` INT NOT NULL,
+`whentaken` TIMESTAMP NOT NULL,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`playerid`) REFERENCES player(`id`)
+) ENGINE = MYISAM;
+END
+
+  update_or_insert($sql);
+
+  print "Created research_cogtest table! $sql\n<br>\n";
+
+  $sql = <<END;
+
+CREATE TABLE `research_testresult`
+(`testid` INT NOT NULL,
+`key` VARCHAR(30) NOT NULL,
+`val` VARCHAR(30) NOT NULL,
+FOREIGN KEY (`testid`) REFERENCES research_cogtest(`id`)
+) ENGINE = MYISAM;
+END
+
+  update_or_insert($sql);
+
+  print "Created research_testresult table! $sql\n<br>\n";
+
+}
+
+sub create_table_action()
+{
+  # Tables to record all player actions - this may be overkill..
+
+  drop_table("research_action"); # one row per player-initiated action
+  drop_table("research_action_type"); # describes action types
+  drop_table("research_action_info"); # rows for additional info for actions
+
+  my $sql = <<END;
+
+CREATE TABLE `research_action_type`
+(`id` INT NOT NULL,
+`name` VARCHAR(30) NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE = MYISAM;
+END
+
+  update_or_insert($sql);
+
+  $sql = <<END;
+
+CREATE TABLE `research_action`
+(`id` INT NOT NULL,
+`actiontype` INT NOT NULL,
+`playerid` INT NOT NULL,
+`when` TIMESTAMP NOT NULL,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`playerid`) REFERENCES player(`id`),
+FOREIGN KEY (`actiontype`) REFERENCES research_action_type(`id`)
+) ENGINE = MYISAM;
+END
+
+  update_or_insert($sql);
+
+  $sql = <<END;
+
+CREATE TABLE `research_action_info`
+(`actionid` INT NOT NULL,
+`key` VARCHAR(30) NOT NULL,
+`val` VARCHAR(30) NOT NULL,
+FOREIGN KEY (`actionid`) REFERENCES research_action(`id`)
+) ENGINE = MYISAM;
+END
+
+  update_or_insert($sql);
+}
+
 
 sub create_tables()
 {
-  print "Creating RESEARCH tables for VE1!\n";
+  print "Creating RESEARCH tables for this environment!\n";
 
-  drop_table("research_objectpos");
-  create_table_position();
+#  create_table_position();
 
+  create_table_cogtest();
+
+  create_table_action();
 }
 
 
