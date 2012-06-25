6a88fe9c7fabe011e51879d093b40e6c64ae6c02
jason.e.colman <jason.e.colman@gmail.com>
2012-06-25 21:15:47 +0000

    GSCogTestMenu is no longer a menu, but drives the sequence of cognitive tests which the player must take.

--- Test/ve1/Source/GSCogTestMenu.cpp
+++ Test/ve1/Source/GSCogTestMenu.cpp
@@ -1,42 +1,59 @@
-#include "GSCogTestMenu.h"
 #include <AmjuGL.h>
 #include <Game.h>
-#include "GSStartMenu.h"
+#include <GuiButton.h>
+#include "GSCogTestMenu.h"
+#include "GSFileUpdateCheck.h"
 #include "GSLetterCancellation1.h"
+#include "GSTitle.h"
 
 namespace Amju
 {
 static void OnCancelButton()
 {
-  TheGame::Instance()->SetCurrentState(TheGSStartMenu::Instance());
+  // If we cancel, the session will not be complete so we should have to do the cog tests
+  //  again ? 
+  // No, the participant just skips the test, if that's what they want to do.
+
+  TheGame::Instance()->SetCurrentState(TheGSTitle::Instance());
+  TheGSCogTestMenu::Instance()->ResetTests();
 }
 
-static void OnLetterSigmaButton()
+static void LetterSigmaTest()
 {
   TheGSLetterCancellation1::Instance()->LoadConfig("letter-cancel-sigma.txt");
   TheGame::Instance()->SetCurrentState(TheGSLetterCancellation1::Instance());
 }
 
-static void OnLetterMButton()
+static void LetterMTest()
 {
   TheGSLetterCancellation1::Instance()->LoadConfig("letter-cancel-m.txt");
   TheGame::Instance()->SetCurrentState(TheGSLetterCancellation1::Instance());
 }
 
+static void Done()
+{
+  TheGame::Instance()->SetCurrentState(TheGSFileUpdateCheck::Instance());
+  TheGSCogTestMenu::Instance()->ResetTests();
+}
+
 GSCogTestMenu::GSCogTestMenu()
 {
+  m_nextTest = 0;
+}
+
+void GSCogTestMenu::ResetTests()
+{
+  m_nextTest = 0;
 }
 
 void GSCogTestMenu::Update()
 {
   GSGui::Update();
-
 }
 
 void GSCogTestMenu::Draw()
 {
   GSGui::Draw();
-
 }
 
 void GSCogTestMenu::Draw2d()
@@ -51,11 +68,41 @@ void GSCogTestMenu::OnActive()
   m_gui = LoadGui("gui-cogtestmenu.txt");
   Assert(m_gui);
 
+  // We display some text, which depends on the next test to administer.
+  // The OK button then starts the next test.
   // TODO Set focus element, cancel element, command handlers
-  GetElementByName(m_gui, "cancel-button")->SetCommand(Amju::OnCancelButton);
-  GetElementByName(m_gui, "letter-m-button")->SetCommand(Amju::OnLetterMButton);
-  GetElementByName(m_gui, "letter-sigma-button")->SetCommand(Amju::OnLetterSigmaButton);
+  GuiButton* cancel = (GuiButton*)GetElementByName(m_gui, "cancel-button");
+  cancel->SetCommand(Amju::OnCancelButton);
+  cancel->SetIsCancelButton(true);
 
+  GuiButton* ok = (GuiButton*)GetElementByName(m_gui, "ok-button");
+  ok->SetIsFocusButton(true);
+
+  GuiText* text = (GuiText*)GetElementByName(m_gui, "text1");
+  Assert(text);
+
+  // Set text and OK button command depending on next test to perform...
+  switch (m_nextTest)
+  {
+  case 0:
+    // TODO Use localisation system so we can improve this text.
+
+    text->SetText("Please take some tests before you start. This first one is a letter cancellation test. Click on all the M letters that you see. You have 3 minutes to complete this task.");
+    ok->SetCommand(Amju::LetterMTest);
+    break;
+
+  case 1:
+    text->SetText("In this test, click on all the sigma letters that you see.");
+    ok->SetCommand(Amju::LetterSigmaTest);
+    break;
+
+  // etc
+
+  default:
+    text->SetText("Well done on doing all those tests. Now you can play the game.");
+    ok->SetCommand(Amju::Done);
+  }
+  m_nextTest++;
 }
 
 } // namespace
