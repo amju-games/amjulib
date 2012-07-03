48a272f0a5ed2652815d5ff5922f2ba98e274894
jason.e.colman <jason.e.colman@gmail.com>
2012-07-03 08:10:47 +0000

    Load funcs: don't assert, we lose error msg info

--- Source/GeomLoad/LoadVec2.cpp
+++ Source/GeomLoad/LoadVec2.cpp
@@ -9,11 +9,15 @@ bool LoadVec2(File* f, Vec2f* v)
   std::string s;
   if (!f->GetDataLine(&s))
   {
-    Assert(0);
     return false;
   }
   std::vector<std::string> strs = Split(s, ',');
-  Assert(strs.size() == 2);
+  if (strs.size() != 2)
+  {
+    f->ReportError("Not a Vec2");
+    return false;
+  }
+
   v->x = (float)atof(strs[0].c_str());
   v->y = (float)atof(strs[1].c_str());
   return true;
--- Source/GeomLoad/LoadVec3.cpp
+++ Source/GeomLoad/LoadVec3.cpp
@@ -9,11 +9,15 @@ bool LoadVec3(File* f, Vec3f* v)
   std::string s;
   if (!f->GetDataLine(&s))
   {
-    Assert(0);
     return false;
   }
   Strings strs = Split(s, ',');
-  Assert(strs.size() == 3);
+  if (strs.size() != 3)
+  {
+    f->ReportError("Not a Vec3");
+    return false;
+  }
+
   v->x = (float)atof(strs[0].c_str());
   v->y = (float)atof(strs[1].c_str());
   v->z = (float)atof(strs[2].c_str());
