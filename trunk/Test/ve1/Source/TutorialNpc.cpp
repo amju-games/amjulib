20d841737aaf9a7e95ba60524c60ce094d79d423
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-07-09 15:08:21 +0000

    Add Tutorial framework - TODO hardly started

---
+++ trunk/Test/ve1/Source/TutorialNpc.cpp
@@ -0,0 +1,32 @@
+#include "TutorialNpc.h"
+
+namespace Amju
+{
+const char* TutorialNpc::TYPENAME = "tutorial_npc";
+
+void TutorialNpc::Update()
+{
+}
+
+bool TutorialNpc::Load(File*)
+{
+  return false;
+}
+
+void TutorialNpc::OnLocationEntry()
+{
+}
+
+void TutorialNpc::OnLocationExit()
+{
+}
+
+void TutorialNpc::SetKeyVal(const std::string& key, const std::string& val)
+{
+}
+
+void TutorialNpc::SetMenu(GuiMenu*)
+{
+}
+
+}
