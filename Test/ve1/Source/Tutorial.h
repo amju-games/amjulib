3b9c8a5fb79dbc6d3f7c0397696589406bd1d26b
jason.e.colman <jason.e.colman@gmail.com>
2012-07-09 15:08:21 +0000

    Add Tutorial framework - TODO hardly started

---
+++ Test/ve1/Source/Tutorial.h
@@ -0,0 +1,11 @@
+#ifndef TUTORIAL_H_INCLUDED
+#define TUTORIAL_H_INCLUDED
+
+namespace Amju
+{
+// Shw text in Chat UI when we can.
+void QueueTutorialText(const std::string& tutText);
+}
+
+#endif
+
