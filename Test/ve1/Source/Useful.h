cb50bd992d52bacfa12933edb6c6b6ad54fb4df1
jason.e.colman <jason.e.colman@gmail.com>
2012-07-09 15:11:35 +0000

    Output game objects

--- Test/ve1/Source/Useful.h
+++ Test/ve1/Source/Useful.h
@@ -1,15 +1,23 @@
 #ifndef USEFUL_H_INCLUDED
 #define USEFUL_H_INCLUDED
 
-#include <Vec3.h>
 #include <iostream>
 #include <string>
+#include <Vec3.h>
+#include <GameObject.h>
 
 namespace Amju
 {
-std::ostream& operator<<(std::ostream& os, const Vec3f& v);
+// Useful functions for output
 
+// Vec3
+std::ostream& operator<<(std::ostream& os, const Vec3f& v);
 std::string ToString(const Vec3f& v);
+
+// Game Object
+std::ostream& operator<<(std::ostream& os, const GameObject& go);
+std::string ToString(const GameObject& go);
+
 }
 
 #endif
