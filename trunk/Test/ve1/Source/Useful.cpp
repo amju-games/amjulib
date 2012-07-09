a5f657c9a049addb5a78a94fc81264d2d2648757
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-07-09 15:11:35 +0000

    Output game objects

--- trunk/Test/ve1/Source/Useful.cpp
+++ trunk/Test/ve1/Source/Useful.cpp
@@ -1,5 +1,6 @@
-#include "Useful.h"
+#include <sstream>
 #include <StringUtils.h>
+#include "Useful.h"
 
 namespace Amju
 {
@@ -10,7 +11,24 @@ std::ostream& operator<<(std::ostream& os, const Vec3f& v)
 
 std::string ToString(const Vec3f& v)
 {
-  return std::string("x: ") + ToString(v.x) + std::string(" y: ") + ToString(v.y) + std::string(" z: ") + ToString(v.z);
+  std::stringstream ss;
+  ss << v;
+  return ss.str();
+
+//  return std::string("x: ") + ToString(v.x) + std::string(" y: ") + ToString(v.y) + std::string(" z: ") + ToString(v.z);
 }
+
+std::ostream& operator<<(std::ostream& os, const GameObject& go)
+{
+  return os << "Object " << go.GetId() << " (" << go.GetTypeName() << ")";
+}
+
+std::string ToString(const GameObject& go)
+{
+  std::stringstream ss;
+  ss << go;
+  return ss.str();
+}
+
 }
 
