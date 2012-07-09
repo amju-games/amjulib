3cb3fca7de32c687f04df2ba217591567d10ba58
jason.e.colman <jason.e.colman@gmail.com>
2012-07-09 15:09:12 +0000

    Add more options - keybaord, cfullscreen, sound. TODO finish this!

--- Test/ve1/Source/GSOptions.cpp
+++ Test/ve1/Source/GSOptions.cpp
@@ -1,7 +1,9 @@
-#include "GSOptions.h"
 #include <AmjuGL.h>
 #include <Game.h>
+#include <GuiButton.h>
+#include "GSOptions.h"
 #include "GSAvatarMod.h"
+#include "Kb.h"
 
 namespace Amju
 {
@@ -16,8 +18,37 @@ static void OnBack()
   TheGSOptions::Instance()->GoBack();
 }
 
+static void OnSound()
+{
+}
+
+static void OnFullscreen()
+{
+}
+
+static void OnKeyboard()
+{
+  TheGSOptions::Instance()->OnKeyboard();
+}
+
 GSOptions::GSOptions()
 {
+}
+
+void GSOptions::OnKeyboard()
+{
+  static Kb* kb = TheKb::Instance();
+  kb->SetEnabled(!kb->IsEnabled());
+
+  GuiButton* b = (GuiButton*)GetElementByName(m_gui, "keyboard-button");
+  if (kb->IsEnabled())
+  {
+    b->SetText("Disable KB");
+  }
+  else
+  {
+    b->SetText("Enable KB");
+  }
 }
 
 void GSOptions::Update()
@@ -43,17 +74,21 @@ void GSOptions::OnActive()
 
   m_gui = LoadGui("gui-options.txt");
   Assert(m_gui);
-  m_gui->GetElementByName("avatar-button")->SetCommand(OnAvatarButton);
-  m_gui->GetElementByName("back-button")->SetCommand(OnBack);
-}
 
-bool GSOptions::OnCursorEvent(const CursorEvent& ce)
-{
-  return false;
-}
+  GetElementByName(m_gui, "back-button")->SetCommand(OnBack);
+  GetElementByName(m_gui, "sound-button")->SetCommand(OnSound);
+  GetElementByName(m_gui, "fullscreen-button")->SetCommand(OnFullscreen);
 
-bool GSOptions::OnMouseButtonEvent(const MouseButtonEvent& mbe)
-{
-  return false;
+  // TODO Hide for iPad etc, this must always have KB.
+  GuiButton* kb = (GuiButton*)GetElementByName(m_gui, "keyboard-button");
+  kb->SetCommand(Amju::OnKeyboard);
+  if (TheKb::Instance()->IsEnabled())
+  {
+    kb->SetText("Disable KB");
+  }
+  else
+  {
+    kb->SetText("Enable KB");
+  }
 }
 } // namespace
