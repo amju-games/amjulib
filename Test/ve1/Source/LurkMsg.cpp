beb387ceaf665075f948614d6b50929753638ea7
jason.e.colman <jason.e.colman@gmail.com>
2012-07-11 12:18:52 +0000

    Lurk msgs - not finished

--- Test/ve1/Source/LurkMsg.cpp
+++ Test/ve1/Source/LurkMsg.cpp
@@ -17,6 +17,8 @@ void LurkMsg::Update()
 {
   //Vec2f m_pos;
   //Vec2f m_vel;
+
+  m_rect->SetLocalPos(m_text->GetLocalPos());
 }
 
 void LurkMsg::Set(const std::string& text, const Colour& fgCol, const Colour& bgCol, LurkPos lp)
@@ -25,12 +27,15 @@ void LurkMsg::Set(const std::string& text, const Colour& fgCol, const Colour& bg
   m_text->SetText(text);
   m_text->SizeToText();
   m_text->SetLocalPos(Vec2f(0, 0)); // TODO TEMP TEST
+  m_text->SetFgCol(fgCol);
 
   //enum LurkPos { AMJU_TOP, AMJU_BOTTOM, AMJU_LEFT, AMJU_RIGHT };
 
   m_rect = new GuiRect;
   m_rect->SetRoundCorners(12); // TODO TEMP TEST
   m_rect->SetSize(m_text->GetSize());
+  m_rect->SetColour(bgCol);
+  m_rect->SetCornerRadius(0.04f); // TODO TEMP TEST
 }
 }
 
