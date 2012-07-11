0baa73ccb9adbb3ee0c855d05f4fee83a4411a22
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-07-11 12:18:52 +0000

    Lurk msgs - not finished

--- trunk/Test/ve1/Source/LurkMsg.h
+++ trunk/Test/ve1/Source/LurkMsg.h
@@ -1,10 +1,12 @@
 #ifndef LURK_MSG_H_INCLUDED
 #define LURK_MSG_H_INCLUDED
 
+#include <queue>
 #include <string>
 #include <Colour.h>
 #include <GuiText.h>
 #include <GuiRect.h>
+#include <Singleton.h>
 
 namespace Amju
 {
@@ -12,9 +14,11 @@ namespace Amju
 class LurkMsg
 {
 public:
-  LurkMsg();
-
   enum LurkPos { AMJU_TOP, AMJU_BOTTOM, AMJU_LEFT, AMJU_RIGHT };
+
+  LurkMsg();
+  LurkMsg(const std::string& text, const Colour& fgCol, const Colour& bgCol, LurkPos lp);
+
 
   void Draw();
   void Update();
@@ -28,7 +32,23 @@ private:
   Vec2f m_vel;
 
   float m_timer;
+  
+  enum State { LURK_SHOWN, LURK_SHOWING, LURK_HIDDEN, LURK_HIDING };
+  State m_state;
 };
+
+class Lurker : public NonCopyable
+{
+public:
+  void Update();
+  void Draw();
+  void Queue(const LurkMsg& lm);
+
+private:
+  typedef std::queue<LurkMsg> m_q; 
+};
+
+typedef Singleton<Lurker> TheLurker;
 }
 
 #endif
