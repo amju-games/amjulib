c6fa25c5732225d41852de51e136f4c64924bc7e
jason.e.colman <jason.e.colman@gmail.com>
2012-07-16 13:52:36 +0000

    MSVC/nightly build

--- Test/ve1/Script/Windows/make_release.bat
+++ Test/ve1/Script/Windows/make_release.bat
@@ -8,7 +8,10 @@ devenv /build "Release|Win32" ve1.sln
 cd ..
 
 REM Run Inno Setup script
-iscc MyGameSetup.iss
+set PATH="\Program Files\Inno Setup 5\";%PATH%
+set PATH="\Program Files (x86)\Inno Setup 5\";%PATH%
+iscc.exe MyGameSetup.iss
 
 cd Output
-copy setup.exe mygame-%SERVERSION%.exe
+copy setup.exe "..\mygame-%VERSION%.exe"
+
