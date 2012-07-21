7fd41caae6cd718af0d673977e1f1a6d6e255b87
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-07-21 18:38:13 +0000

    Drop any carried item on logout

--- trunk/Test/ve1/Server/cgi-bin/logout.pl
+++ trunk/Test/ve1/Server/cgi-bin/logout.pl
@@ -6,26 +6,9 @@
 ##  wants to end session.
 #############################################
 
-require "common.pl";
+require "logout_impl.pl";
 
 my_connect();
 
-sub logout();
 logout();
-
-
-##### end of main function
-
-sub logout()
-{
-  my $session_id = param('session_id') or die "No session id";
-
-  my $sql = "update session set `expires`=now() where id=$session_id";
-  insert($sql);
-
-  $sql = "insert into objectstate (`id`, `key`, `val`) select player.obj_id, 'loggedin', 'n' from player, session where player.id = session.player_id and session.id=$session_id on duplicate key update val='n'";
-  insert($sql); 
-  
-}
-
 
