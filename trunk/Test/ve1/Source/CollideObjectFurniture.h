48b714aaa2f8af520e910e20df38082f7a2ac810
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-07-22 22:19:02 +0000

    Changed to work with any collision with an immovable object. E.g. baddies work the same way as Furniture.

--- trunk/Test/ve1/Source/CollideObjectFurniture.h
+++ trunk/Test/ve1/Source/CollideObjectFurniture.h
@@ -5,6 +5,8 @@ namespace Amju
 {
 class GameObject;
 
+// Collision between Ve1Object and an immovable object, e.g. Furniture.
+// go1 is pushed back. go2 doesn't move.
 void CollideObjectFurniture(GameObject* go1, GameObject* go2);
 }
 
