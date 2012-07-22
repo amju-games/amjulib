041740e9b16d209d8e28e9a080b96270ae287939
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-07-22 22:19:31 +0000

    Tidy up code, it's like Baddie now.

--- trunk/Test/ve1/Source/TutorialNpc.cpp
+++ trunk/Test/ve1/Source/TutorialNpc.cpp
@@ -1,6 +1,7 @@
 #include <GameObjectFactory.h>
 #include "TutorialNpc.h"
 #include "Ve1SceneGraph.h"
+#include "Ve1Character.h"
 
 namespace Amju
 {
@@ -22,36 +23,7 @@ bool TutorialNpc::Load(File* f)
 {
   Ve1Character* node = new Ve1Character;
 
-  File dinoFile;
-  if (!dinoFile.OpenRead("tutorial-npc.txt"))
-  {
-std::cout << "Baddie scene node: Failed to load dino.\n";
-    Assert(0);
-  }
-
-  if (!node->Load(&dinoFile))
-  {
-std::cout << "Baddie scene node: Failed to load dino.\n";
-    Assert(0); //return false;
-  }
-
   m_sceneNode = node;
-
-  m_shadow = new Shadow;
-  m_shadow->SetSize(20.0f); // TODO
-  Texture* tex = (Texture*)TheResourceManager::Instance()->GetRes("shadow.png"); // TODO
-
-  m_shadow->SetTexture(tex);
-  m_sceneNode->AddChild(m_shadow.GetPtr());
-
-/*
-  Ve1Character* psn = new Ve1Character; 
-  m_sceneNode = psn;
-
-  if (!psn->Load(f))
-  {
-    return false;
-  }
 
   m_shadow = new Shadow;
   if (!m_shadow->Load(f))
@@ -59,9 +31,8 @@ std::cout << "Baddie scene node: Failed to load dino.\n";
     return false;
   }
   m_sceneNode->AddChild(m_shadow.GetPtr());
-*/
 
-  // TODO Load script
+  m_inNewLocation = true;
 
   return true;
 }
