347bee5c03050e8edbb4db3038bf69c57b2fefe1
jason.e.colman <jason.e.colman@gmail.com>
2012-07-22 09:10:35 +0000

    Pick ups: only take ownership of an object if it's not already owned

--- Test/ve1/Server/cgi-bin/updatereq.pl
+++ Test/ve1/Server/cgi-bin/updatereq.pl
@@ -25,14 +25,32 @@ sub updatestate()
   my $val = param('val');
   my $obj_id = param('obj_id') or die "No ID";
 
+  if ($key eq "pickup")
+  {
+    # Special case key:
+    # We can only pick up (take ownership) of object if there is no current owner
+    # TODO Check player strength here and any other restrictions
+    if ($val ne "0")
+    {
+      # First make sure row exists. Then update it.
+      my $sql = "insert ignore into objectstate values ($obj_id, '$key', 0, 1)";
+      insert($sql);
+      $sql = "update objectstate set val='$val', whenchanged=now() where `key`='$key' and id=$obj_id and val='0'";
+      update($sql);
+    }
+    else
+    {
+      # We can only drop the object if we are the current owner.
+      # TODO
+      my $sql = "update objectstate set val='0', whenchanged=now() where `key`='$key' and id=$obj_id";
+      # .. and val in (select objectId from player, session)... 
+      update($sql);
+    }
+    return;
+  }
+
   my $sql = "insert into objectstate values ($obj_id, '$key', '$val', now()) on duplicate key update val='$val', whenchanged=now()";
-
-  print "Query: $sql\n\n";
-
-  my $query = $dbh->prepare($sql) or die
-    "Query prepare failed for this query: $sql\n";
-
-  $query->execute;
+  update_or_insert($sql);
 
 }
 
