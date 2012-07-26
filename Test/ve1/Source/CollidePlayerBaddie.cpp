49b097e58594c38c854154236165e641ade89326
jason.e.colman <jason.e.colman@gmail.com>
2012-07-26 09:40:21 +0000

    Bddies are immovable obstacles

---
+++ Test/ve1/Source/CollidePlayerBaddie.cpp
@@ -0,0 +1,17 @@
+#include "CollisionManager.h"
+#include "Player.h"
+#include "Baddie.h"
+#include "CollideObjectFurniture.h"
+
+namespace Amju
+{
+void CollidePlayerBaddie(Player* player, Baddie* b)
+{
+  player->SetIsColliding(b);
+  CollideObjectFurniture(player, b); // player moves back
+  // TODO This is a baddie, so we should lose health etc
+}
+
+static bool b1 = TheCollisionManager::Instance()->Add(
+  Player::TYPENAME, Baddie::TYPENAME, (CollisionManager::CollisionHandler)CollidePlayerBaddie);
+} 
