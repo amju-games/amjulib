a1b977faf8bb26a3d46228748c9e416dc3b2c347
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-08-08 19:18:54 +0000

    Save/load options to/from player info

--- trunk/Test/ve1/Source/GSOptions.h
+++ trunk/Test/ve1/Source/GSOptions.h
@@ -3,6 +3,7 @@
 
 #include <Singleton.h>
 #include "GSGui.h"
+#include "PlayerInfo.h"
 
 namespace Amju 
 {
@@ -16,8 +17,17 @@ public:
   virtual void Draw();
   virtual void Draw2d();
   virtual void OnActive();
+  virtual void OnDeactive(); 
 
   void OnKeyboard();
+
+  void LoadSettingsFromPI(PlayerInfo*);
+  void SaveSettingsToPI(PlayerInfo*);
+
+private:
+  // TODO Variables for volume, screen, kb, etc.
+  float m_musicVol;
+  float m_soundVol;
 };
 typedef Singleton<GSOptions> TheGSOptions;
 } // namespace
