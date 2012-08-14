6377dc6bbfa65e8c0de4e484cb4e8a82db0bd469
jason.e.colman <jason.e.colman@gmail.com>
2012-08-14 16:08:44 +0000

    Fix path..?

--- Test/ve1/Script/Windows/make_release_debug.bat
+++ Test/ve1/Script/Windows/make_release_debug.bat
@@ -8,8 +8,8 @@ devenv /build "Debug|Win32" ve1.sln
 cd ..
 
 REM Run Inno Setup script
-set PATH="\Program Files\Inno Setup 5\";%PATH%
-set PATH="\Program Files (x86)\Inno Setup 5\";%PATH%
+set PATH=\Program Files\Inno Setup 5\;%PATH%
+set PATH=\Program Files (x86)\Inno Setup 5\;%PATH%
 iscc.exe MyGameSetup_debug.iss
 
 cd Output
