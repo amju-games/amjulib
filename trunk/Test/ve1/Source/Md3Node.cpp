7943f9416b75cbe286b5aca43a982b682ee38139
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-08-23 09:18:57 +0000

    Stub for Md3 node type

---
+++ trunk/Test/ve1/Source/Md3Node.cpp
@@ -0,0 +1,18 @@
+#include "Md3Node.h"
+
+namespace Amju
+{
+void Md3Node::Draw()
+{
+}
+
+void Md3Node::SetFromCharacterName(const std::string& characterName)
+{
+}
+
+void Md3Node::SetAnim(const std::string& animName)
+{
+}
+
+}
+
