2dbcc6677bbe98b7074c69cf6a8641f4869f1c27
jason.e.colman <jason.e.colman@gmail.com>
2012-08-25 20:47:48 +0000

    Add MD3 support

--- Test/ve1/Source/Md3Node.h
+++ Test/ve1/Source/Md3Node.h
@@ -1,6 +1,7 @@
 #ifndef MD3_NODE_H_INCLUDED
 #define MD3_NODE_H_INCLUDED
 
+#include <Md3.h>
 #include "Ve1Character.h"
 
 namespace Amju
@@ -8,12 +9,15 @@ namespace Amju
 class Md3Node : public Ve1Character
 {
 public:
+  Md3Node();
   virtual void Draw();
+  virtual void Update();
   virtual void SetFromCharacterName(const std::string& characterName);
   virtual void SetAnim(const std::string& animName);
 
 private:
-  // Md3 mesh
+  // Md3 mesh - owned by ResourceManager
+  RCPtr<CModelMD3> m_model;
 };
 
 }
