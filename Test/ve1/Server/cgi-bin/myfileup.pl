db340200b22b930096504ca71eeb5561ebce8b7d
jason.e.colman <jason.e.colman@gmail.com>
2012-08-27 10:22:11 +0000

    DON'T disconnect, in case the connection is kept alive server side

--- Test/ve1/Server/cgi-bin/myfileup.pl
+++ Test/ve1/Server/cgi-bin/myfileup.pl
@@ -56,8 +56,6 @@ sub myfileup_impl()
     my_connect2();
     my $sql = "insert into fileupdate (filename, whenchanged) values ('$directory/$filename', now()) on duplicate key update whenchanged=now()";
     update_or_insert($sql);
-    # Disconnect ASAP, to avoid running out of connections
-    disconnect();
   }
 
   #my $upload_filehandle = $query->param('data'); 
