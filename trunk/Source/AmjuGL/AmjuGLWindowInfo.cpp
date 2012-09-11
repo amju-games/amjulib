cfcb9ca8786a50290e1e0662d259046d7eabf41e
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-09-11 15:21:44 +0000

    Add title to window info

--- trunk/Source/AmjuGL/AmjuGLWindowInfo.cpp
+++ trunk/Source/AmjuGL/AmjuGLWindowInfo.cpp
@@ -2,9 +2,14 @@
 
 namespace Amju
 {
-AmjuGLWindowInfo::AmjuGLWindowInfo(int w, int h, bool full) :
-  m_width(w), m_height(h), m_isFullScreen(full)
+AmjuGLWindowInfo::AmjuGLWindowInfo(int w, int h, bool full, const std::string& title) :
+  m_width(w), m_height(h), m_isFullScreen(full), m_title(title)
 {
+}
+
+const char* AmjuGLWindowInfo::GetTitle() const
+{
+  return m_title.c_str();
 }
 
 int AmjuGLWindowInfo::GetWidth() const
