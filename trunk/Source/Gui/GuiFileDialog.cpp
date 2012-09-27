e850d1ce2e377cead58e10175d23dc02ce7add64
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-09-27 19:32:42 +0000

    Set default dir and populate on load

--- trunk/Source/Gui/GuiFileDialog.cpp
+++ trunk/Source/Gui/GuiFileDialog.cpp
@@ -1,4 +1,5 @@
 #include "GuiFileDialog.h"
+#include "GuiFileListBox.h"
 
 namespace Amju
 {
@@ -9,5 +10,30 @@ GuiElement* CreateFileDialog()
   GuiFileDialog* d = new GuiFileDialog;
   return d;
 }
+
+bool GuiFileDialog::Load(File* f)
+{
+  if (!GuiDialog::Load(f))
+  {
+    return false;
+  }
+
+  // Element name is in text file.
+  // TODO Look up by type instead ??
+  GuiFileListBox* fb = dynamic_cast<GuiFileListBox*>(GetElementByName("fd-file-list-box"));
+  Assert(fb);
+
+  // Set default/start dir
+#ifdef WIN32
+  fb->SetDir("c:\\");
+#endif
+
+#ifdef MACOSX
+  fb->SetDir("/");
+#endif
+
+  return true;
+}
+
 }
 
