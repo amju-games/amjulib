080f2e3df4a5fb16ca396f77ce5b6190e80794a5
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-09-27 17:28:23 +0000

    Dialog boxes

--- trunk/Source/Gui/GuiDialog.h
+++ trunk/Source/Gui/GuiDialog.h
@@ -1,11 +1,11 @@
 #ifndef GUI_DIALOG_H_INCLUDED
 #define GUI_DIALOG_H_INCLUDED
 
-#include "GuiComposite.h"
+#include "GuiWindow.h"
 
 namespace Amju
 {
-class GuiDialog : public GuiComposite
+class GuiDialog : public GuiWindow
 {
 public:
   static const char* NAME;
