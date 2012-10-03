98d63ebdb1756e2d71c913e6c0f62859f60a8736
jason.e.colman <jason.e.colman@gmail.com>
2012-10-03 10:19:16 +0000

    Fix script to add a new uncommitted location to DB

--- Test/ve1/Server/cgi-bin/newlocation_row.pl
+++ Test/ve1/Server/cgi-bin/newlocation_row.pl
@@ -2,28 +2,32 @@
 
 require "common.pl";
 
-
-my $loc_id = param('loc_id') or die "Expected location ID";
-my $assetFile = param('asset_file') or die "Expected asset file name for location";
-my $dataFile = param('data_file') or die "Expected data file name for location";
-
 my_connect();
 
-# Add row to object table and objectpos
-$sql = "insert into object (type, assetfile, datafile, owner, createtime) values ('terrain', '$assetFile', '$dataFile', 0, now() ) ";
-insert($sql);
+sub doit();
+doit();
 
-$sql = "insert into objectpos (id, x, y, z, loc, whenchanged) values (LAST_INSERT_ID(), 0, 0, 0, $loc_id, now())";
-insert($sql);
-
-$sql = "update location set obj_id = LAST_INSERT_ID(), whenchanged = now() where id=$loc_id";
-update($sql);
-
-
-if (check_session() == 0)
+sub doit()
 {
-  return;
-}
+  if (check_session() == 0)
+  {
+    return;
+  }
 
-print "OK!\n";
+  my $loc_id = param('loc_id') or die "Expected location ID";
+  my $assetFile = param('asset_file') or die "Expected asset file name for location";
+  my $dataFile = param('data_file') or die "Expected data file name for location";
+
+  # Add row to object table and objectpos
+  $sql = "insert into object (type, assetfile, datafile, owner, createtime) values ('terrain', '$assetFile', '$dataFile', 0, now() ) ";
+  insert($sql);
+
+  $sql = "insert into objectpos (id, x, y, z, loc, whenchanged) values (LAST_INSERT_ID(), 0, 0, 0, $loc_id, now())";
+  insert($sql);
+
+  $sql = "update location set obj_id = LAST_INSERT_ID(), whenchanged = now() where id=$loc_id";
+  update($sql);
+
+  print "OK!\n";
+}
 
