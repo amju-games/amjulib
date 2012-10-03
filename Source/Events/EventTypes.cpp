e09c2c9486ca0461a167059b30cab9efd1ff609d
jason.e.colman <jason.e.colman@gmail.com>
2012-10-03 12:15:40 +0000

    Added dx/dy members for cursor events. Y U No Work!!

--- Source/Events/EventTypes.cpp
+++ Source/Events/EventTypes.cpp
@@ -10,6 +10,27 @@ bool QuitEvent::UpdateListener(EventListener* el)
 
 bool CursorEvent::UpdateListener(EventListener* el)
 {
+  static const int MAX_CURSORS = 4; // ..?
+
+  static float oldx[MAX_CURSORS];
+  static float oldy[MAX_CURSORS];
+  static bool first[MAX_CURSORS] = { true, true, true, true };
+
+  if (first[controller])
+  {
+    first[controller] = false;
+    oldx[controller] = x;
+    oldy[controller] = y;
+  }
+
+  dx = x - oldx[controller];
+  oldx[controller] = x;
+
+  dy = y - oldy[controller];
+  oldy[controller] = y;
+
+std::cout << "x: " << x << " y: " << y << " dx: " << dx << " dy: " << dy << "\n";
+
   return el->OnCursorEvent(*this);
 }
 
