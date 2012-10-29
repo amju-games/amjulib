3f9bd52b7f5b0f3d0e20daf4632156805ccfc16f
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-10-29 18:48:10 +0000

    Remember/restore window size

--- trunk/Test/ve1/Source/ResizeHandler.cpp
+++ trunk/Test/ve1/Source/ResizeHandler.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <ConfigFile.h>
 #include "ResizeHandler.h"
 
 namespace Amju
@@ -7,6 +7,12 @@ namespace Amju
 bool ResizeHandler::OnResizeEvent(const ResizeEvent& re)
 {
 std::cout << "RESIZE EVENT! Save this shit to game.cfg!!!\n";
+
+  ConfigFile* cf = TheGameConfigFile::Instance();
+  cf->SetInt("screen-x", re.x);
+  cf->SetInt("screen-y", re.y);
+
+  return true;
 }
 }
 
