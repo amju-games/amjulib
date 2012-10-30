9023ec642061f50ed28057b8b69cc18c5bd3bca5
jason.e.colman <jason.e.colman@gmail.com>
2012-10-30 09:02:38 +0000

    Del debug msg

--- Test/ve1/Source/ResizeHandler.cpp
+++ Test/ve1/Source/ResizeHandler.cpp
@@ -6,8 +6,6 @@ namespace Amju
 {
 bool ResizeHandler::OnResizeEvent(const ResizeEvent& re)
 {
-std::cout << "RESIZE EVENT! Save this shit to game.cfg!!!\n";
-
   ConfigFile* cf = TheGameConfigFile::Instance();
   cf->SetInt("screen-x", re.x);
   cf->SetInt("screen-y", re.y);
