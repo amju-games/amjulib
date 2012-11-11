72f217f361fd8db69cb910f999a747bbb19871ea
jason.e.colman <jason.e.colman@gmail.com>
2012-11-11 08:33:39 +0000

    Add Spaceship type

---
+++ Test/ve1/Source/CollidePlayerSpaceship.cpp
@@ -0,0 +1,28 @@
+#include <CollisionManager.h>
+#include "Player.h"
+#include "Spaceship.h"
+#include "FirstTimeMsg.h"
+
+namespace Amju
+{
+void CollidePlayerSpaceship(Player* player, Spaceship* spaceship)
+{
+std::cout << "Hi collide player/spaceship\n";
+
+  if (player->IsHidden() || spaceship->IsHidden())
+  {
+    return;
+  }
+
+  player->SetIsColliding(spaceship);
+
+  FirstTimeMsgThisSession("This is your spaceship! It needs fuel. Please find fuel cells and bring them back here!", MsgNum(3), false);
+
+  // TODO carrying fuel cell ?
+}
+
+static bool b1 = TheCollisionManager::Instance()->Add(
+  Player::TYPENAME, Spaceship::TYPENAME, (CollisionManager::CollisionHandler)CollidePlayerSpaceship);
+
+}
+
