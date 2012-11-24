82f587e11235c2c943c51291556e3f12b25b0c6e
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-11-24 19:52:36 +0000

    Add functions to get player valuse safely

--- trunk/Test/ve1/Source/HeartCount.h
+++ trunk/Test/ve1/Source/HeartCount.h
@@ -8,11 +8,15 @@ namespace Amju
 // Get heart count for local player, if available
 bool GetHeartCount(int* result);
 
+bool GetFuelCellCount(int* result);
+
 // Change heart count - this updates the server
 bool ChangeHeartCount(int delta);
 
 // Change a numeric attribute for the local player
 bool ChangePlayerCount(const std::string& key, int delta);
+
+bool GetPlayerCount(const std::string& key, int* result);
 }
 
 #endif
