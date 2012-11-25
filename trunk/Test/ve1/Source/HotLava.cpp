515c67412efba1a874e2e8f3cb3a154b727e90e4
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2012-11-25 17:43:59 +0000

    Add GameObject type for hot lava (will work for anything harmful, e.g. spikes, toxic lake, etc.) Not finished yet.

---
+++ trunk/Test/ve1/Source/HotLava.cpp
@@ -0,0 +1,33 @@
+#include <GameObjectFactory.h>
+#include "HotLava.h"
+
+namespace Amju
+{
+HotLava::HotLava()
+{
+  m_isPickable = false;
+}
+
+const char* HotLava::TYPENAME = "hotlava";
+
+const char* HotLava::GetTypeName() const
+{
+  return TYPENAME;
+}
+
+GameObject* CreateHotLava()
+{
+  return new HotLava;
+}
+
+static bool registered = TheGameObjectFactory::Instance()->Add(HotLava::TYPENAME, CreateHotLava);
+
+void HotLava::SetEditMenu(GuiMenu* menu)
+{
+}
+
+//void HotLava::Update()
+//{
+//}
+
+}
