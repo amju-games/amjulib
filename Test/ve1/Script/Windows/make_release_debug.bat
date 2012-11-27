dd8613160217031a0ec4dbc913bbe602a073de2b
jason.e.colman <jason.e.colman@gmail.com>
2012-11-27 09:46:56 +0000

    Updated make release script for debug build for Windows

--- Test/ve1/Script/Windows/make_release_debug.bat
+++ Test/ve1/Script/Windows/make_release_debug.bat
@@ -1,6 +1,6 @@
 REM Make windows release of MY GAME
 
-set VERSION=0.4
+set VERSION=0.6
 
 cd ve1
 devenv /build "Debug|Win32" ve1.sln
@@ -8,10 +8,10 @@ devenv /build "Debug|Win32" ve1.sln
 cd ..
 
 REM Run Inno Setup script
-set PATH=\Program Files\Inno Setup 5\;%PATH%
-set PATH=\Program Files (x86)\Inno Setup 5\;%PATH%
+set PATH="\Program Files\Inno Setup 5\";%PATH%
+set PATH="\Program Files (x86)\Inno Setup 5\";%PATH%
 iscc.exe MyGameSetup_debug.iss
 
 cd Output
-copy setup.exe "..\..\..\..\..\..\MyGame-v.%VERSION%-setup.exe"
+copy setup.exe "..\..\..\..\..\..\CrashSite-v.%VERSION%-setup.exe"
 
