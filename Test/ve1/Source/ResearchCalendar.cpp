e53132cd58207e49889c63c8710878b52b25b70e
jason.e.colman <jason.e.colman@gmail.com>
2012-11-29 18:32:42 +0000

    Get day number

--- Test/ve1/Source/ResearchCalendar.cpp
+++ Test/ve1/Source/ResearchCalendar.cpp
@@ -1,3 +1,4 @@
+#include <TimePeriod.h>
 #include "ResearchCalendar.h"
 
 namespace Amju
@@ -19,12 +20,44 @@ void ResearchCalendar::AddResearchDate(const ResearchDate& d)
 
 const ResearchDate* ResearchCalendar::GetToday() const
 {
+  Time today(Time::Now());
+  today.RoundDown(TimePeriod(ONE_DAY_IN_SECONDS));
+
+  for (unsigned int i = 0; i < m_dates.size(); i++)
+  {
+    if (m_dates[i].m_time == today)
+    {
+      return &m_dates[i];
+    }
+  }
+
   return 0;
 }
 
 const ResearchDate* ResearchCalendar::GetNext() const
 {
   return 0;
+}
+
+int ResearchCalendar::GetDayOnPlanet() const
+{
+  int d = 0;
+  Time today(Time::Now());
+  today.RoundDown(TimePeriod(ONE_DAY_IN_SECONDS));
+
+  for (unsigned int i = 0; i < m_dates.size(); i++)
+  {
+    if (m_dates[i].m_play)
+    {
+      d++;
+    }
+
+    if (!(m_dates[i].m_time < today)) // sigh, TODO >=
+    {
+      return d;
+    }
+  }
+  return d;
 }
 
 }
