456e2252fca0e2f55f26cb794504afa97cfc9bab
jason.e.colman <jason.e.colman@gmail.com>
2013-03-29 18:23:07 +0000

    MSVC warnings

--- Source/FileUtils/GlueFile.cpp
+++ Source/FileUtils/GlueFile.cpp
@@ -4,7 +4,9 @@ Amju Games source code (c) Copyright Jason Colman 2004
 
 #include "AmjuFirst.h"
 #ifdef WIN32
+#ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
+#endif
 #endif // WIN32
 
 #include "GlueFile.h"
