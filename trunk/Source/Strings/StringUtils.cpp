22c117467ea6925f01f358836eecbcde7a9c5d49
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-03-29 18:23:07 +0000

    MSVC warnings

--- trunk/Source/Strings/StringUtils.cpp
+++ trunk/Source/Strings/StringUtils.cpp
@@ -39,7 +39,9 @@ Added to repository
 #include "AmjuFirst.h"
 
 #ifdef WIN32
+#ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
+#endif
 #endif // WIN32
 
 #include <algorithm> // remove()
