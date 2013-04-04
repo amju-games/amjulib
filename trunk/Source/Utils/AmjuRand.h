0342e1a7c9448f240d43922a1dc9122c05c5f277
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-04-04 15:21:19 +0000

    Add Randomise()

--- trunk/Source/Utils/AmjuRand.h
+++ trunk/Source/Utils/AmjuRand.h
@@ -3,6 +3,9 @@
 
 namespace Amju
 {
+// Set random seed to current time
+void Randomise();
+
 // Return rand float between min and max, min <= r < max
 // Uses std lib rand()
 float Rnd(float min, float max);
