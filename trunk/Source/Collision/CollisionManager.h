1d2f81dacda52c13efa478b1cfddf6b2e3fcf248
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-04-12 11:03:44 +0000

    Only handle collisions on first contact

--- trunk/Source/Collision/CollisionManager.h
+++ trunk/Source/Collision/CollisionManager.h
@@ -2,6 +2,7 @@
 #define COLLISION_MANAGER_H
 
 #include <map>
+#include <set>
 #include <string>
 #include "Singleton.h"
 #include "GameObject.h"
@@ -16,13 +17,26 @@ private:
 
 public:
   typedef void (*CollisionHandler)(GameObject*, GameObject*);
+
+  // TODO Flag so we can handle collision only on first contact, or every frame as appropriate.
   bool Add(const std::string& name1, const std::string& name2, CollisionHandler);
+
+  // Called from collision detection code
   bool HandleCollision(GameObject*, GameObject*);
+
+  // Call each frame before testing for collisions 
+  void InitFrame(); 
 
 private:
   typedef std::pair<std::string, std::string> TypeNamesPair;
   typedef std::map<TypeNamesPair, CollisionHandler> CollisionMap;
   CollisionMap m_map;
+
+  // remember pairs of colliding objects so we only notify on first contact
+  typedef std::pair<int, int> IdPair;
+  typedef std::set<IdPair> IdPairSet; 
+  IdPairSet m_collisionsThisFrame;
+  IdPairSet m_collisionsLastFrame;
 };
 
 typedef Singleton<CollisionManager> TheCollisionManager;
