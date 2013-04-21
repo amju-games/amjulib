24b2f4bbe6b28def1754e4d9a88377300b723cd2
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-04-21 17:29:48 +0000

    Fix texture filtering on iphone - fonts now display

--- trunk/Source/AmjuGLOpenGL/AmjuGL-OpenGL-Base.cpp
+++ trunk/Source/AmjuGLOpenGL/AmjuGL-OpenGL-Base.cpp
@@ -414,8 +414,13 @@ void AmjuGLOpenGLBase::SetTextureFilter(AmjuGL::TextureFilter tf)
 {
   if (tf == AmjuGL::AMJU_TEXTURE_NICE)
   {
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+#ifdef IPHONE
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+#else
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+#endif
   }
   else if (tf == AmjuGL::AMJU_TEXTURE_NEAREST)
   {
