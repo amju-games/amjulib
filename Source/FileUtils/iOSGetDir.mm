f144e82438fdc7cadc2e9b19804d822567a298e8
jason.e.colman <jason.e.colman@gmail.com>
2013-04-21 08:50:03 +0000

    Added iOS GetSaveDir()

--- Source/FileUtils/iOSGetDir.mm
+++ Source/FileUtils/iOSGetDir.mm
@@ -7,17 +7,23 @@ std::string iOSGetDataDir()
   // Get path to data files
   NSString *filePath = [[NSBundle mainBundle] pathForResource:@"Info" ofType:@"plist"];
 
-  // From http://forums.macrumors.com/showthread.php?t=494103
+  // Convert NSString to char* From http://forums.macrumors.com/showthread.php?t=494103
   //NSString *foo = @"your text here";
   //const char *bar = [foo UTF8String];
 
   const char* cFilePath = [filePath UTF8String];
-  return Amju::GetFilePath(cFilePath) + "/Contents/Resources/";
+  return Amju::GetFilePath(cFilePath) + "/";
 }
 
 std::string iOSGetSaveDir()
 {
-  return ""; // TODO
+  // From http://stackoverflow.com/questions/6907381/what-is-the-documents-directory-nsdocumentdirectory
+    
+  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+  NSString *basePath = ([paths count] > 0) ? [paths objectAtIndex:0] : nil;
+
+  const char* cFilePath = [basePath UTF8String];
+  return std::string(cFilePath) + "/";
 }
 
 
