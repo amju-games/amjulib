cb499066ef4d28ce2b6b56b0bc62112c4924c437
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-04-23 08:41:37 +0000

    Updated to make gluefile for iphone

--- trunk/Test/ve2/Script/AssetCompiler/makeglue-iphone.sh
+++ trunk/Test/ve2/Script/AssetCompiler/makeglue-iphone.sh
@@ -2,45 +2,56 @@
 
 export PLATFORM=iphone
 export THIS_DIR=`pwd`
-export TOP_DIR=$THIS_DIR/../..
-export COMPILED_ASSETS=$TOP_DIR/Build/CompiledAssets
+export TOP_DIR=$THIS_DIR/../../../..
+export COMPILED_ASSETS=$TOP_DIR/Build/ve2/CompiledAssets
 export DEST_DIR=$COMPILED_ASSETS/$PLATFORM
-export SRC_DIR=$TOP_DIR/Assets
+export SRC_DIR=../../Assets
 export GLUE_EXE=$THIS_DIR/glue
 export GLUE_FILE=$DEST_DIR/../data-$PLATFORM.glue
 
+# These are the shared font assets, not just for this project
+export GUIASSETSDIR=../../../../Assets/gui
+export FONT2D=../../../../Assets/font2d
+
 rm -rf $DEST_DIR
 mkdir -p $DEST_DIR
+mkdir -p $DEST_DIR/characters/2d/
+mkdir -p $DEST_DIR/font2d/
+mkdir -p $DEST_DIR/obj/
+mkdir -p $DEST_DIR/locations/location1/
+mkdir -p $DEST_DIR/Sound/
 
-# use png, not bmp
 cp $SRC_DIR/*.png $DEST_DIR
 cp $SRC_DIR/*.txt $DEST_DIR
-cp $SRC_DIR/*.text $DEST_DIR
-cp $SRC_DIR/*.attr $DEST_DIR
-cp $SRC_DIR/*.bill $DEST_DIR
-cp $SRC_DIR/*.move $DEST_DIR
-cp $SRC_DIR/*.room $DEST_DIR
-cp $SRC_DIR/*.lite $DEST_DIR
-cp $SRC_DIR/*.ini $DEST_DIR
-cp $SRC_DIR/*.amju2 $DEST_DIR
-cp $SRC_DIR/*.comp $DEST_DIR
-cp $SRC_DIR/*.md2 $DEST_DIR
-#cp $SRC_DIR/*.obj $DEST_DIR
-#cp $SRC_DIR/*.mtl $DEST_DIR
+cp $SRC_DIR/characters/2d/*.png $DEST_DIR/characters/2d/
+cp $SRC_DIR/font2d/*.png $DEST_DIR/font2d/
+cp $SRC_DIR/font2d/*.txt $DEST_DIR/font2d/
+cp $FONT2D/arial*.png $DEST_DIR/font2d/
+cp $FONT2D/arial*.txt $DEST_DIR/font2d/
+cp $GUIASSETSDIR/*.png $DEST_DIR
+cp $GUIASSETSDIR/*.txt $DEST_DIR
+cp $SRC_DIR/obj/*.obj $DEST_DIR/obj/
+cp $SRC_DIR/obj/*.mtl $DEST_DIR/obj/
+cp $SRC_DIR/obj/*.png $DEST_DIR/obj/
+cp $SRC_DIR/locations/location1/*.obj $DEST_DIR/locations/location1/
+cp $SRC_DIR/locations/location1/*.mtl $DEST_DIR/locations/location1/
+cp $SRC_DIR/locations/location1/*.png $DEST_DIR/locations/location1/
 
-cp $SRC_DIR/*.mod $DEST_DIR
-cp $SRC_DIR/*.wav $DEST_DIR
+# no need to have Sound separately??
+cp $SRC_DIR/Sound/*.it $DEST_DIR/Sound/
+cp $SRC_DIR/Sound/*.wav $DEST_DIR/Sound/
 
 # TODO Convert text files to binary
-. makeobjsbin-iphone.sh
+#  When we do this, no need to distribute separate .mtl files.
+#. makeobjsbin-mac.sh
 
 
 cd $DEST_DIR
 
 # Create empty glue file
 $GLUE_EXE -c $GLUE_FILE
-
-for f in *.txt *.text *.attr *.bill *.move *.room *.lite *.ini *.amju2 *.md2 *.png *.obj *.comp
+$
+for f in *.txt *.png *.obj *.mtl characters/2d/* font2d/* obj/* locations/location1/* Sound/*
 do
     echo "Adding file: " $f
     $GLUE_EXE -a $GLUE_FILE $f
