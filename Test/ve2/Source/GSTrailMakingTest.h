2996a82f4ed44f31b96a90bbf7d5088048623789
jason.e.colman <jason.e.colman@gmail.com>
2013-07-04 17:31:46 +0000

    Trail making test improvements

--- Test/ve2/Source/GSTrailMakingTest.h
+++ Test/ve2/Source/GSTrailMakingTest.h
@@ -14,7 +14,8 @@ typedef Singleton<GSTrailMakingTest> TheGSTrailMakingTest;
 
 struct TrailCircle
 {
-  TrailCircle(const Vec2f& pos, const std::string& str) : m_pos(pos), m_str(str), m_clicked(false) {}
+  TrailCircle(const Vec2f& pos, const std::string& str) : 
+    m_pos(pos), m_str(str), m_clicked(false), m_incorrect(false) {}
 
   void Draw();
   bool IsClicked(const Vec2f& cursorPos);
@@ -22,6 +23,7 @@ struct TrailCircle
   Vec2f m_pos;
   std::string m_str; // 1, A, 2, B... etc
   bool m_clicked;
+  bool m_incorrect;
 };
 
 class TrailListener;
@@ -49,6 +51,8 @@ protected:
   int m_correct;
 
   TrailListener* m_listener;
+
+  bool m_alternatingVersion;
 };
 }
 
