38a00122e7f1f7bd4fd8c71e1eeaa9f3d353d3b5
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-07-06 14:37:59 +0000

    More features for baddies

--- trunk/Test/ve2/Source/Baddie.cpp
+++ trunk/Test/ve2/Source/Baddie.cpp
@@ -1,4 +1,5 @@
 #include <AmjuFirst.h>
+#include <SoundManager.h>
 #include <Game.h>
 #include <GameObjectFactory.h>
 #include <File.h>
@@ -6,6 +7,7 @@
 #include <LoadVec3.h>
 #include <LoadVec2.h>
 #include <Timer.h>
+#include <AmjuRand.h>
 #include "Baddie.h"
 #include "BaddieNode.h"
 #include "Ve1SceneGraph.h"
@@ -44,10 +46,31 @@ Baddie::Baddie()
   m_maxHealth = 1; 
   m_health = m_maxHealth;
 
+  m_harmful = false;
+  for (int i = 0; i < 4; i++)
+  {
+    m_time[i] = 0;
+    m_cellRange[i] = 0;
+  }
+  m_timeInState = 0;
+  m_maxTimeInState = 0;
 }
 
 void Baddie::Update()
 {
+  Player* lp = GetLocalPlayer();
+  if (m_harmful && lp)
+  {
+    Vec3f v = lp->GetPos() - m_pos;
+    v.Normalise();
+    v *= m_chaseSpeed;
+    SetVel(v);
+  }
+  else
+  {
+    SetVel(Vec3f()); // Or move more slowly??
+  }
+
   Ve1Object::Update();
 
   // Surely this is all done in base class ???
@@ -60,15 +83,46 @@ void Baddie::Update()
 
   if (GetAABB())
   {
+    float x = m_size.x * 0.4f;
+    float y = m_size.y * 0.4f;
+
     GetAABB()->Set(
-      m_pos.x - XSIZE, m_pos.x + XSIZE,
+      m_pos.x - x, m_pos.x + x,
       m_pos.y, m_pos.y + YSIZE,
-      m_pos.z - XSIZE, m_pos.z + XSIZE);
+      m_pos.z - y, m_pos.z + y);
 
     //if (m_effect)
     //{
     //  *(m_effect->GetAABB()) = *(m_sceneNode->GetAABB());
     //}
+  }
+
+  float dt = TheTimer::Instance()->GetDt();
+  m_timeInState += dt;
+  if (m_timeInState > m_maxTimeInState)
+  {
+    Assert(dynamic_cast<SpriteNode*>(m_sceneNode.GetPtr()));
+    Sprite* spr = ((SpriteNode*)m_sceneNode.GetPtr())->GetSprite();
+
+    m_timeInState = 0;
+    m_harmful = !m_harmful;
+    // New random time, within range for this state
+    if (m_harmful) 
+    {
+      TheSoundManager::Instance()->PlayWav(m_sound[1]);
+
+      m_maxTimeInState = Rnd(m_time[2], m_time[3]);
+      spr->SetCellRange(m_cellRange[2], m_cellRange[3]);
+      spr->SetCell(m_cellRange[2]);
+    }
+    else
+    {
+      TheSoundManager::Instance()->PlayWav(m_sound[0]);
+
+      m_maxTimeInState = Rnd(m_time[0], m_time[1]);
+      spr->SetCellRange(m_cellRange[0], m_cellRange[1]);
+      spr->SetCell(m_cellRange[1]);
+    }
   }
 }
 
@@ -97,8 +151,8 @@ bool Baddie::Load(File* f)
     f->ReportError("Expected num cells Vec2");
     return false;
   }
-  Vec2f size;
-  if (!LoadVec2(f, &size))
+  
+  if (!LoadVec2(f, &m_size))
   {
     f->ReportError("Expected size Vec2");
     return false;
@@ -109,14 +163,57 @@ bool Baddie::Load(File* f)
     return false;
   }
 
-  // TODO Load max health
-
-  m_maxHealth = 10; 
+  if (!f->GetInteger(&m_maxHealth))
+  {
+    f->ReportError("Expected max health");
+    return false;
+  }
   m_health = m_maxHealth;
 
-  BaddieNode* bn = new BaddieNode(this, tex, cells.x, cells.y, size.x * 0.5f, size.y * 0.5f);
+  if (!f->GetFloat(&m_chaseSpeed))
+  {
+    f->ReportError("Expected chase speed");
+    return false;
+  }
+
+  BaddieNode* bn = new BaddieNode(this, tex, cells.x, cells.y, m_size.x * 0.5f, m_size.y * 0.5f);
 
   SetSceneNode(bn);
+
+  for (int i = 0; i < 4; i++)
+  {
+    if (!f->GetFloat(&m_time[i]))
+    {
+      f->ReportError("Expected time " + ToString(i));
+      return false;
+    }
+  }
+
+  for (int i = 0; i < 4; i++)
+  {
+    if (!f->GetInteger(&m_cellRange[i]))
+    {
+      f->ReportError("Expected cell range " + ToString(i));
+      return false;
+    }
+  }
+
+  m_timeInState = 0;
+  m_maxTimeInState = 0;
+
+  // Get sound filenames or <none>
+  for (int i = 0; i < 2; i++)
+  {
+    if (!f->GetDataLine(&m_sound[i]))
+    {
+      f->ReportError("Expected sound " + ToString(i));
+      return false;
+    }
+    if (m_sound[i] == "<none>")
+    {
+      m_sound[i] = "";
+    }
+  }
 
   /*
   if (!m_shadow->Load(f))
@@ -155,13 +252,15 @@ void Baddie::SetMenu(GuiMenu* menu)
   {
     p->ShootBaddie(this);
   }
-
-  //menu->AddChild(new GuiMenuItem("FIGHT!!", new CommandFight(this))); 
-////  AddMenuItem("FIGHT!!", new CommandFight(this)); 
 }
 
 void Baddie::OnCollideBullet()
 {
+  if (!m_isDestructible)
+  {
+    return;
+  }
+
   // Take damage - destroy if health <= 0
   m_health--;
 
@@ -176,44 +275,9 @@ void Baddie::OnCollideBullet()
   }
 }
 
-
-ToggleBaddie::ToggleBaddie()
-{
-  m_harmful = false;
-  for (int i = 0; i < 4; i++)
-  {
-    m_time[i] = 0;
-    m_cellRange[i] = 0;
-  }
-  m_timeInState = 0;
-  m_maxTimeInState = 0;
-}
-
-void ToggleBaddie::Update()
-{
-  float dt = TheTimer::Instance()->GetDt();
-  m_timeInState += dt;
-  if (m_timeInState > m_maxTimeInState)
-  {
-    m_timeInState = 0;
-    m_harmful = !m_harmful;
-    // New random time, within range for this state
-  }
-}
-
-int ToggleBaddie::GetDamage() const
+int Baddie::GetDamage() const
 {
   return m_harmful ? m_damage : 0;
-}
-
-bool ToggleBaddie::Load(File* f)
-{
-  if (!Baddie::Load(f))
-  {
-    return false;
-  }
-
-  return true;
 }
 }
 
--- trunk/Test/ve2/Source/Baddie.h
+++ trunk/Test/ve2/Source/Baddie.h
@@ -5,6 +5,12 @@
 
 namespace Amju
 {
+// BADDIE TYPE
+// Can inflict damage on local player.
+// Toggles between harmful and harmless state
+// e.g. grabber, moving spike, guillotine.
+// In harmful mode, can chase player.
+// Take damage, and so player can kill them.
 class Baddie : public Ve1Object
 {
 public:
@@ -16,7 +22,7 @@ public:
   virtual bool Load(File*);
   virtual void SetMenu(GuiMenu* menu);
 
-  virtual int GetDamage() const { return m_damage; }
+  int GetDamage() const; 
 
   void OnCollideBullet();
 
@@ -24,32 +30,27 @@ protected:
   int m_damage; // damage inflicked on player on contact
   bool m_isDestructible;
 
-  // So players can kill baddies - TODO
+  Vec2f m_size;
+
+  // Players can kill baddies 
   int m_maxHealth;
   int m_health;
 
-  // Chase player?
+  // Chase player in harmful mode if speed > 0.
+  // (Flees player if speed < 0! -- is this useful?)
   float m_chaseSpeed;
-};
 
-// Toggles between harmful and harmless state
-// e.g. grabber, moving spike, guillotine
-class ToggleBaddie : public Baddie
-{
-public:
-  ToggleBaddie();
-
-  virtual void Update();
-  virtual int GetDamage() const;
-  virtual bool Load(File*);
-
-protected:
   bool m_harmful; // toggles
+
   // 0-1 (min, max) time in harmless state, 2-3 (min, max) time in harmful state
   float m_time[4]; 
+
   int m_cellRange[4]; // 0-1 harmless, 2-3 harmful cell range
+
   float m_timeInState;
   float m_maxTimeInState;
+
+  std::string m_sound[2]; // harmless/harmful state change sound filename
 };
 }
 
