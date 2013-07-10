a4558ce32ee435adc01621bd436666618f67c25c
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-07-10 15:46:00 +0000

    Trying to improve text for "Today's Task" screen -- but maybe we don't need this at all.

--- trunk/Test/ve2/Source/GSTodaysTask.cpp
+++ trunk/Test/ve2/Source/GSTodaysTask.cpp
@@ -44,32 +44,25 @@ void GSTodaysTask::OnActive()
   GuiButton* ok = (GuiButton*)GetElementByName(m_gui, "ok-button");
   ok->SetCommand(OnTodaysTaskOk);
 
-  int d = TheResearchCalendar::Instance()->GetDayOnPlanet();
-  Assert(d > 0);
-  //Assert(d <= 8); //????
-  std::string str = "Day " + ToString(d) + "\non the island";
 
-  GuiText* dayN = (GuiText*)GetElementByName(m_gui, "date-text");
-  dayN->SetText(str);
-
-  const int FUEL_CELL_OBJECTIVE[8] = { 10, 20, 25, 30, 35, 40, 50, 100 };
-  int f = FUEL_CELL_OBJECTIVE[d - 1];
-
+  std::string str;
   GameMode gm = GetGameMode();
   if (gm == AMJU_MODE_SINGLE)
   {
-    str = "Find food for yourself!";
+    str = "Your goal is to find treasure! Keep yourself alive by eating food you find!";
   }
   else if (gm == AMJU_MODE_MULTI)
   {
-    str = "Find food for your friends on the island!";
+    str = "Your goal is to find treasure, and to keep your friends alive by giving them food!";
   }
   else
   {
     Assert(0);
     str = "";
   }
-
+  str += "\n\nTo move around, click the ground where you want to go.\n\n"
+    "You can change the way you look - click on your player, and choose \"Change Look\"."
+    "\n\nGood luck!"; 
   GuiText* obj = (GuiText*)GetElementByName(m_gui, "obj-text");
   obj->SetText(str);
 }
