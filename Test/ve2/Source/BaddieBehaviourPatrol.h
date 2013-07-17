f7c3b455f835604d9a56011bda98db516d7bcda6
jason.e.colman <jason.e.colman@gmail.com>
2013-07-17 14:51:39 +0000

    Add patrolling behaviour

---
+++ Test/ve2/Source/BaddieBehaviourPatrol.h
@@ -0,0 +1,29 @@
+#ifndef BB_PATROL_H_INCLUDED
+#define BB_PATROL_H_INCLUDED
+
+#include <Rect.h>
+#include "Baddie.h"
+
+namespace Amju
+{
+class BBPatrol : public BaddieBehaviour
+{
+public:
+  static const char* TYPENAME;
+
+  BBPatrol();
+  virtual void Update();
+  virtual bool Load(File*);
+
+protected:
+  // Patrol direction (velocity vector in x,z)
+  Vec2f m_patrolDir;
+  // Bounding box
+  Rect m_rect;
+  // Reverse direction when we reach the edge of the box, or wrap around to the other side?
+  bool m_reverse;
+};
+}
+
+#endif
+
