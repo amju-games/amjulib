c7b0aab6485af63804c04f716c967ac4ab0b8467
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-08-01 12:50:02 +0000

    Added events to ES2 app

--- trunk/Test/ve2/Script/iOSXCodeES2/Hungry People/Hungry People/ViewController.mm
+++ trunk/Test/ve2/Script/iOSXCodeES2/Hungry People/Hungry People/ViewController.mm
@@ -112,4 +112,126 @@
   Amju::TheGame::Instance()->RunOneLoop();
 }
 
+void QueueEvent(Amju::Event* e)
+{
+	Amju::TheEventPoller::Instance()->GetImpl()->QueueEvent(e);
+}
+
+void PopulateMBEvent(Amju::MouseButtonEvent* mbe, int x, int y)
+{
+  float scrX2 = float(Amju::Screen::X() / 2);
+  float scrY2 = float(Amju::Screen::Y() / 2);
+  
+#ifdef LANDSCAPE
+  mbe->x = 1.0f - (float)y / scrX2;
+  mbe->y = 1.0f - (float)x / scrY2;
+  
+#else
+  mbe->x = (float)x / scrY2 - 1.0f;
+  mbe->y = 1.0f - (float)y / scrX2;
+  
+#endif
+}
+
+void PopulateCursorEvent(Amju::CursorEvent* ce, int x, int y)
+{
+  ce->controller = 0;
+  
+  // Rotate cursor data to screen orientation - TODO make this run time
+  float scrX2 = float(Amju::Screen::X() / 2);
+  float scrY2 = float(Amju::Screen::Y() / 2);
+  
+#ifdef LANDSCAPE
+  ce->x = 1.0f - (float)y / scrX2;
+  ce->y = 1.0f - (float)x / scrY2;
+  
+#else
+  ce->x = (float)x / scrY2 - 1.0f;
+  ce->y = 1.0f - (float)y / scrX2;
+  
+#endif
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	int i = 0;
+	for (UITouch* touch in touches)
+	{
+		CGPoint touchPoint = [touch locationInView:self.view];
+		if (i++ == 0)
+		{
+			Amju::MouseButtonEvent* mbe = new Amju::MouseButtonEvent;
+			mbe->button = Amju::AMJU_BUTTON_MOUSE_LEFT;
+			mbe->isDown = true;
+			PopulateMBEvent(mbe, touchPoint.x, touchPoint.y);
+			QueueEvent(mbe);
+		}
+		
+		Amju::CursorEvent* ce = new Amju::CursorEvent;
+		PopulateCursorEvent(ce, touchPoint.x, touchPoint.y);
+		QueueEvent(ce);
+	}
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	int i = 0;
+	for (UITouch* touch in touches)
+	{
+		CGPoint touchPoint = [touch locationInView:self.view];
+		if (i++ == 0)
+		{
+			Amju::MouseButtonEvent* mbe = new Amju::MouseButtonEvent;
+			mbe->button = Amju::AMJU_BUTTON_MOUSE_LEFT;
+			mbe->isDown = false;
+			PopulateMBEvent(mbe, touchPoint.x, touchPoint.y);
+			QueueEvent(mbe);
+			return; // ignore other data, it this ok ?
+		}
+	}
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	for (UITouch* touch in touches)
+	{
+		CGPoint touchPoint = [touch locationInView:self.view];
+		Amju::CursorEvent* ce = new Amju::CursorEvent;
+		PopulateCursorEvent(ce, touchPoint.x, touchPoint.y);
+		QueueEvent(ce);
+	}
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
+{
+  // TODO Don't know what to do here
+}
+
+// TODO This isn't called
+- (void)accelerometer:(UIAccelerometer*)accelerometer didAccelerate:(UIAcceleration*)acceleration
+{
+	const float kFilteringFactor = 0.1;
+	static float accel[3] = { 0, 0, 0 };
+	
+	accel[0] = acceleration.x * kFilteringFactor + accel[0] * (1.0 - kFilteringFactor);
+	accel[1] = acceleration.y * kFilteringFactor + accel[1] * (1.0 - kFilteringFactor);
+	accel[2] = acceleration.z * kFilteringFactor + accel[2] * (1.0 - kFilteringFactor);
+	
+	// accel[0] corresponds to tilting forward/back, i.e. rotation about x-axis when in landscape mode
+	// accel[1] corresponds to z-rotation, like twisting a Wii remote
+	
+	Amju::BalanceBoardEvent* be = new Amju::BalanceBoardEvent;
+  
+  // TODO This depends on iphone orientation
+  be->x = accel[1];
+  be->y = accel[0];
+  
+#ifdef ACC_DEBUG
+	std::cout << "ACCEL: X: " << accel[0] << " Y: " << accel[1] << " Z: " << accel[2] << "\n";
+#endif
+  
+	QueueEvent(be);
+}
+
+
 @end
