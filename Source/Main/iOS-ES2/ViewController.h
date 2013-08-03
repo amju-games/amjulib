c2cda0f692329a6221accf0d48655c0259e3e9dd
jason.e.colman <jason.e.colman@gmail.com>
2013-08-03 11:06:09 +0000

    Common files for iOS ES2 projects

---
+++ Source/Main/iOS-ES2/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Hungry People
+//
+//  Created by Jason Colman on 29/07/2013.
+//  Copyright (c) 2013 Jason Colman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
