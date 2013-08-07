e153bb1a3f939d83a6de0dcca1aa0b0312ec7089
jason.e.colman <jason.e.colman@gmail.com>
2013-08-07 12:07:47 +0000

    Don't move camera too far outside of room boundary

--- Test/ve2/Source/Cam2d.cpp
+++ Test/ve2/Source/Cam2d.cpp
@@ -1,6 +1,7 @@
 #include <AmjuFirst.h>
 #include "Cam2d.h"
 #include "ROConfig.h"
+#include "Room.h"
 #include <AmjuFinal.h>
 
 namespace Amju
@@ -16,6 +17,34 @@ void Cam2d::Update()
   if (m_target)
   {
     pos = m_target->GetPos();
+  }
+
+  Room* room = GetRoom();
+  if (room)
+  {
+    Vec2f size = room->GetSize() * 0.5f;
+
+std::cout << "Cam: pos.x: " << pos.x << " size.x: " << size.x << "\n";
+
+    float MAX_X = 500.0f; 
+    if (pos.x + size.x > MAX_X)
+    {
+      pos.x = MAX_X - size.x;
+    }
+    else if (pos.x - size.x < -MAX_X)
+    {
+      pos.x = size.x - MAX_X;
+    }
+
+    float MAX_Y = 400.0f;
+    if (pos.z + size.y > MAX_Y)
+    {  
+      pos.z = MAX_Y - size.y;
+    }
+    else if (pos.z - size.y < -MAX_Y)
+    {
+      pos.z = size.y - MAX_Y;
+    } 
   }
 
   SetLookAtPos(pos);
