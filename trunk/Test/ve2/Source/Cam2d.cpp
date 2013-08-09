703240056ff1bffaedb4ec0bc71b31adbd90b40d
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-08-09 13:00:53 +0000

    Fix camera

--- trunk/Test/ve2/Source/Cam2d.cpp
+++ trunk/Test/ve2/Source/Cam2d.cpp
@@ -24,28 +24,30 @@ void Cam2d::Update()
   {
     Vec2f size = room->GetSize() * 0.5f;
 
-    float MAX_X = 500.0f; 
-    if (pos.x + size.x > MAX_X)
-    {
-      pos.x = MAX_X - size.x;
-    }
-    else if (pos.x - size.x < -MAX_X)
+    float MAX_X = 250.0f;  // visible to L/R of player
+
+    if (size.x - pos.x < MAX_X)
     {
       pos.x = size.x - MAX_X;
     }
-
-    float MAX_Y = 400.0f;
-    if (pos.z + size.y > MAX_Y)
-    {  
-      pos.z = MAX_Y - size.y;
+    else if (pos.x + size.x < MAX_X)
+    {
+      pos.x = -size.x + MAX_X;
     }
-    else if (pos.z - size.y < -MAX_Y)
+  
+    float MAX_Y = 150.0f; 
+    if (size.y - pos.z < MAX_Y)
     {
       pos.z = size.y - MAX_Y;
-    } 
+    }
+    else if (pos.z + size.y < MAX_Y)
+    {
+      pos.z = -size.y + MAX_Y;
+    }
   }
 
   SetLookAtPos(pos);
+
   static const float HEIGHT = ROConfig()->GetFloat("cam-z", 300.0f);
   SetEyePos(pos + Vec3f(0, HEIGHT, 2.0f)); // TODO TEMP TEST
 }
