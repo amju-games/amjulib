f65888f6dd58e7b242c12be4a10ff0d1bff651fa
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-08-15 12:35:54 +0000

    Fix env map demo state

--- trunk/Test/gltest/Source/GSEnvMap.cpp
+++ trunk/Test/gltest/Source/GSEnvMap.cpp
@@ -2,6 +2,7 @@
 #include <Teapot.h>
 #include <ResourceManager.h>
 #include <Texture.h>
+#include <Timer.h>
 #include "GSEnvMap.h"
 
 namespace Amju
@@ -19,16 +20,25 @@ void GSEnvMap::Update()
 
 void GSEnvMap::Draw()
 {
-  AmjuGL::SetClearColour(Colour(1, 0, 0, 1));
+  AmjuGL::SetClearColour(Colour(0.2f, 0.2f, 0.2f, 1));
   GSBase::Draw();
 
   static Teapot tp;
 
   static Texture* tex = 
     (Texture*)TheResourceManager::Instance()->GetRes("spheremap_bar.png");
-//  tex->UseThisTexture();
 
+  tex->SetTextureType(AmjuGL::AMJU_TEXTURE_SPHERE_MAP);
+  tex->UseThisTexture();
+
+  AmjuGL::PushMatrix();
+  AmjuGL::Scale(3, 3, 3);
+  static float f = 0;
+  f += TheTimer::Instance()->GetDt() * 30.0f;
+  AmjuGL::RotateX(f);
+  AmjuGL::RotateY(f);
   tp.Draw();
+  AmjuGL::PopMatrix();
 }
 
 void GSEnvMap::Draw2d()
