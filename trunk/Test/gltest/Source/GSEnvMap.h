81130d9759391bf45153e1f18f8cdd63e3544b09
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-08-15 09:20:07 +0000

    Add test for env (sphere) mapping

---
+++ trunk/Test/gltest/Source/GSEnvMap.h
@@ -0,0 +1,26 @@
+#ifndef GS_ENVMAP_H_INCLUDED
+#define GS_ENVMAP_H_INCLUDED
+
+#include <Singleton.h>
+#include <EventListener.h>
+#include "GSBase.h"
+
+namespace Amju 
+{
+class GSEnvMap : public GSBase
+{
+  GSEnvMap();
+  friend class Singleton<GSEnvMap>;
+
+public:
+  virtual void Update();
+  virtual void Draw();
+  virtual void Draw2d();
+  virtual void OnActive();
+
+  virtual bool OnCursorEvent(const CursorEvent&);
+  virtual bool OnMouseButtonEvent(const MouseButtonEvent&);
+};
+typedef Singleton<GSEnvMap> TheGSEnvMap;
+} // namespace
+#endif
