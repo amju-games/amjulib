8ee63e37426681aa824e3aad0cd130cb8ff373a6
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-10-30 11:16:30 +0000

    Logic error in dtor, replaced useless code with an Assert - indicates bad inheritance diamond.

--- trunk/Source/Events/EventListener.cpp
+++ trunk/Source/Events/EventListener.cpp
@@ -12,7 +12,11 @@ EventListener::~EventListener()
   // Make sure this Listener is removed from the Poller's list!
   if (TheEventPoller::Instance()->HasListener(this))
   {
-    TheEventPoller::Instance()->RemoveListener(this);
+    // This should never be called, because if we are still in the EventPoller's
+    //  list, our ref count will be 1, so we will never be in the dtor!!
+    // BUT sometimes it happens -- this indicates a bad multiple inheritance
+    //  diamond!!!
+    Assert(0);
   }
 
 }
