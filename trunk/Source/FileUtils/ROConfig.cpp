4156da94112aa431e52507d900cf9dfb320f8641
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-11-03 10:16:01 +0000

    Add ROConfig to amjulib (was in ve2 only)

---
+++ trunk/Source/FileUtils/ROConfig.cpp
@@ -0,0 +1,32 @@
+#include <AmjuFirst.h>
+#include <AmjuAssert.h>
+#include "ROConfig.h"
+#include <AmjuFinal.h>
+
+namespace Amju
+{
+static const char* RO_CONFIG_FILENAME = "roconfig.txt";
+
+//void SaveROConfig()
+//{
+//  ROConfig()->Save(RO_CONFIG_FILENAME);
+//}
+
+ConfigFile* ROConfig()
+{
+  static ConfigFile* cf = 0;
+  if (!cf)
+  {
+    cf = new ConfigFile;
+    if (!cf->Load(RO_CONFIG_FILENAME))
+    {
+      Assert(0);
+    }
+
+    // Duh, it's read only
+    //atexit(SaveROConfig);
+  }
+  return cf;
+}
+
+}
