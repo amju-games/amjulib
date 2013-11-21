1ff3b692c4f9b4726d17c56fbc5cbfe0293faaea
jason.e.colman <jason.e.colman@gmail.com>
2013-11-21 08:48:39 +0000

    Move Prev State code to base class

--- Source/Game/GameState.cpp
+++ Source/Game/GameState.cpp
@@ -1,4 +1,5 @@
 #include <AmjuFirst.h>
+#include "Game.h"
 #include "GameState.h"
 #include "EventPoller.h"
 #include <AmjuFinal.h>
@@ -12,4 +13,32 @@ void GameState::OnActive()
 void GameState::OnDeactive()
 {
 }
+
+void GameState::SetPrevState(GameState* prevState)
+{
+  Assert(prevState != this);
+  m_prevState = prevState;
+}
+
+GameState* GameState::GetPrevState()
+{
+  return m_prevState;
+}
+
+void GameState::GoBack()
+{
+  if (!m_prevState)
+  {
+    // Not sure if using type_info like this works reliably :-(
+    std::cout << "FAIL in Go back to prev state: current state: " << typeid(*this).name() << "\n"; 
+  }
+  Assert(m_prevState);
+
+#ifdef _DEBUG
+std::cout << "GO BACK to previous state... (" << typeid(*m_prevState).name() << ")\n";
+#endif
+
+  TheGame::Instance()->SetCurrentState(m_prevState);
+}
+
 }
