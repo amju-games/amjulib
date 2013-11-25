c73f7e00de5be7fbe27524a145da4c6f00d78ef8
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-11-25 22:56:51 +0000

    Save game config when we go inactive

--- trunk/Source/Main/iOS-ES2/AppDelegate.mm
+++ trunk/Source/Main/iOS-ES2/AppDelegate.mm
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 #import "ViewController.h"
 #include <Game.h>
+#include <ConfigFile.h>
 
 @implementation AppDelegate
 
@@ -38,7 +39,8 @@
   // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
   // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 
-  Amju::TheGame::Instance()->PauseGame(); 
+  Amju::TheGameConfigFile::Instance()->Save();
+  Amju::TheGame::Instance()->PauseGame();
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -46,7 +48,8 @@
   // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
   // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
   
-  Amju::TheGame::Instance()->PauseGame(); 
+  Amju::TheGameConfigFile::Instance()->Save();
+  Amju::TheGame::Instance()->PauseGame();
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
