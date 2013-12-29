b4ac922460058c3845c84b6c653d9267be494a1d
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2013-12-29 21:33:06 +0000

    Improve informal time period strings (TODO Still not localised)

--- trunk/Test/ve3/Source/TimePeriodString.cpp
+++ trunk/Test/ve3/Source/TimePeriodString.cpp
@@ -10,19 +10,35 @@ std::string InformalTimePeriodStringNotCapitalised(
   unsigned int sec = period.ToSeconds();
   if (sec < ONE_MINUTE)
   {
-    return past ? "a minute ago" : "a minute";
+    return past ? "less than a minute ago" : "less than a minute";
+  }
+  else if (sec < 5 * ONE_MINUTE)
+  {
+    return past ? "less than 5 minutes ago" : "less than 5 minutes";
+  }
+  else if (sec < 10 * ONE_MINUTE)
+  {
+    return past ? "less than 10 minutes ago" : "less than 10 minutes";
+  }
+  else if (sec < 30 * ONE_MINUTE)
+  {
+    return past ? "less than half an hour ago" : "less than half an hour";
   }
   else if (sec < ONE_HOUR)
   {
-    return past ? "an hour ago" : "an hour";
+    return past ? "less than an hour ago" : "less than an hour";
   }
   else if (sec < ONE_DAY_IN_SECONDS)
   {
-    return past ? "yesterday" : "one day";
+    return past ? "today" : "less than one day";
+  }
+  else if (sec < 2 * ONE_DAY_IN_SECONDS)
+  {
+    return past ? "yesterday" : "less than 2 days";
   }
   else if (sec < ONE_WEEK_IN_SECONDS)
   {
-    return past ? "a week ago" : "a week";
+    return past ? "less than a week ago" : "less than a week";
   }
   return past ? "over a week ago" : "more than a week";
 }
