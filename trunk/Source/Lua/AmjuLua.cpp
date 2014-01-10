49c0290338ffe7c23cdcb2caf2217883b53a79ec
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2014-01-10 19:14:00 +0000

    Lua fixes

--- trunk/Source/Lua/AmjuLua.cpp
+++ trunk/Source/Lua/AmjuLua.cpp
@@ -9,18 +9,30 @@ extern "C"
 #include "AmjuLua.h"
 #include <File.h>
 #include <Variable.h>
+#include <StringUtils.h>
 #include <AmjuFinal.h>
-
-#define LUA_DEBUG
 
 namespace Amju
 {
 extern void ReportError(const std::string&);
 
+static bool s_showInfo = false;
+
+void Lua::ShowInfoMessages(bool show)
+{
+  s_showInfo = show;
+}
+
+static bool ShowInfo()
+{
+  return s_showInfo;
+}
+
 // Push args onto Lua stack.
 // Used by Call() and Return() below
 // Returns no of args pushed.
-int PushLuaArgs(lua_State* L, const Variable& v)
+// Called recursively to flatten vectors of variables
+static int PushLuaArgs(lua_State* L, const Variable& v)
 {
   int numPushed = 0;
 
@@ -38,8 +50,7 @@ int PushLuaArgs(lua_State* L, const Variable& v)
   }
   else if (v.IsStringType())
   {
-    const char* strArg = v.ToString().c_str();
-    lua_pushstring(L, strArg);
+    lua_pushstring(L, v.GetString().c_str());
     numPushed = 1;
   }
   else if (v.IsVectorType())
@@ -52,47 +63,72 @@ int PushLuaArgs(lua_State* L, const Variable& v)
       numPushed += PushLuaArgs(L, vv);
     }
   }
+  else
+  {
+    // TODO
+    std::cout << "LUA: c++: Type not handled yet! Get on it dude!\n";
+    Assert(0);
+  }
+
   return numPushed;
 }
 
-void Lua::Return(lua_State* L, const Variable& retVals)
+int Lua::Return(lua_State* L, const Variable& retVals)
 {
-  PushLuaArgs(L, retVals);
+  if (ShowInfo())
+  {
+    std::cout << "LUA: c++: returning these vals: " << retVals.ToString() << "\n";
+  }
+
+  // Return total number of args pushed after flattening
+  return PushLuaArgs(L, retVals);
+}
+
+static Variable GetVariableFromLua(lua_State* L, int i)
+{
+  int t = lua_type(L, i);
+  switch (t)
+  {
+  case LUA_TNUMBER:
+    // sigh, can't distinguish between float and int numeric types?
+    return Variable(float(lua_tonumber(L, i)));
+    break;
+  case LUA_TBOOLEAN:
+    return Variable(lua_toboolean(L, i));
+    break;
+  case LUA_TSTRING:
+    return Variable(std::string(lua_tostring(L, i)));
+    break;
+      
+  case LUA_TTABLE:
+  case LUA_TNIL:
+  case LUA_TFUNCTION:
+  case LUA_TUSERDATA: 
+  case LUA_TTHREAD:
+  case LUA_TLIGHTUSERDATA:
+    std::cout << "LUA: c++: GetParams(): Type not handled yet! Get on it dude!\n";
+    Assert(0); // TODO
+  }
+  return Variable(); // null type
 }
 
 Variable Lua::GetParams(lua_State* L)
 {
   int n = lua_gettop(L);    /* number of arguments */
 
-  std::cout << n << " arguments...\n";
-
   VariableVec vv;
   for (int i = 1; i <= n; i++)
   {
-    int t = lua_type(L, i);
-    switch (t)
-    {
-    case LUA_TNUMBER:
-      // sigh, can't distinguish between float and int numeric types?
-      vv.push_back(Variable(float(lua_tonumber(L, i))));
-      break;
-    case LUA_TBOOLEAN:
-      vv.push_back(Variable(lua_toboolean(L, i)));
-      break;
-    case LUA_TSTRING:
-      vv.push_back(Variable(std::string(lua_tostring(L, i))));
-      break;
-      
-    case LUA_TTABLE:
-    case LUA_TNIL:
-    case LUA_TFUNCTION:
-    case LUA_TUSERDATA: 
-    case LUA_TTHREAD:
-    case LUA_TLIGHTUSERDATA:
-      Assert(0); // TODO
-    }
+    vv.push_back(GetVariableFromLua(L, i));
   }
-  return vv;
+
+  Variable v(vv);
+  if (ShowInfo())
+  {
+    std::cout << "LUA: c++: got these params: " << v.ToString() << "\n";
+  }
+
+  return v;
 }
 
 Lua::Lua()
@@ -102,8 +138,7 @@ Lua::Lua()
   // Include all lua libraries.
   luaL_openlibs(m_pL);
 
-  luaopen_debug(m_pL); // we want to make it as easy as possible to 
-    // debug scripts!
+  luaopen_debug(m_pL); // we want to make it as easy as possible to debug scripts!
 }
 
 Lua::~Lua()
@@ -111,32 +146,33 @@ Lua::~Lua()
   lua_close(m_pL);
 }
 
-bool Lua::LoadScript(const char* filename, bool useGlue)
+bool Lua::LoadScriptFromFile(const std::string& filename, bool useGlue)
 {
-#ifdef LUA_DEBUG
-std::cout << "Loading lua script " << filename << "...\n";
-#endif
+  m_filename = filename;
 
-  // TODO TEMP TEST
-  int r = luaL_loadfile(m_pL, filename);
-#ifdef LUA_DEBUG
-std::cout << " ..result: " << r << "\n";
-#endif
+  // TODO Use glue file if required
+  int r = luaL_loadfile(m_pL, filename.c_str());
 
   if (r == 0)
   {
     // Need to do this before we can call any functions in the file.
     lua_call(m_pL, 0, 0);
+    if (ShowInfo())
+    {
+      std::cout << "LUA: c++: Loaded lua file " << filename << " OK\n";
+    }
   }
   else
   {
-    const char* error = lua_tostring(m_pL, -1);
+    std::string error = "LUA: c++: Loading lua file \"" + filename + 
+      "\" failed: Code " + ToString(r) + ": " + lua_tostring(m_pL, -1);
     ReportError(error);
   }
 
   return (r == 0);
 
 
+  // TODO use Amju::File
   bool NOVERSIONINFO = false;
   File f(NOVERSIONINFO, useGlue ? File::GLUE : File::STD);
   if (!f.OpenRead(filename)) // TODO binary ? use Root Dir ?
@@ -151,7 +187,7 @@ std::cout << " ..result: " << r << "\n";
   return false;
 }
 
-bool Lua::LoadScript(const std::string& script)
+bool Lua::LoadScriptFromMem(const std::string& script)
 {
   // TODO
   return false;
@@ -163,31 +199,40 @@ bool Lua::Call(
   Variable* pResults,
   int numRetVals)
 {
+  if (ShowInfo())
+  {
+    std::cout << "LUA: c++: calling lua function \"" << funcName
+      << "\" Args: " 
+      << args.ToString() << "\n";
+  }
+
   lua_getglobal(m_pL, funcName.c_str());
 
   // Push each arg on to the lua stack
   int numArgs = PushLuaArgs(m_pL, args);
 
-#ifdef LUA_DEBUG
-std::cout << "LUA: c++ calling lua function: " << funcName
-  << " num args pushed: " << numArgs << "\nArgs: " 
-  << args.ToString().c_str() << "\n";
-#endif
-
   // Call func, give no of args and no of return values
   int retcode = lua_pcall(m_pL, numArgs, numRetVals, 0);
   if (retcode != 0)
   {
-    // TODO report error ?
-
     // Pop error message off stack.
     const char* msg = lua_tostring(m_pL, -1);
     lua_pop(m_pL, 1);
-    ReportError(msg);
+    std::string err = "LUA: c++: attempt to call lua function \"" + funcName + "\" ";
+    if (m_filename.empty())
+    {
+      err += "(no file name) ";
+    }
+    else
+    {
+      err += "in file \"" + m_filename + "\" ";
+    }
+    err += "failed: ";
+    ReportError(err + msg);
 
     return false;
   }
-  
+
   // Get return vals
   if (numRetVals == 0)
   {
@@ -195,31 +240,28 @@ std::cout << "LUA: c++ calling lua function: " << funcName
   }
   else if (numRetVals == 1)
   {
-    // TODO handle different return types
-
-//    lua_getresult();
-    int retVal = (int)lua_tonumber(m_pL, -1);
+    Variable v = GetVariableFromLua(m_pL, -1);
     lua_pop(m_pL, 1);
-    *pResults = retVal;
+    *pResults = v;
   }
   else
   {
     VariableVec vec;
     for (int j = 0; j < numRetVals; j++)
     {
-      // TODO How can we handle other return types ?
-      int retVal = (int)lua_tonumber(m_pL, -1);
-      vec.push_back(Variable(retVal));
+      Variable v = GetVariableFromLua(m_pL, -1);
+      vec.push_back(v);
       lua_pop(m_pL, 1);
     }
+    std::reverse(vec.begin(), vec.end()); // !
     *pResults = vec;
   }
 
-#ifdef LUA_DEBUG
-std::cout << "LUA: function call succeeded, num ret vals: "
-  << numRetVals << ". Values: " 
-  << pResults->ToString() << "\n";
-#endif
+  if (ShowInfo())
+  {
+    std::cout << "LUA: c++ call to lua function \"" << funcName << "\" succeeded. Return values: " 
+      << pResults->ToString() << "\n";
+  }
  
   return true;
 }
@@ -228,6 +270,12 @@ bool Lua::Register(const Lua::LuaFuncName& funcName, lua_CFunction func)
 {
   const char* funcNameStr = funcName.c_str();
   lua_register(m_pL, funcNameStr, func);
+
+  if (ShowInfo())
+  {
+    std::cout << "LUA: c++ function \"" << funcName << "\" registered OK\n"; 
+  }
+
   return true;
 }
 }
--- trunk/Test/luatest/Assets/test.lua
+++ trunk/Test/luatest/Assets/test.lua
@@ -2,14 +2,14 @@
 
 -- function to be called from C++ 
 
-function hello()
-  io.write("Hello world, from ",_VERSION,"!\n");
+function hello_ret3vals()
+  io.write("lua: Hello world, from ",_VERSION,"!\n");
   -- how to return multiple values, e.g. Vec3 or Matrix? - easy!!?
   return 1, 2, 3;
 end
 
 function TestMyRegisteredFunction()
-  io.write("Calling C function...\n");
+  io.write("lua: Calling C function...\n");
   return mycfunc(78, "wow!", true);  
 end
 
