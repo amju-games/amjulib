491b9564b98900b4ad2bb74b537430592b2d7055
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2014-01-24 14:02:00 +0000

    Fix SceneNode loading

--- trunk/Source/Scene/LoadScene.cpp
+++ trunk/Source/Scene/LoadScene.cpp
@@ -1,7 +1,73 @@
+#include <StringUtils.h>
+#include <ObjMesh.h>
+#include <ResourceManager.h>
+#include <File.h>
 #include "LoadScene.h"
+#include "SceneMesh.h"
+#include "SceneNodeFactory.h"
 
 namespace Amju
 {
+PSceneNode LoadScene(const std::string& filename)
+{
+  // TODO Factory of loader types
+  if (GetFileExt(filename) == "obj")
+  {
+    ObjMesh* mesh = (ObjMesh*)TheResourceManager::Instance()->GetRes(filename);
+    if (!mesh)
+    {
+      return nullptr;
+    }
+
+    SceneMesh* sm  = new SceneMesh;
+    sm->SetMesh(mesh);
+    return sm;
+  }
+
+  File f;
+  if (!f.OpenRead(filename))
+  {
+    return 0;
+  }
+  return LoadScene(&f);
+}
+
+PSceneNode LoadScene(File* f)
+{
+  std::string s;
+  if (!f->GetDataLine(&s))
+  {
+    f->ReportError("Expected scene node type");
+    return 0;
+  }
+  PSceneNode node = TheSceneNodeFactory::Instance()->Create(s);
+  if (!node)
+  {
+    f->ReportError("Failed to create scene node of type: " + s);
+    return 0;
+  }
+  if (!node->Load(f))
+  {
+    return 0;
+  }
+  int numChildren = 0;
+  if (!f->GetInteger(&numChildren))
+  {
+    // OK, we are not specifying any children - allow this.
+    //f->ReportError("Expected num children for scene node.");
+    return node;
+  }
+  for (int i = 0; i < numChildren; i++)
+  {
+    PSceneNode child = LoadScene(f);
+    if (!child)
+    {
+      return 0;
+    }
+    node->AddChild(child);
+  }
+  return node;
+}
 
 }
 
--- trunk/Source/Scene/SceneNode.cpp
+++ trunk/Source/Scene/SceneNode.cpp
@@ -14,67 +14,6 @@ const char* SceneNode::NAME = "comp";
 
 SceneNode* SceneNode::Create() { return new SceneNode; }
 
-PSceneNode LoadScene(const std::string& filename)
-{
-  // TODO Factory of loader types
-  if (GetFileExt(filename) == "obj")
-  {
-    ObjMesh* mesh = (ObjMesh*)TheResourceManager::Instance()->GetRes(filename);
-    if (!mesh)
-    {
-      return nullptr;
-    }
-
-    SceneMesh* sm  = new SceneMesh;
-    sm->SetMesh(mesh);
-    return sm;
-  }
-
-  File f;
-  if (!f.OpenRead(filename))
-  {
-    return 0;
-  }
-  return LoadScene(&f);
-}
-
-PSceneNode LoadScene(File* f)
-{
-  std::string s;
-  if (!f->GetDataLine(&s))
-  {
-    f->ReportError("Expected scene node type");
-    return 0;
-  }
-  PSceneNode node = TheSceneNodeFactory::Instance()->Create(s);
-  if (!node)
-  {
-    f->ReportError("Failed to create scene node of type: " + s);
-    return 0;
-  }
-  if (!node->Load(f))
-  {
-    return 0;
-  }
-  int numChildren = 0;
-  if (!f->GetInteger(&numChildren))
-  {
-    // OK, we are not specifying any children - allow this.
-    //f->ReportError("Expected num children for scene node.");
-    return node;
-  }
-  for (int i = 0; i < numChildren; i++)
-  {
-    PSceneNode child = LoadScene(f);
-    if (!child)
-    {
-      return 0;
-    }
-    node->AddChild(child);
-  }
-  return node;
-}
-
 SceneNode::SceneNode()
 {
   m_parent = 0;
