841453afd755d256a7bcc96a882eb844ee237eaa
jason.e.colman <jason.e.colman@gmail.com>
2014-02-03 22:17:46 +0000

    Add script to make release - MacOSX

---
+++ Script/MacOSX/make_release.sh
@@ -0,0 +1,61 @@
+# Make release of amjulib for MacOSX
+export AMJU_BUILD_ARCH=Mac
+export AMJU_MACHDEP='-arch i386 -DMACOSX -DXP_MACOSX -D__i386__ -DAMJU_USE_BASS -DAMJU_USE_CURL'
+
+# Build (release mode)
+export EXTRA_CFLAGS='-g -O2 -DNDEBUG'
+
+make clean
+make
+
+export LIB=../../Build/amjulib/lib/
+export INC=../../Build/amjulib/include/
+export SRC=../../Source/
+
+# Create release dir structure
+mkdir -p $LIB
+mkdir -p $INC
+
+rm $INC/*
+rm $LIB/*
+
+# Copy .lib binary to lib folder
+cp ../../Build/Mac/amjulib.a $LIB/
+
+# Copy . headers required by client code to include folder
+cp $SRC/AmjuGL/*.h $INC
+cp $SRC/AmjuGLOpenGL/*.h $INC
+cp $SRC/Collision/*.h $INC
+cp $SRC/Events/*.h $INC
+cp $SRC/FileUtils/*.h $INC
+cp $SRC/Font/*.h $INC
+cp $SRC/Game/*.h $INC
+cp $SRC/Geom/*.h $INC
+cp $SRC/GeomDraw/*.h $INC
+cp $SRC/GeomLoad/*.h $INC
+cp $SRC/Gui/*.h $INC
+cp $SRC/Image/*.h $INC
+cp $SRC/Localise/*.h $INC
+cp $SRC/Lua/*.h $INC
+cp $SRC/Main/*.h $INC
+rm $INC/nonglutmain.h
+cp $SRC/Md2/Md2Model.h $INC
+cp $SRC/Network/*.h $INC
+cp $SRC/Obj/*.h $INC
+cp $SRC/Resource/*.h $INC
+cp $SRC/Scene/*.h $INC
+cp $SRC/Sound/*.h $INC
+cp $SRC/SoundBass/*.h $INC
+cp $SRC/Strings/*.h $INC
+cp $SRC/Thread/*.h $INC
+cp $SRC/TimeUtils/*.h $INC
+cp $SRC/Utils/*.h $INC
+
+# Make documentation and copy to docs folder
+
+# Copy other stuff, e.g. examples
+
+
+# Copy readme
+
+
