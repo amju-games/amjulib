88bd9465cf22efd538d1ca7caa13aa8073d6e1d5
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2014-02-15 18:11:16 +0000

    Create list of all .cpp files in the include dirs.

--- trunk/Script/Android/jni/Android.mk
+++ trunk/Script/Android/jni/Android.mk
@@ -17,7 +17,7 @@ LOCAL_CFLAGS := -DANDROID_NDK \
 # Oh, sigh
 LOCAL_C_INCLUDES := \
 	../../../Source/AmjuGL \
-	../../../Source/AmjuGLOpenGL \
+	../../../Source/AmjuGLES \
 	../../../Source/Collision \
 	../../../Source/Community \
 	../../../Source/Events \
@@ -29,6 +29,7 @@ LOCAL_C_INCLUDES := \
 	../../../Source/GeomLoad \
 	../../../Source/Gui \
 	../../../Source/Image \
+	../../../Source/Image/Png \
 	../../../Source/Localise \
 	../../../Source/Lua \
 	../../../Source/Md2 \
@@ -44,47 +45,7 @@ LOCAL_C_INCLUDES := \
 	../../../Source/Utils \
 	../../../Source/Utils/Mgc
 
-# Oh noes :-(
-LOCAL_SRC_FILES := \
-	../../../Source/AmjuGL/AmjuGL.cpp \
-	../../../Source/AmjuGL/AmjuGLWindowInfo.cpp \
-	../../../Source/AmjuGL/Colour.cpp \
-	../../../Source/AmjuGL/Screen.cpp \
-	../../../Source/AmjuGL/TriList.cpp \
-	../../../Source/AmjuGLOpenGL/AmjuGL-OpenGL-Base.cpp \
-	../../../Source/AmjuGLOpenGL/AmjuGL-OpenGLES.1.cpp \
-	../../../Source/AmjuGLOpenGL/AmjuGL-OpenGLES.2.cpp \
-	../../../Source/AmjuGLOpenGL/ES2DefaultShaders.cpp \
-	../../../Source/AmjuGLOpenGL/GLShader.cpp \
-	../../../Source/Collision/BruteForce.cpp \
-	../../../Source/Collision/CollisionManager.cpp \
-	../../../Source/Collision/CollisionMesh.cpp \
-	../../../Source/Collision/SAP.cpp \
-	../../../Source/Community/DownloadReq.cpp \
-	../../../Source/Events/EventListener.cpp \
-	../../../Source/Events/EventPoller.cpp \
-	../../../Source/Events/EventTypes.cpp \
-	../../../Source/Events/GameStateListener.cpp \
-	../../../Source/FileUtils/ConfigFile.cpp \
-	../../../Source/FileUtils/Directory.cpp \
-	../../../Source/FileUtils/Endian.cpp \
-	../../../Source/FileUtils/File.cpp \
-	../../../Source/FileUtils/FileImpl.cpp \
-	../../../Source/FileUtils/FileImplGlue.cpp \
-	../../../Source/FileUtils/FileImplMem.cpp \
-	../../../Source/FileUtils/FileImplStd.cpp \
-	../../../Source/FileUtils/FileSize.cpp \
-	../../../Source/FileUtils/GlueFile.cpp \
-	../../../Source/FileUtils/GlueFileMem.cpp \
-	../../../Source/FileUtils/MkDir.cpp \
-	../../../Source/FileUtils/ROConfig.cpp \
-	../../../Source/FileUtils/StringsFile.cpp \
-	../../../Source/FileUtils/Tokeniser.cpp \
-	../../../Source/Font/Font.cpp \
-	../../../Source/Font/TextureSequence.cpp \
-	../../../Source/Game/Game.cpp \
-	../../../Source/Game/GameObject.cpp \
-	../../../Source/Game/GameState.cpp
-
+# Oh yeah. Create list of all .cpp files in above directory list.
+LOCAL_SRC_FILES := $(foreach dir,$(LOCAL_C_INCLUDES),$(wildcard $(dir)/*.cpp))
 
 include $(BUILD_STATIC_LIBRARY)
