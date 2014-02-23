d4e0cbfe8dda9df4a1e701a44040e635e590f9a8
jason.e.colman <jason.e.colman@gmail.com>
2014-02-23 20:36:29 +0000

    Fix simplest for Android

--- Examples/simplest/Source/main.cpp
+++ Examples/simplest/Source/main.cpp
@@ -8,13 +8,21 @@
 #endif 
 #endif // WIN32
 
+#if !defined(ANDROID_NDK)
+#include <iostream>
 #define AMJU_USE_OPENGL
 #define AMJU_USE_GLUT
-
-#include <iostream>
 #include <main.h>
+#endif
+
+#ifdef ANDROID_NDK
+#include <unistd.h>
+#include <AmjuGL-OpenGLES.1.h>
+#endif
+
 #include <AmjuGLWindowInfo.h>
 #include <Game.h>
+#include <EventPoller.h>
 #include "GSSimplest.h"
 
 namespace Amju
@@ -24,15 +32,30 @@ Amju::AmjuGLWindowInfo w(640, 480, false, "Simplest");
 
 void ReportError(const std::string& str)
 {
+#if !defined(ANDROID_NDK)
   std::cout << str << "\n";
+#endif
 }
 
 void StartUpBeforeCreateWindow()
 {
+// Android: wait for debugger, or any crash at startup will be too soon 
+// Thanks: http://stackoverflow.com/questions/17705109/how-to-debug-c-c-code-ndk-in-eclipse
+#if defined(ANDROID_NDK) && !defined(NDEBUG)
+usleep(5000 * 1000);
+#endif
+
+#ifdef ANDROID_NDK
+  TheEventPoller::Instance()->SetImpl(new EventPollerImpl);
+#endif
 }
 
 void StartUpAfterCreateWindow()
 {
+#ifdef ANDROID_NDK
+  AmjuGL::SetImpl(new AmjuGLOpenGLES1);
+#endif
+
   TheGame::Instance()->SetCurrentState(TheGSSimplest::Instance());
 }
 }
