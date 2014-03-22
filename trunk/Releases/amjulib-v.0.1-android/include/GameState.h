4e915f40048975afa4d9533dce4d65468f892cc5
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2014-03-22 18:35:54 +0000

    Release v.0.1 for macosx and android

---
+++ trunk/Releases/amjulib-v.0.1-android/include/GameState.h
@@ -0,0 +1,35 @@
+#ifndef GAME_STATE_H
+#define GAME_STATE_H
+
+#include "EventListener.h"
+
+namespace Amju
+{
+class GameStateListener;
+
+class GameState : public EventListener
+{
+public:
+  GameState();
+  virtual ~GameState();
+
+  virtual void Update() = 0;
+  virtual void Draw() = 0;
+  virtual void Draw2d() = 0;
+ 
+  // Call these base class impls from subclass impls!
+  virtual void OnActive();
+  virtual void OnDeactive();
+
+  void SetPrevState(GameState* prev);
+  GameState* GetPrevState();
+  void GoBack();  // return to prev state - must have been set!
+
+protected:
+  GameState* m_prevState;
+  int m_eventPriority; 
+  RCPtr<GameStateListener> m_listener; // forwards events to this state
+};
+}
+
+#endif
