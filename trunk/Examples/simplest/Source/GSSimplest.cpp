924b02f07c5ff29791c86e6b8356b1f0fc65362d
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2014-05-01 16:12:45 +0000

    Android fixes

--- trunk/Examples/simplest/Source/GSSimplest.cpp
+++ trunk/Examples/simplest/Source/GSSimplest.cpp
@@ -14,8 +14,16 @@ void GSSimplest::Draw()
   AmjuGL::SetIdentity();
   AmjuGL::LookAt(0, 5, 10,   0, 0, 0,   0, 1, 0);
 
+  AmjuGL::Enable(AmjuGL::AMJU_LIGHTING);
+  AmjuGL::DrawLighting(
+    AmjuGL::LightColour(0.2f, 0.2f, 0.2f),
+    AmjuGL::LightColour(0.3f, 0.3f, 0.3f),
+    AmjuGL::LightColour(0.7f, 0.7f, 0.3f),
+    AmjuGL::LightColour(1.0f, 1.0f, 1.0f),
+    AmjuGL::Vec3(1, 1, 1));
+
   static Teapot tp;
-  AmjuGL::SetColour(1, 0, 0);
+  AmjuGL::SetColour(1, 0.7f, 0);
   tp.Draw();
 }
 }
--- trunk/Examples/simplest/Source/main.cpp
+++ trunk/Examples/simplest/Source/main.cpp
@@ -8,16 +8,12 @@
 #endif 
 #endif // WIN32
 
-#if !defined(ANDROID_NDK)
 #include <iostream>
+
+#if !defined(ANDROID_NDK) && !defined(AMJU_IOS)
 #define AMJU_USE_OPENGL
 #define AMJU_USE_GLUT
 #include <main.h>
-#endif
-
-#ifdef ANDROID_NDK
-#include <unistd.h>
-#include <AmjuGL-OpenGLES.1.h>
 #endif
 
 #include <AmjuGLWindowInfo.h>
@@ -32,30 +28,15 @@ Amju::AmjuGLWindowInfo w(640, 480, false, "Simplest");
 
 void ReportError(const std::string& str)
 {
-#if !defined(ANDROID_NDK)
   std::cout << str << "\n";
-#endif
 }
 
 void StartUpBeforeCreateWindow()
 {
-// Android: wait for debugger, or any crash at startup will be too soon 
-// Thanks: http://stackoverflow.com/questions/17705109/how-to-debug-c-c-code-ndk-in-eclipse
-#if defined(ANDROID_NDK) && !defined(NDEBUG)
-usleep(5000 * 1000);
-#endif
-
-#ifdef ANDROID_NDK
-  TheEventPoller::Instance()->SetImpl(new EventPollerImpl);
-#endif
 }
 
 void StartUpAfterCreateWindow()
 {
-#ifdef ANDROID_NDK
-  AmjuGL::SetImpl(new AmjuGLOpenGLES1);
-#endif
-
   TheGame::Instance()->SetCurrentState(TheGSSimplest::Instance());
 }
 }
