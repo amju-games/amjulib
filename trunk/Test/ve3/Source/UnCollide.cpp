5853fc65a276989bc1f5553108a66e30a74887b3
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2014-05-04 09:06:17 +0000

    Ve3 improvements/fixes

--- trunk/Test/ve3/Source/UnCollide.cpp
+++ trunk/Test/ve3/Source/UnCollide.cpp
@@ -1,9 +1,13 @@
+#include <Timer.h>
 #include "UnCollide.h"
+#include "Player.h"
 
 namespace Amju
 {
 void UnCollide(GameObject* go, const Vec3f& oldPos, const AABB& aabb)
 {
+  Player* player = dynamic_cast<Player*>(go);
+
   /* 
   Move back so not interpenetrating:
   Boxes now intersect in all axes. Moving away by penetration depth in any
@@ -11,9 +15,16 @@ void UnCollide(GameObject* go, const Vec3f& oldPos, const AABB& aabb)
   position. E.g. if previously not intersecting in X, move away in X axis.
   */
 
+  // Push away more?
+  const float EXTRA = 1.1f;
+  //const float MOVE_AWAY_VEL = 200.0f;
+  const float dt = TheTimer::Instance()->GetDt();
+
   // Intersecton region
   AABB ir = aabb.Intersection(go->GetAABB());
   Vec3f goPos = go->GetPos();
+  Vec3f goVel = go->GetVel();
+  Vec3f moveToPos = player->GetMoveToPos();
 
   AABB oldBox = go->GetAABB();
   Vec3f move = oldPos - goPos;
@@ -29,11 +40,15 @@ void UnCollide(GameObject* go, const Vec3f& oldPos, const AABB& aabb)
   if (oldBox.GetMax(0) < aabb.GetMin(0))
   {
     // Old box to left of box, so move away to the left
-    goPos.x -= penDist.x; 
+    goPos.x -= (penDist.x * EXTRA);
+    moveToPos.x = goPos.x;
+    //goVel.x  = -MOVE_AWAY_VEL;
   }
   else if (oldBox.GetMin(0) > aabb.GetMax(0))
   {
-    goPos.x += penDist.x;
+    goPos.x += penDist.x * EXTRA;
+    moveToPos.x = goPos.x;
+    //goVel.x  = MOVE_AWAY_VEL;
   }
   /*
   else if (oldBox.GetMax(1) < aabb.GetMin(1))
@@ -47,13 +62,20 @@ void UnCollide(GameObject* go, const Vec3f& oldPos, const AABB& aabb)
   */
   else if (oldBox.GetMax(2) < aabb.GetMin(2))
   {
-    goPos.z -= penDist.z;
+    goPos.z -= (penDist.z * EXTRA);
+    moveToPos.z = goPos.z;
+//    goVel.z  = -MOVE_AWAY_VEL;
   }
   else if (oldBox.GetMin(2) > aabb.GetMax(2))
   {
-    goPos.z += penDist.z;
+    goPos.z += penDist.z * EXTRA;
+    moveToPos.z = goPos.z;
+//    goVel.z  = MOVE_AWAY_VEL;
   }
 
   go->SetPos(goPos);
+  player->MoveTo(moveToPos);
+
+  //  go->SetVel(goVel);
 }
 }
