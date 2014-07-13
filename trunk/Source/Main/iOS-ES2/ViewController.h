ed20879dfda82593e4e3f308243afaf2f2948a3d
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2014-07-13 01:51:25 +0000

    Support for compass and accelerometer for head tracking

--- trunk/Source/Main/iOS-ES2/ViewController.h
+++ trunk/Source/Main/iOS-ES2/ViewController.h
@@ -9,12 +9,17 @@
 #import <UIKit/UIKit.h>
 #import <GLKit/GLKit.h>
 #import <CoreMotion/CoreMotion.h>
+#import <CoreLocation/CoreLocation.h> // for compass
 
-@interface ViewController : GLKViewController
+@interface ViewController : GLKViewController <CLLocationManagerDelegate>
 
 // j.c. accelerometer - http://www.appcoda.com/ios-maze-game-tutorial-accelerometer/
 @property (assign, nonatomic) CMAcceleration acceleration;
 @property (strong, nonatomic) CMMotionManager  *motionManager;
 @property (strong, nonatomic) NSOperationQueue *queue;
+
+// j.c. compass - http://www.devfright.com/how-to-use-the-iphone-digital-compass-in-your-app/
+@property (strong, nonatomic) CLLocationManager *locationManager;
+
 
 @end
