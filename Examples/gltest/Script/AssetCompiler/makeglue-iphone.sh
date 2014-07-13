e5f4c5842f39c45ea82684c8f126d8e3561f646b
jason.e.colman <jason.e.colman@gmail.com>
2014-07-13 01:48:44 +0000

    iOS XCode project for gltest - requires glue file, as apple does something sucky to .png files.

---
+++ Examples/gltest/Script/AssetCompiler/makeglue-iphone.sh
@@ -0,0 +1,38 @@
+# Create glue file
+
+export PLATFORM=iphone
+export THIS_DIR=`pwd`
+export TOP_DIR=$THIS_DIR/../../../..
+export COMPILED_ASSETS=$TOP_DIR/Build/gltest/CompiledAssets
+export DEST_DIR=$COMPILED_ASSETS/$PLATFORM
+export SRC_DIR=../../Assets
+export GLUE_EXE=$THIS_DIR/glue
+export GLUE_FILE=$DEST_DIR/../data-$PLATFORM.glue
+
+rm -rf $DEST_DIR
+mkdir -p $DEST_DIR
+mkdir -p $DEST_DIR/font2d/
+mkdir -p $DEST_DIR/Shaders/gles
+
+cp $SRC_DIR/*.png $DEST_DIR
+cp $SRC_DIR/*.txt $DEST_DIR
+cp $SRC_DIR/Shaders/gles/*.txt $DEST_DIR/Shaders/gles
+cp $SRC_DIR/font2d/*.png $DEST_DIR/font2d/
+cp $SRC_DIR/font2d/*.txt $DEST_DIR/font2d/
+
+cd $DEST_DIR
+
+# Create empty glue file
+$GLUE_EXE -c $GLUE_FILE
+
+for f in *.txt *.png font2d/* Shaders/gles/*
+do
+    echo "Adding file: " $f
+    $GLUE_EXE -a $GLUE_FILE $f
+done
+
+# Verify glue file contents
+$GLUE_EXE -d $GLUE_FILE
+
+cd $THIS_DIR
+
