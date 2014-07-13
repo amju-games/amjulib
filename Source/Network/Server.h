a9f4b644c5a37c9d90bd1082f46d9e13160f2d48
jason.e.colman <jason.e.colman@gmail.com>
2014-07-13 09:16:13 +0000

    Add Server type, needs work

---
+++ Source/Network/Server.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <Thread.h>
+#include <Mutex.h>
+#include <Singleton.h>
+
+namespace Amju
+{
+// For processes that want to listen on a socket.
+// Call the public member functions from the main thread.
+// Thread-safe.
+class Server
+{
+public:
+  typedef std::map<int, std::string> Clients; // TODO ClientInfo
+
+  bool Start(int port);
+ 
+  void Update();
+
+private:
+  Mutex m_mutex;
+  Clients m_clients;
+
+};
+
+typedef Singleton<Server> TheServer;
+}
+
