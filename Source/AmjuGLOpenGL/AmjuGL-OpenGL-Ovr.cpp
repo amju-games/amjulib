29e1b3678e3432ca89dbfd8fa483b2a9c68013fa
jason.e.colman <jason.e.colman@gmail.com>
2014-07-14 19:50:15 +0000

    Add version of AmjuGL which works with OVR

---
+++ Source/AmjuGLOpenGL/AmjuGL-OpenGL-Ovr.cpp
@@ -0,0 +1,18 @@
+#include "AmjuGL-OpenGL-Ovr.h"
+
+namespace Amju
+{
+void AmjuGLOpenGL_Ovr::Init()
+{
+}
+
+bool AmjuGLOpenGL_Ovr::CreateWindow(AmjuGLWindowInfo*)
+{
+}
+
+void AmjuGLOpenGL_Ovr::Flip()
+{
+}
+
+}
+
