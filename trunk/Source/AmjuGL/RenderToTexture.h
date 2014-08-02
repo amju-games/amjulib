33f17268dbb9637378eef5895b998b0f8463de0a
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2014-08-02 12:43:16 +0000

    Add flags so we can render colour, depth, or both

--- trunk/Source/AmjuGL/RenderToTexture.h
+++ trunk/Source/AmjuGL/RenderToTexture.h
@@ -11,6 +11,11 @@ public:
 
   RenderToTexture();
 
+  // Set what to render, depth and/or colour. Default value is AMJU_RENDER_COLOUR
+  static const int AMJU_RENDER_COLOUR = 0x01;
+  static const int AMJU_RENDER_DEPTH = 0x02; 
+  void SetRenderFlags(int renderFlags);
+
   void SetSize(int w, int h);
 
   // Draw scene between Begin() and End() calls. Then you can use the texture.
@@ -23,7 +28,10 @@ public:
   // "Drawing" the RenderToTexture object means we use the texture
   //  for what we draw next (e.g. draw full screen quad)
 
-  virtual void Draw() override { UseThisTexture(); } 
+  virtual void Draw() override { UseThisTexture(); }
+
+  // Make the texture(s) active. There could be 1 texture (depth or colour)
+  //  or 2 textures if both are set in SetRenderFlags.
   virtual void UseThisTexture() = 0;
 
 protected:
@@ -31,6 +39,8 @@ protected:
   
   // Viewport before switching to this render target, so we revert in End()
   int m_vpX, m_vpY, m_vpW, m_vpH;
+
+  int m_renderFlags;
 };
 }
 
