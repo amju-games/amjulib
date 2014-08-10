b8d6f9458af261347f7308b7894bdceaa0acb3ad
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2014-08-10 20:20:33 +0000

    Use GL_CHECK

--- trunk/Source/AmjuGLOpenGL/RenderToTextureES2.cpp
+++ trunk/Source/AmjuGLOpenGL/RenderToTextureES2.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <AmjuGL.h>
 #include "RenderToTextureES2.h"
+#include "AmjuGL-OpenGL-Base.h"
 
 namespace Amju
 {
@@ -23,58 +24,69 @@ RenderToTextureES2::~RenderToTextureES2()
  
 void RenderToTextureES2::UseThisTexture()
 {
-  glEnable(GL_TEXTURE_2D);
-  AmjuGL::Enable(AmjuGL::AMJU_TEXTURE_2D);
+//  GL_CHECK(glEnable(GL_TEXTURE_2D));
+//  AmjuGL::Enable(AmjuGL::AMJU_TEXTURE_2D);
+
+  // ??
+//////  GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, 0));
   
-  glBindTexture(GL_TEXTURE_2D, m_tex);
+  GL_CHECK(glActiveTexture(GL_TEXTURE0));  
+  GL_CHECK(glBindTexture(GL_TEXTURE_2D, m_tex));
 }
   
 bool RenderToTextureES2::Init()
 {
-  glGetIntegerv(GL_FRAMEBUFFER_BINDING, &m_old_fbo);  
+  GL_CHECK(glGetIntegerv(GL_FRAMEBUFFER_BINDING, &m_old_fbo));
   
 #ifdef AMJU_IOS
 
-  glGenFramebuffers(1, &m_framebuffer);
-  glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer);
+  GL_CHECK(glGenFramebuffers(1, &m_framebuffer));
+  GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer));
 
   // create the texture
-  glGenTextures(1, &m_tex);
-  glBindTexture(GL_TEXTURE_2D, m_tex);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_width, m_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_tex, 0);
+  GL_CHECK(glGenTextures(1, &m_tex));
+  GL_CHECK(glBindTexture(GL_TEXTURE_2D, m_tex));
+  GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+  GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+  GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+  GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
+  GL_CHECK(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_width, m_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL));
+  
+  GL_CHECK(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_tex, 0));
 
-  GLuint depthRenderbuffer;
-  glGenRenderbuffers(1, &depthRenderbuffer);
-  glBindRenderbuffer(GL_RENDERBUFFER, depthRenderbuffer);
-  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, m_width, m_height);
-  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderbuffer);
+  GLuint depthRenderbuffer = 0;
+  GL_CHECK(glGenRenderbuffers(1, &depthRenderbuffer));
+  GL_CHECK(glBindRenderbuffer(GL_RENDERBUFFER, depthRenderbuffer));
+  GL_CHECK(glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT16, m_width, m_height));
+  GL_CHECK(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderbuffer));
+  
+  GL_CHECK(glClearColor(0, 0, 0, 1));
+  GL_CHECK(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)); // + depth??
   
 #else
   
-  glGenFramebuffers(1, &m_framebuffer);
-  glGenTextures(1, &m_tex);
+  GL_CHECK(glGenFramebuffers(1, &m_framebuffer));
+  GL_CHECK(glGenTextures(1, &m_tex));
 
-  glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer);
-  glBindTexture(GL_TEXTURE_2D, m_tex);
+  GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer));
+  GL_CHECK(glBindTexture(GL_TEXTURE_2D, m_tex));
   
   // This fixed black screen of death
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  GL_CHECK(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
   
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 
+  GL_CHECK(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
                width, height, 0, GL_RGBA, 
-               GL_UNSIGNED_BYTE, 0);
+               GL_UNSIGNED_BYTE, 0));
   
   // Use GL_COLOR_ATTACHMENT0 or GL_DEPTH_ATTACHMENT????
   
   // Doesn't work
 //  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_tex, 0);
   
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_tex, 0);
+  GL_CHECK(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_tex, 0));
 
-  glClearColor(0, 0, 0, 1);
-  glClear(GL_COLOR_BUFFER_BIT); // + depth??
+  GL_CHECK(glClearColor(0, 0, 0, 1));
+  GL_CHECK(glClear(GL_COLOR_BUFFER_BIT)); // + depth??
 
 #endif
   
@@ -84,7 +96,7 @@ bool RenderToTextureES2::Init()
     std::cout << status << std::endl; // this is not called
   }
   
-  glBindFramebuffer(GL_FRAMEBUFFER, m_old_fbo);
+  GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, m_old_fbo));
 
   return true;
 }
@@ -94,19 +106,22 @@ bool RenderToTextureES2::Begin()
   AmjuGL::GetViewport(&m_vpX, &m_vpY, &m_vpW, &m_vpH);
   AmjuGL::Viewport(0, 0, m_width, m_height); // same as size in Init
     
-  glBindTexture(GL_TEXTURE_2D, 0);
-  glGetIntegerv(GL_FRAMEBUFFER_BINDING, &m_old_fbo);
-  glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer);
+  GL_CHECK(glBindTexture(GL_TEXTURE_2D, 0));
+  GL_CHECK(glGetIntegerv(GL_FRAMEBUFFER_BINDING, &m_old_fbo));
+//  std::cout << "Wow, old fbo is " << m_old_fbo << "\n";
+  
+  GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer));
 
-  glClearColor(0, 0, 1, 1);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // depth??
+  const Colour& c = AmjuGL::GetClearColour();
+  GL_CHECK(glClearColor(c.m_r, c.m_g, c.m_b, 1));
+  GL_CHECK(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
   
   return true;
 }
 
 bool RenderToTextureES2::End()
 {
-  glBindFramebuffer(GL_FRAMEBUFFER, m_old_fbo);
+  GL_CHECK(glBindFramebuffer(GL_FRAMEBUFFER, m_old_fbo));
   AmjuGL::Viewport(m_vpX, m_vpY, m_vpW, m_vpH);
   
   return true;
