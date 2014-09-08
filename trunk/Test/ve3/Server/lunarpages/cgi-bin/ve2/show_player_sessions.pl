434a5bb8861b281da6982fa1820ac9537ed447c0
jason.e.colman@gmail.com <jason.e.colman@gmail.com@3cf48660-8901-11de-b841-6521bc8c149b>
2014-09-08 11:02:25 +0000

    Server scripts to output results

--- trunk/Test/ve3/Server/lunarpages/cgi-bin/ve2/show_player_sessions.pl
+++ trunk/Test/ve3/Server/lunarpages/cgi-bin/ve2/show_player_sessions.pl
@@ -1,24 +1,37 @@
 #!/usr/bin/perl -w
 
 #############################################
-## GET OBJECTS
+## SHOW PLAYER SESSIONS
+## Creates a .csv file for each player with
+##  login info 
 #############################################
 
 require "common.pl";
 
 my_connect();
 
-sub showsessions();
-showsessions();
+sub showsessions($);
+sub showresults();
+
+showresults();
 
 
 ##### end of main function
 
-sub showsessions()
+sub showsessions($)
 {
-  my $this_player = param("player") or die "Expected player ID";
+  my $this_player = shift;
 
-  my $sql = "select s.id, player_id, start, expires, expires-start, playername, email from session as s, player as p where s.player_id=p.id and p.id=$this_player order by s.id DESC limit 0, 40";
+  # public_html/ve2/results
+  my $dir = "../../ve2/results/";
+  my $filename = $dir . "player-$this_player-sessions.csv";
+
+  open (OUTFILE, ">$filename") or die "Failed to open output file $filename";
+  print "Writing to file $filename<br>\n";
+
+  print OUTFILE "Date, Yes, Session, Duration\n";
+
+  my $sql = "select s.id, player_id, start, expires, expires-start, playername, email from session as s, player as p where s.player_id=p.id and p.id=$this_player and start > DATE('2014-05-08')";
 
 #  print "Query: $sql\n\n";
 
@@ -37,9 +50,27 @@ sub showsessions()
     print "<tr>";
     print "<td>$id</td> <td>$player_id</td> <td>$start</td> <td>$expires</td> <td>$duration</td> <td>$playername</td> <td>$email</td>";
     print "</tr>\n";
+
+    print OUTFILE "$start, 1, $id, $duration\n";
   }
   print "</table>\n";
 
+  close(OUTFILE);
 }
 
+sub showresults()
+{
+  my $sql = "SELECT id, playername, email from player";
+  if (not $dbh) { print "Oh no, dbh is undefined!?!?!\n"; }
+  my $query = $dbh->prepare($sql) or die "Query prepare failed for this query: $sql\n";
+
+  $query->execute;
+  while (my ($player_id, $playername, $email) = $query->fetchrow_array)
+  {
+    print "Player ID: $player_id Name: $playername Email: $email<br>\n";
+
+    showsessions($player_id);
+    print "\n<br><br>\n";
+  }
+}
 
