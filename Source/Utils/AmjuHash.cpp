3cac9ef27fcfa13f342acf3215e039563596415a
Jason Colman <jason@touchsurgery.com>
2016-08-23 07:44:58 +0100

    Update HashString() to use std::string

--- Source/Utils/AmjuHash.cpp
+++ Source/Utils/AmjuHash.cpp
@@ -2,17 +2,17 @@
 
 namespace Amju
 {
-  unsigned int HashString(const char* string)
+unsigned int HashString(const std::string& str)
+{
+  unsigned int hash = 5381;
+  //int c;
+  //while ((c = *string++))
+  for (int c : str)
   {
-    unsigned int hash = 5381;
-    int c;
-    while ((c = *string++))
-    {
-      c = (c < 'A' || c > 'Z') ? c : (c - 'A' + 'a'); // Ignore case
-      hash = ((hash << 5) + hash) + c;                // hash*33 + c
-    }
-    return hash;
+    c = (c < 'A' || c > 'Z') ? c : (c - 'A' + 'a'); // Ignore case
+    hash = ((hash << 5) + hash) + c;                // hash*33 + c
   }
-
+  return hash;
+}
 }
 
