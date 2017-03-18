512f9329d7fcb73b29196fe27032b9f5fd577e3f
Jason Colman <jason@amju.com>
2017-03-18 21:52:59 +0000

    Remove cvs log

--- Source/Font/TextureSequence.h
+++ Source/Font/TextureSequence.h
@@ -1,26 +1,3 @@
-/*
-Amju Games source code (c) Copyright Jason Colman 2004
-$Log: TextureSequence.h,v $
-Revision 1.6  2007/11/13 21:33:13  jay
-Remove all OpenGL calls from game code
-
-Revision 1.5  2006/11/29 23:01:37  jay
-Print text in world space, for PetZoo pet names.
-
-Revision 1.4  2006/03/10 19:20:28  jay
-Add Set functions
-
-Revision 1.3  2006/02/17 19:01:46  jay
-New size parameter added to Draw(). Use this rather than glScale.
-
-Revision 1.2  2004/10/17 18:38:33  jay
-Load from File; store size
-
-Revision 1.1  2004/09/08 15:42:59  jay
-Added to repository
-  
-*/
-
 #if !defined(TEXTURE_SEQUENCE_H_INCLUDED)
 #define TEXTURE_SEQUENCE_H_INCLUDED
 
