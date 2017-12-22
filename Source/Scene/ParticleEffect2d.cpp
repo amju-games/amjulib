de487d3e3ae6568330530c5142f6554730825998
Jason Colman <jason@amju.com>
2017-12-22 10:39:16 +0000

    Move TriList building to Draw call

--- Source/Scene/ParticleEffect2d.cpp
+++ Source/Scene/ParticleEffect2d.cpp
@@ -95,6 +95,88 @@ void ParticleEffect2d::Draw()
     return;
   }
 
+  // Update particles here, so we can get MView matrix.
+  // TODO Move this to shader
+  float dt = TheTimer::Instance()->GetDt();
+  bool isDead = true;
+
+  int s = m_particles.size();
+
+  for (int i = 0; i < s; i++)
+  {
+    Particle2d& p = m_particles[i];
+
+    UpdateParticle(&p, dt);
+
+    // Update time - dead ?
+    p.m_time += dt;
+    if (p.m_time > m_maxTime)
+    {
+      HandleDeadParticle(&p);
+      // TODO continue ??
+    }
+
+    if (p.m_pos.y < m_minY)
+    {
+      HandleMinY(&p);
+    }
+
+    isDead &= p.m_isDead;
+
+    p.m_vel += p.m_acc * dt;
+    p.m_pos += p.m_vel * dt; // TODO not accurate    
+    p.m_rot += p.m_rotVel * dt;
+  }
+  m_isDead = isDead;
+
+  Matrix mat;
+  mat.ModelView(); // Get Modelview matrix
+  mat = m_combined * mat; // also take all rotation into account
+  Vec3f right(mat[0], mat[4], mat[8]);
+  Vec3f up(mat[1], mat[5], mat[9]);
+  Vec3f fwd(mat[2], mat[6], mat[10]);
+
+  // Avoid reallocs
+  //AmjuGL::Tris tris;
+  m_tris.resize(s * 2);
+
+  for (int i = 0; i < s; i++)
+  {
+    Particle2d& p = m_particles[i];
+
+    Quaternion q(fwd, p.m_rot);
+    Vec3f upR = q.RotateVec(up);
+    Vec3f rightR = q.RotateVec(right);
+
+    Vec3f v0 = p.m_pos + ( upR + rightR) * m_size;
+    Vec3f v1 = p.m_pos + ( upR - rightR) * m_size;
+    Vec3f v2 = p.m_pos + (-upR - rightR) * m_size;
+    Vec3f v3 = p.m_pos + (-upR + rightR) * m_size;
+    
+    // TODO Also we can now set per-vertex colours!
+    AmjuGL::Vert verts[4] =
+    { 
+      AmjuGL::Vert(v0.x, v0.y, v0.z,   1, 0,   0, 1, 0), // x, y, z, u, v, nx, ny, nz    
+      AmjuGL::Vert(v1.x, v1.y, v1.z,   1, 1,   0, 1, 0),
+      AmjuGL::Vert(v2.x, v2.y, v2.z,   0, 1,   0, 1, 0),
+      AmjuGL::Vert(v3.x, v3.y, v3.z,   0, 0,   0, 1, 0)
+    };
+    
+    AmjuGL::Tri* tri = &m_tris[i * 2];
+    tri->m_verts[0] = verts[0];
+    tri->m_verts[1] = verts[1];
+    tri->m_verts[2] = verts[2];
+    
+    tri = &m_tris[i * 2 + 1];
+    tri->m_verts[0] = verts[0];
+    tri->m_verts[1] = verts[2];
+    tri->m_verts[2] = verts[3];
+  }
+
+  m_triList->Set(m_tris);
+
+  // Do the actual Draw
+
   // TODO Also push lighting and blend state ?
   AmjuGL::PushAttrib(AmjuGL::AMJU_DEPTH_WRITE);
   AmjuGL::Disable(AmjuGL::AMJU_DEPTH_WRITE);
@@ -149,6 +231,11 @@ void ParticleEffect2d::Recycle(Particle2d* p)
 {
   p->m_pos = NewPos(); 
   p->m_time = NewTime();
+  // Shuuurely also:
+  p->m_vel = NewVel();
+  p->m_acc = NewAcc();
+  p->m_rot = NewRot();
+  p->m_rotVel = NewRotVel();
 }
 
 void ParticleEffect2d::HandleDeadParticle(Particle2d* p)
@@ -158,94 +245,12 @@ void ParticleEffect2d::HandleDeadParticle(Particle2d* p)
 
 void ParticleEffect2d::Update()
 {
-  if (m_isDead)
-  {
-    return;
-  }
-
-  float dt = TheTimer::Instance()->GetDt();
-  bool isDead = true;
-
-  int s = m_particles.size();
-
-  for (int i = 0; i < s; i++)
-  {
-    Particle2d& p = m_particles[i];
-
-    UpdateParticle(&p, dt);
-    
-    // Update time - dead ?
-    p.m_time += dt;
-    if (p.m_time > m_maxTime)
-    {
-      HandleDeadParticle(&p);
-      // TODO continue ??
-    }
-
-    if (p.m_pos.y < m_minY)
-    {
-      HandleMinY(&p);
-    }
-
-    isDead &= p.m_isDead;
-
-    p.m_vel += p.m_acc * dt;
-    p.m_pos += p.m_vel * dt; // TODO not accurate    
-    p.m_rot += p.m_rotVel * dt;
-  }
-  m_isDead = isDead;
-
-  // TODO Not needed if we can disable Z-writing..?
-//  std::sort(m_particles.begin(), m_particles.end());
-
-  Matrix mat;
-  mat.ModelView(); // Get Modelview matrix
-  mat = m_combined * mat; // also take all rotation into account
-  Vec3f right(mat[0], mat[4], mat[8]);
-  Vec3f up(mat[1], mat[5], mat[9]);
-  Vec3f fwd(mat[2], mat[6], mat[10]);
-
-  // Avoid reallocs
-  //AmjuGL::Tris tris;
-  m_tris.resize(s * 2);
-
-  for (int i = 0; i < s; i++)
-  {
-    Particle2d& p = m_particles[i];
-
-    Quaternion q(fwd, p.m_rot);
-    Vec3f upR = q.RotateVec(up);
-    Vec3f rightR = q.RotateVec(right);
-
-    Vec3f v0 = p.m_pos + ( upR + rightR) * m_size;
-    Vec3f v1 = p.m_pos + ( upR - rightR) * m_size;
-    Vec3f v2 = p.m_pos + (-upR - rightR) * m_size;
-    Vec3f v3 = p.m_pos + (-upR + rightR) * m_size;
-
-    AmjuGL::Vert verts[4] = 
-    {
-      AmjuGL::Vert(v0.x, v0.y, v0.z,   1, 0,   0, 1, 0), // x, y, z, u, v, nx, ny, nz  
-      AmjuGL::Vert(v1.x, v1.y, v1.z,   1, 1,   0, 1, 0),
-      AmjuGL::Vert(v2.x, v2.y, v2.z,   0, 1,   0, 1, 0),
-      AmjuGL::Vert(v3.x, v3.y, v3.z,   0, 0,   0, 1, 0)
-    };
-
-    AmjuGL::Tri* tri = &m_tris[i * 2];
-    tri->m_verts[0] = verts[0];
-    tri->m_verts[1] = verts[1];
-    tri->m_verts[2] = verts[2];
-
-    tri = &m_tris[i * 2 + 1];
-    tri->m_verts[0] = verts[0];
-    tri->m_verts[1] = verts[2];
-    tri->m_verts[2] = verts[3];
-  }
-
-  m_triList->Set(m_tris);
 }
 
 void ParticleEffect2d::Start()
 {
+std::cout << "****ParticleEffect2d::Start!!!\n";
+
   m_isDead = false;
 
   for (int i = 0; i < m_numParticles; i++)
