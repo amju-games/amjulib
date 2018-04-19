2351f5d4ab181445c6406bdb4289b2317345dff7
Jason Colman <jason@amju.com>
2018-04-19 11:25:08 +0100

    Vec2 ToString()

--- Source/GeomLoad/LoadVec2.h
+++ Source/GeomLoad/LoadVec2.h
@@ -1,6 +1,7 @@
 #ifndef LOAD_VEC2_H
 #define LOAD_VEC2_H
 
+#include <sstream>
 #include <string>
 #include "Vec2.h"
 
@@ -14,6 +15,13 @@ bool LoadVec2(File* f, Vec2i* v);
 bool ToVec2(const std::string& s, Vec2f* v);
 bool ToVec2(const std::string& s, Vec2i* v);
 
+template<class T>
+std::string ToString(const Vec2<T>& v)
+{
+  std::stringstream ss;
+  ss << v.x << "," << v.y;
+  return ss.str();
+}
 }
 
 #endif
