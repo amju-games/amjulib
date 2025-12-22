980f69f285923c6aef4db616b4346df504196dbc
Juliet <juliet.e.colman@gmail.com>
2025-12-22 22:32:42 +0000

    Glue tool: Mac makefile -- XCode project is deprecated

---
+++ Tools/glue/Script/MaxOSX/make_glue.sh
@@ -0,0 +1,8 @@
+# Make build of GLUE for MacOSX
+export AMJU_BUILD_ARCH=Mac
+export AMJU_MACHDEP='-DMACOSX -DXP_MACOSX -DAMJU_USE_BASS -DAMJU_USE_CURL -DUSE_SHADOW_MAP_OPENGL_2'
+
+export AMJULIB_MAC_SCRIPT_PATH=`pwd`/../../../../Script/MacOSX/
+
+make
+
