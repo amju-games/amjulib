155874713790a8c1e058b745690cc5ccb3a0d500
Juliet <juliet.e.colman@gmail.com>
2025-12-22 22:30:02 +0000

    Extra instructions for forgetful old hacks

--- Script/MacOSX/setup_make_vars.sh
+++ Script/MacOSX/setup_make_vars.sh
@@ -1,4 +1,6 @@
-# Hello, run me to set vars for amjulib makefiles
+# Hello, SOURCE me to set vars for amjulib makefiles
+# Like this:
+# . ./setup_make_vars.sh
 
 export AMJU_BUILD_ARCH=Mac
 export AMJU_MACHDEP='-DMACOSX -DXP_MACOSX -DAMJU_USE_BASS_MIDI -DAMJU_USE_BASS -DAMJU_USE_CURL -DUSE_SHADOW_MAP_OPENGL_2 -DGL_SILENCE_DEPRECATION'
