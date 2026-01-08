7ac23639019cc61aa58df91f101bfd6873080bc4
Juliet <juliet.e.colman@gmail.com>
2026-01-08 16:00:30 +0000

    Improvements for Endian family of functions - there's a C++20 check for machine endianness, but unfortunately my compiler doesn't support bit_cast, so the swapping code uses memcpy and std::reverse. Still, nice

--- Source/FileUtils/Endian.h
+++ Source/FileUtils/Endian.h
@@ -1,30 +1,38 @@
 /*
 Amju Games source code (c) Copyright Juliet Colman 2004
-$Log: Endian.h,v $
-Revision 1.3  2007/09/22 20:25:07  jay
-Move types to AmjuTypes.h, first step towards decoupling from OpenGL
-
-Revision 1.2  2006/01/16 13:34:19  jay
-Add test for Mac Intel
-
-Revision 1.1  2004/09/08 15:43:18  jay
-Added to repository
-  
 */
 
-#if !defined(SCHMICKEN_ENDIAN_H_INCLUDED)
-#define SCHMICKEN_ENDIAN_H_INCLUDED
+#pragma once
+
+#include <algorithm>
+#include <bit>      // For std::endian - C++20
+#include <cstring>   // For std::memcpy
 
 #include "AmjuTypes.h"
 
-// These functions swap the order of bytes. This would be useful when a 
-// binary file saved under one Endianness was loaded under the other.
+// Swap byte order if neccessary. 
 namespace Amju
 {
-int16 Endian(int16 i);
-uint16 Endian(uint16 u);
-int32 Endian(int32 i);
-uint32 Endian(uint32 u);
-float32 Endian(float32 f);
+template <typename T>
+T Endian(T val) 
+{
+    if constexpr (std::endian::native == std::endian::big) 
+    {
+        // 1. Copy the object into a byte array safely
+        unsigned char bytes[sizeof(T)];
+        std::memcpy(bytes, &val, sizeof(T));
+        
+        // 2. Reverse the bytes in place
+        std::reverse(bytes, bytes + sizeof(T));
+        
+        // 3. Copy the reversed bytes back into a temporary result
+        T result;
+        std::memcpy(&result, bytes, sizeof(T));
+        return result;
+    }
+    
+    // On Little Endian, this is all that runs
+    return val;
 }
-#endif
+}
+
