0912c73bb29322c8d27468f27c7eabd140ca00c4
Juliet <juliet.e.colman@gmail.com>
2026-01-13 16:19:28 +0000

    Sigh, Sign change was in the cpp file. OK now we don't need that file. updated MSVC project.

--- Source/Utils/Sign.h
+++ Source/Utils/Sign.h
@@ -3,7 +3,19 @@
 
 namespace Amju
 {
-float Sign(float f);
+  template <typename T>
+  T Sign(T t)
+  {
+    if (t < 0)
+    {
+      return static_cast<T>(-1);
+    }
+    else if (t > 0)
+    {
+      return static_cast<T>(1);
+    }
+    return 0;
+  }
 }
 
 #endif
