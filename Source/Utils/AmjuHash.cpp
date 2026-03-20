f08438d0f32745188a5b8cf560150af709bf5cba
Juliet <juliet.e.colman@gmail.com>
2026-03-20 09:24:23 +0000

    Use modern C++ hashing

--- Source/Utils/AmjuHash.cpp
+++ Source/Utils/AmjuHash.cpp
@@ -4,15 +4,17 @@ namespace Amju
 {
 unsigned int HashString(const std::string& str)
 {
+  return std::hash<std::string>{}(str);
+
+#ifdef OLD_SKOOL
   unsigned int hash = 5381;
-  //int c;
-  //while ((c = *string++))
   for (int c : str)
   {
     c = (c < 'A' || c > 'Z') ? c : (c - 'A' + 'a'); // Ignore case
     hash = ((hash << 5) + hash) + c;                // hash*33 + c
   }
   return hash;
+#endif
 }
 }
 
