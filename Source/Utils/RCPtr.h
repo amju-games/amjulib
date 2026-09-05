9a32fe4e91c7bbfb79b3f5cccc083d1321f3d530
Juliet <juliet@amju.com>
2026-09-05 10:56:08 +0100

    Add WeakPtr<T>: RCPtr<T> checks that there are no outstanding weak ptrs when deleting.
    Use WeakPtr where appropriate -- overuse of RCPtr has led to lifetime issues. In particular, we should not have RCPtrs that are static or owned by a static object.

--- Source/Utils/RCPtr.h
+++ Source/Utils/RCPtr.h
@@ -2,10 +2,8 @@
 Amju Games source code (c) Copyright Juliet Colman 2009
 */
 
-#ifndef AMJU_RC_PTR_INCLUDED
-#define AMJU_RC_PTR_INCLUDED
+#pragma once
 
-#include <algorithm>
 #include "AmjuAssert.h"
 
 namespace Amju 
@@ -16,10 +14,13 @@ class RefCounted
 {
 private:
   template <class T> friend class RCPtr;
-  mutable int m_shareable_refcount;
+  template <class T> friend class WeakPtr;
+  int m_shareable_refcount = 0;
+  int m_weakcount = 0;
 
 public:
   int GetRefCount() const { return m_shareable_refcount; }
+  int GetWeakCount() const { return m_weakcount; }
 
 protected:
   RefCounted() : m_shareable_refcount(0) {}
@@ -28,6 +29,7 @@ protected:
   virtual ~RefCounted() 
   { 
     Assert(m_shareable_refcount == 0); 
+    Assert(m_weakcount == 0);
   }
 };
 
@@ -39,7 +41,7 @@ public:
   RCPtr(T * pNew) : m_ptr(pNew) { Attach(); }
   RCPtr(const RCPtr & src) : m_ptr(src.m_ptr) { Attach(); }
 
-  ~RCPtr() { Detach(m_ptr); }
+  ~RCPtr() { Detach(); }
 
   RCPtr& operator=(const RCPtr& src) 
   {
@@ -67,20 +69,62 @@ private:
     }
   }
 
-  static void Detach(T * p)
+  void Detach()
   {
-    if (p) 
+    if (m_ptr) 
     {
-      Assert(p->m_shareable_refcount > 0);
-      if (--(p->m_shareable_refcount) == 0)
+      Assert(m_ptr->m_shareable_refcount > 0);
+      if (--(m_ptr->m_shareable_refcount) == 0)
       {
-        delete p;
+        delete m_ptr;
       } 
     }
   }
 };
 
+template <class T>
+class WeakPtr
+{
+public:
+  WeakPtr() = default;
+  WeakPtr(T* pNew) : m_ptr(pNew) { IncWeakCount(); }
+  WeakPtr(const WeakPtr& src) : m_ptr(src.m_ptr) { IncWeakCount(); }
+
+  ~WeakPtr() { DecWeakCount(); }
+
+  WeakPtr& operator=(const WeakPtr& src)
+  {
+    WeakPtr temp(src);
+    std::swap(m_ptr, temp.m_ptr);
+    return *this;
+  }
+
+  // Use this to access the raw ptr if necessary 
+  T* GetPtr() const { return m_ptr; }
+
+  // Type conversion operator
+  operator T* () const { return m_ptr; }
+
+  T* operator->() const { return m_ptr; }
+
+private:
+  T* m_ptr = nullptr;
+  void IncWeakCount()
+  {
+    if (m_ptr)
+    {
+      Assert(m_ptr->m_weakcount >= 0);
+      ++(m_ptr->m_weakcount);
+    }
+  }
+
+  void DecWeakCount()
+  {
+    if (m_ptr)
+    {
+      Assert(m_ptr->m_weakcount > 0);
+      --(m_ptr->m_weakcount);
+    }
+  }
+};
 } 
-
-#endif 
-
